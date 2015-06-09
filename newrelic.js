e3f9f602a2932019f93e521e4ff0d4a04ad86778
autoboxer <jared.j.collier@gmail.com>
2015-06-08 23:27:34 -0400

    Create base New Relic configuration to prevent application crashes.

--- newrelic.js
+++ newrelic.js
@@ -4,21 +4,21 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-exports.config = {
+ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['My Application'],
+   app_name: ['MARE'],
   /**
    * Your New Relic license key.
    */
-  license_key: 'license key here',
-  logging: {
+  // license_key: 'license key here',
+  // logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
-  }
-}
+//     level: 'info'
+//   }
+ }
