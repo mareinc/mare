01821a022f6df76631dfb0b9ce96c7a80b0a03f2
autoboxer <jared.j.collier@gmail.com>
2015-07-05 22:11:04 -0400

    Remove Q promises from the photoListing.js middleware, and fix data passthrough to allow the photoListing template to render children data.

--- routes/views/photoListing.js
+++ routes/views/photoListing.js
@@ -1,8 +1,5 @@
 var keystone = require('keystone');
 
-// Load Q for promises
-var Q = require('q');
-
 // Load model to allow fetching of children data
 var Child = keystone.list('Child')
 
@@ -11,31 +8,16 @@ exports = module.exports = function(req, res) {
   
     var locals = res.locals;
     
-    var view    = new keystone.View(req, res),
-        locals  = res.locals,
-        children = locals.children;
-
-    var childrenFetched = Q.defer();
+    var view = new keystone.View(req, res);
 
     // Use the menu ID to find all page references it contains
     Child.model.find()
         .exec()
         .then(function (results) {
-            children = results;
 
-            childrenFetched.resolve();
+            locals.children = results;
+
+            view.render('photoListing');
         });
-
-    // Load menu data via deferreds to ensure they are fully populated before moving on
-  Q.all([
-    childrenFetched
-  ]).then(function() {
-    // Fetch the page with the matching URL
-    // If it exists, pass the object into the rendering
-    // If it doesn't exist, forward to a 404 page
-    
-    // Render the view
-    view.render('photoListing');
-  }).done();
 
 };
