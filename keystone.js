ee2368cdcfaa7613bc03d1817fe8dcc17df3589f
Jared Collier <jared.j.collier@gmail.com>
2015-08-02 11:28:14 -0400

    Update spacing across files.

--- keystone.js
+++ keystone.js
@@ -5,7 +5,7 @@ require('dotenv').load();
 
 // Require keystone
 var keystone = require('keystone'),
-  handlebars = require('express-handlebars');
+	handlebars = require('express-handlebars');
 
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
@@ -13,31 +13,31 @@ var keystone = require('keystone'),
 
 keystone.init({
 
-  'name': 'MARE',
-  'brand': 'MARE',
+	'name': 'MARE',
+	'brand': 'MARE',
 
-  'sass': 'public',
-  'static': 'public',
-  'favicon': 'public/favicon.ico',
-  'views': 'templates/views',
-  'view engine': 'hbs',
-  
-  'custom engine': handlebars.create({
-    layoutsDir: 'templates/views/layouts',
-    partialsDir: 'templates/views/partials',
-    defaultLayout: 'default',
-    helpers: new require('./templates/views/helpers')(),
-    extname: '.hbs'
-  }).engine,
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'hbs',
+	
+	'custom engine': handlebars.create({
+		layoutsDir: 'templates/views/layouts',
+		partialsDir: 'templates/views/partials',
+		defaultLayout: 'default',
+		helpers: new require('./templates/views/helpers')(),
+		extname: '.hbs'
+	}).engine,
 
-  'emails': 'templates/emails',
-  
-  'auto update': true,
-  'session': true,
-  'auth': true,
-  'user model': 'User',
-  // TODO: Check to see if this should remain private, especially if the project is open source
-  'cookie secret': 'tq+qW&CaS~{bTSw|nG=]KGW#2X*u}<?$v9S|LaD|K*.q>.a<&rC[A~6%9rEwtl~h',
+	'emails': 'templates/emails',
+	
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	// TODO: Check to see if this should remain private, especially if the project is open source
+	'cookie secret': 'tq+qW&CaS~{bTSw|nG=]KGW#2X*u}<?$v9S|LaD|K*.q>.a<&rC[A~6%9rEwtl~h',
 
 });
 
@@ -52,10 +52,10 @@ keystone.import('models');
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
-  _: require('underscore'),
-  env: keystone.get('env'),
-  utils: keystone.utils,
-  editable: keystone.content.editable
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
 });
 
 // Load your project's Routes
@@ -64,18 +64,18 @@ keystone.set('routes', require('./routes'));
 // Setup common locals for your emails. The following are required by Keystone's
 // default email templates, you may remove them if you're using your own.
 keystone.set('email locals', {
-  logo_src: '/images/logo-email.gif',
-  logo_width: 194,
-  logo_height: 76,
-  theme: {
-    email_bg: '#f9f9f9',
-    link_color: '#2697de',
-    buttons: {
-      color: '#fff',
-      background_color: '#2697de',
-      border_color: '#1a7cb7'
-    }
-  }
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7'
+		}
+	}
 });
 
 // Setup replacement rules for emails, to automate the handling of differences
@@ -102,11 +102,11 @@ keystone.set('email locals', {
 // }]);
 
 keystone.set('email rules', [{
-  find: '/images/',
-  replace: (keystone.get('env') === 'production') ? 'http://adoptions.io/images/' : 'http://localhost:3000/images/'
+	find: '/images/',
+	replace: (keystone.get('env') === 'production') ? 'http://adoptions.io/images/' : 'http://localhost:3000/images/'
 }, {
-  find: '/keystone/',
-  replace: (keystone.get('env') === 'production') ? 'http://adoptions.io/keystone/' : 'http://localhost:3000/keystone/'
+	find: '/keystone/',
+	replace: (keystone.get('env') === 'production') ? 'http://adoptions.io/keystone/' : 'http://localhost:3000/keystone/'
 }]);
 
 // Load your project's email test routes
@@ -114,7 +114,7 @@ keystone.set('email tests', require('./routes/emails'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-  'users': 'users'
+	'users': 'users'
 });
 
 // Start Keystone to connect to your database and initialise the web server
--- models/Form.js
+++ models/Form.js
@@ -1,33 +1,33 @@
 var keystone = require('keystone'),
-    Types = keystone.Field.Types;
+	Types = keystone.Field.Types;
 
 // Create model. Additional options allow menu name to be used what auto-generating URLs
 var Form = new keystone.List('Form', {
-    track: true,
-    autokey: { path: 'key', from: 'title', unique: true },
-    map: { name: 'title' }
+	track: true,
+	autokey: { path: 'key', from: 'title', unique: true },
+	map: { name: 'title' }
 });
 
 // Create fields
 Form.add({
-    title: { type: String, label: 'form name', required: true, initial: true, index: true },
-    url: { type: Types.Url, noedit: true },
-    contact: { type: Types.Relationship, ref: 'User', label: 'contact person', initial: true, dependsOn: { selectUnregisteredContact: false } },
-    selectUnregisteredContact: { type: Types.Boolean, label: 'contact person isn\'t registered with MARE', initial: true },
-    unregisteredContact: { type: String, label: 'contact person\'s email', dependsOn: { selectUnregisteredContact: true } },
-    selectFormRange: { type: Types.Boolean, label: 'form valid for specific time range', initial: true },
-    availableFrom: { type: Types.Datetime, label: 'available from', initial: true, dependsOn: { selectFormRange: true } },
-    availableTo: { type: Types.Datetime, label: 'available to', initial: true, dependsOn: { selectFormRange: true } },
-    content: { type: Types.Html, wysiwyg: true, initial: true },
-    graphic: { type: Types.CloudinaryImage, folder: 'forms/', autoCleanup : true }
+	title: { type: String, label: 'form name', required: true, initial: true, index: true },
+	url: { type: Types.Url, noedit: true },
+	contact: { type: Types.Relationship, ref: 'User', label: 'contact person', initial: true, dependsOn: { selectUnregisteredContact: false } },
+	selectUnregisteredContact: { type: Types.Boolean, label: 'contact person isn\'t registered with MARE', initial: true },
+	unregisteredContact: { type: String, label: 'contact person\'s email', dependsOn: { selectUnregisteredContact: true } },
+	selectFormRange: { type: Types.Boolean, label: 'form valid for specific time range', initial: true },
+	availableFrom: { type: Types.Datetime, label: 'available from', initial: true, dependsOn: { selectFormRange: true } },
+	availableTo: { type: Types.Datetime, label: 'available to', initial: true, dependsOn: { selectFormRange: true } },
+	content: { type: Types.Html, wysiwyg: true, initial: true },
+	graphic: { type: Types.CloudinaryImage, folder: 'forms/', autoCleanup : true }
 });
 
 // Pre Save
 Form.schema.pre('save', function(next) {
-    'use strict';
+	'use strict';
 
-    this.url = '/forms/' + this.key;
-    next();
+	this.url = '/forms/' + this.key;
+	next();
 });
 
 // Define default columns in the admin interface and register the model
