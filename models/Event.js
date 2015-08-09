438dd0228e61d73bf4dd9fe7f9b05d802d3d1241
Jared Collier <jared.j.collier@gmail.com>
2015-08-09 08:25:10 -0400

    Update the Event model to include all address fields in the first create screen (This will prevent required fields from stopping Event creation.)  Move the Contact field towards the top to make it more prominent, and change it to appear on Event creation instead of after.

--- models/Event.js
+++ models/Event.js
@@ -11,21 +11,21 @@ var Event = new keystone.List('Event', {
 // Create fields
 Event.add({
 	title: { type: Types.Text, label: 'event name', required: true, initial: true, index: true },
-	url: { type: Types.Url, noedit: true } },
+	url: { type: Types.Url, noedit: true },
+	contact: { type: Types.Relationship, label: 'contact person', ref: 'User', initial: true},
+	graphic: { type: Types.CloudinaryImage, folder: 'events/', autoCleanup : true } },
 
 	{ heading: 'Location' }, {
-		addressLine1: { type: Types.Text, label: 'Address Line 1', required: true, initial: false },
-		addressLine2: { type: Types.Text, label: 'Address Line 2', initial: false },
-		city: { type: Types.Text, label: 'City', required: true, initial: false },
-		state: { type: Types.Text, label: 'State', required: true, initial: false },
-		zip: { type: Types.Number, label: 'Zip Code', required: true, initial: false },
-		country: { type: Types.Text, label: 'Country (This will be a dropdown menu)', required: true, initial: false } },
+		addressLine1: { type: Types.Text, label: 'Address Line 1', required: true, initial: true },
+		addressLine2: { type: Types.Text, label: 'Address Line 2', initial: true },
+		city: { type: Types.Text, label: 'City', required: true, initial: true },
+		state: { type: Types.Text, label: 'State', required: true, initial: true },
+		zip: { type: Types.Number, label: 'Zip Code', required: true, initial: true },
+		country: { type: Types.Text, label: 'Country (This will be a dropdown menu)', required: true, initial: true } },
 	{ heading: 'Details'}, {
 		starts: { type: Types.Datetime, initial: true },
 		ends: { type: Types.Datetime, initial: true },
-		description: { type: Types.Html, wysiwyg: true, initial: true },
-		graphic: { type: Types.CloudinaryImage, folder: 'events/', autoCleanup : true },
-		contact: { type: Types.Relationship, label: 'contact person', ref: 'User'}
+		description: { type: Types.Html, wysiwyg: true, initial: true }
 });
 
 // Pre Save
