5fd3291bd828e26da2dd938ea9d061e824507576
Jared Collier <jared.j.collier@gmail.com>
2015-08-30 20:26:15 -0400

    Add global update to WYSIWYG editor for HTML field types to include more options.

--- keystone.js
+++ keystone.js
@@ -39,6 +39,19 @@ keystone.init({
 	'auth': true,
 	'user model': 'User',
 
+	'wysiwyg override toolbar': false,
+	'wysiwyg menubar': true,
+	'wysiwyg skin': 'lightgray',
+	'wysiwyg additional buttons': 'searchreplace visualchars,'
+	 + ' charmap ltr rtl pagebreak paste, forecolor backcolor,'
+	 +' emoticons media image, preview print ',
+	'wysiwyg additional plugins': 'table, advlist, anchor,'
+	 + ' autolink, autosave, bbcode, charmap, contextmenu, '
+	 + ' directionality, emoticons, fullpage, hr, media, pagebreak,'
+	 + ' paste, preview, print, searchreplace, textcolor,'
+	 + ' visualblocks, visualchars, wordcount',
+	'wysiwyg images': true,
+
 	'cookie secret': process.env.COOKIE_SECRET || 'mare',
 
 	'mandrill api key': process.env.MANDRILL_APIKEY,
