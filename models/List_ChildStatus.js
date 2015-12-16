8a6d017079f09fdaf32cb68efcbcdb06792d1c6a
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:35:26 -0500

    Add Reference list model for Child Statuses.

---
+++ models/List_ChildStatus.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+// Create model. Additional options allow menu name to be used what auto-generating URLs
+var ChildStatus = new keystone.List('Child Status', {
+	autokey: { path: 'key', from: 'childStatus', unique: true },
+	map: { name: 'childStatus' }
+});
+
+// Create fields
+ChildStatus.add({
+	childStatus: { type: String, label: 'Child Status', required: true, index: true, initial: true }
+});
+
+// Define default columns in the admin interface and register the model
+ChildStatus.defaultColumns = 'childStatus';
+ChildStatus.register();
