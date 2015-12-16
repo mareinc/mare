97a8fd718b990dc2a6fef0fb74955d61b5c6e287
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:26:02 -0500

    Create script to automatically create the list for Child Statuses.

---
+++ updates/0.0.1-list_child-status.js
@@ -0,0 +1,15 @@
+exports.create = {
+	'Child Status': [{
+		childStatus: 'active'
+	}, {	
+		childStatus: 'on hold'
+	}, {	
+		childStatus: 'withdrawn'
+	}, {	
+		childStatus: 'placed'
+	}, {	
+		childStatus: 'disrupted'
+	}, {	
+		childStatus: 'reunification'
+	}]
+};
