6c95a0b6b04ed8eca8004fe3f184504f97afddca
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:36:33 -0500

    Add Reference list model for Family Constellations.

---
+++ models/List_FamilyConstellation.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+// Create model. Additional options allow menu name to be used what auto-generating URLs
+var FamilyConstellation = new keystone.List('Family Constellation', {
+	autokey: { path: 'key', from: 'familyConstellation', unique: true },
+	map: { name: 'familyConstellation' }
+});
+
+// Create fields
+FamilyConstellation.add({
+	familyConstellation: { type: String, label: 'Family Constellation', required: true, index: true, initial: true }
+});
+
+// Define default columns in the admin interface and register the model
+FamilyConstellation.defaultColumns = 'familyConstellation';
+FamilyConstellation.register();
