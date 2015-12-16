23be93986069fa3eb81cf0d656bb2865d72a3006
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:38:10 -0500

    Add Reference list model for States.

---
+++ models/List_State.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+// Create model. Additional options allow menu name to be used what auto-generating URLs
+var State = new keystone.List('State', {
+	autokey: { path: 'key', from: 'state', unique: true },
+	map: { name: 'state' }
+});
+
+// Create fields
+State.add({
+	state: { type: String, label: 'State', required: true, index: true, initial: true }
+});
+
+// Define default columns in the admin interface and register the model
+State.defaultColumns = 'state';
+State.register();
