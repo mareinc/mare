66f36740b95825d39247ef0f54d2ae088e9e62d2
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:34:41 -0500

    Rename adoption party Model without the List_ prefix reserved for Reference lists.

--- models/AdoptionParties.js
+++ models/AdoptionParties.js
@@ -2,17 +2,17 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 // Create model. Additional options allow menu name to be used what auto-generating URLs
-var AdoptionParties = new keystone.List('Adoption Parties', {
+var AdoptionParty = new keystone.List('Adoption Party', {
 	autokey: { path: 'key', from: 'title', unique: true },
 	map: { name: 'title' }
 });
 
 // Create fields
-AdoptionParties.add({
+AdoptionParty.add({
 	title: { type: String, label: 'Adoption Party Title', required: true, index: true, initial: true },
 	date: { type: Types.Date, label: 'Event Date', required: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
-AdoptionParties.defaultColumns = 'title, date';
-AdoptionParties.register();
+AdoptionParty.defaultColumns = 'title, date';
+AdoptionParty.register();
