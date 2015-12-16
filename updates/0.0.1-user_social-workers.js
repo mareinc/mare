179dc5289e454d46265fb10c21e6c9265d53a49c
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:23:39 -0500

    Create script to automatically create the core Social Workers.

---
+++ updates/0.0.1-user_social-workers.js
@@ -0,0 +1,25 @@
+exports.create = {
+	'Social Worker': [{
+		permissions: {
+			isVerified: true,
+			isActive: true
+		},
+		name: {
+			first: 'Jonny',
+			last: 'Social'
+		},
+		password: 'JonnySocial',
+		email: 'jonny.social@gmail.com',
+		phone: '(617) 100-2000',
+		address: {
+			street1: '100 Social Street',
+			street2: 'Suite 100',
+			city: 'Boston',
+			state: 'Massachusetts',
+			zipCode: '02110'
+		},
+		position: 'adoption worker',
+		agency: 'ACME Social Workers',
+		title: 'sample social worker'
+	}]
+};
