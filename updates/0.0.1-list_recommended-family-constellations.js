de57019a64d2ced61f3bdb9dd21a973adf8f0a21
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:28:43 -0500

    Create script to automatically create the list for Recommended Family Constellations.

---
+++ updates/0.0.1-list_recommended-family-constellations.js
@@ -0,0 +1,17 @@
+exports.create = {
+	'Recommended Family Constellation': [{
+		familyConstellation: '2-parent (male and female)'
+	}, {	
+		familyConstellation: '2-parent (females)'
+	}, {	
+		familyConstellation: '2-parent (males)'
+	}, {
+		familyConstellation: 'Single Parent (female)'
+	}, {	
+		familyConstellation: 'Single Parent (male)'
+	}, {	
+		familyConstellation: 'Childless Home'
+	}, {	
+		familyConstellation: 'Multi Child Home'
+	}]
+};
