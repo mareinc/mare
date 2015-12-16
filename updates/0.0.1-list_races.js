30758b3540bea44e841632a52d071b66653f2765
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:27:26 -0500

    Create script to automatically create the list for Races.

---
+++ updates/0.0.1-list_races.js
@@ -0,0 +1,35 @@
+exports.create = {
+	'Race': [{
+		race: 'African American'
+	}, {	
+		race: 'African American/Asian'
+	}, {	
+		race: 'African American/Caucasian'
+	}, {	
+		race: 'African American/Hispanic'
+	}, {	
+		race: 'African American/Native American'
+	}, {	
+		race: 'Asian'
+	}, {	
+		race: 'Asian/Caucasian'
+	}, {	
+		race: 'Asian/Hispanic'
+	}, {	
+		race: 'Asian/Native American'
+	}, {	
+		race: 'Caucasian'
+	}, {	
+		race: 'Caucasian/Hispanic'
+	}, {	
+		race: 'Caucasian/Native American'
+	}, {	
+		race: 'Hispanic'
+	}, {	
+		race: 'Hispanic/Native American'
+	}, {	
+		race: 'Native American'
+	}, {	
+		race: 'Other'
+	}]
+};
