a59310a5dc7b69ca79bf621bbd9a2c8b8a1593fa
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:36:12 -0500

    Add Reference list model for Disabilities.

--- models/List_Disabilities.js
+++ models/List_Disabilities.js
@@ -2,16 +2,16 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 // Create model. Additional options allow menu name to be used what auto-generating URLs
-var Disabilities = new keystone.List('Disabilities', {
+var Disability = new keystone.List('Disability', {
 	autokey: { path: 'key', from: 'disability', unique: true },
 	map: { name: 'disability' }
 });
 
 // Create fields
-Disabilities.add({
+Disability.add({
 	disability: { type: String, label: 'Disability', required: true, index: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
-Disabilities.defaultColumns = 'disability';
-Disabilities.register();
+Disability.defaultColumns = 'disability';
+Disability.register();
