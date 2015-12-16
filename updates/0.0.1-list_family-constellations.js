9db433ed305249aded671e09365c886db8bcb7a8
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:26:51 -0500

    Create script to automatically create the list for Family Constellations.

---
+++ updates/0.0.1-list_family-constellations.js
@@ -0,0 +1,19 @@
+exports.create = {
+	'Family Constellation': [{
+		familyConstellation: 'Single Male'
+	}, {	
+		familyConstellation: 'Single Gay Male'
+	}, {	
+		familyConstellation: 'Single Straight Male'
+	}, {	
+		familyConstellation: 'Single Female'
+	}, {	
+		familyConstellation: 'Single Gay Female'
+	}, {	
+		familyConstellation: 'Single Straight Female'
+	}, {	
+		familyConstellation: 'Female/Female Couple'
+	}, {	
+		familyConstellation: 'Male/Male Couple'
+	}]
+};
