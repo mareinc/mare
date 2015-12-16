07c2cb89ca13c0f66fd1bbbc6eacb85c797f4365
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:23:19 -0500

    Create script to automatically create the core Site Users.

---
+++ updates/0.0.1-user_site-users.js
@@ -0,0 +1,27 @@
+exports.create = {
+	'Site User': [{
+		permissions: {
+			isVerified: true,
+			isActive: true
+		},
+		name: {
+			first: 'Jonny',
+			last: 'Site'
+		},
+		password: 'JonnySite',
+		email: 'jonny.site@gmail.com',
+		phone: {
+			work: '(617) 100-3000',
+			workExtension: '1111',
+			mobile: '(617) 200-3000',
+			home: '(617) 300-3000'
+		},
+		address: {
+			street1: '200 Social Street',
+			street2: 'Suite 200',
+			city: 'Boston',
+			state: 'Massachusetts',
+			zipCode: '02110'
+		}
+	}]
+};
