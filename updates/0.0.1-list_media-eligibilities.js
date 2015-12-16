fdb07b00830ebe724ef4322024c898b430a4ebfc
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:28:08 -0500

    Create script to automatically create the list for Media Eligibilities.

---
+++ updates/0.0.1-list_media-eligibilities.js
@@ -0,0 +1,29 @@
+exports.create = {
+	'Media Eligibility': [{
+		mediaEligibility: 'AdoptUSkids'
+	}, {
+		mediaEligibility: 'Annual report'
+	}, {
+		mediaEligibility: 'Facebook'
+	}, {
+		mediaEligibility: 'Heart Gallery'
+	}, {
+		mediaEligibility: 'Other - TEXT BOX'
+	}, {
+		mediaEligibility: 'Spanish print'
+	}, {
+		mediaEligibility: 'Sunday\'s Child'
+	}, {
+		mediaEligibility: 'WWK'
+	}, {
+		mediaEligibility: 'Wednesday\'s Child'
+	}, {
+		mediaEligibility: 'other MARE publications'
+	}, {
+		mediaEligibility: 'other TV'
+	}, {
+		mediaEligibility: 'other media'
+	}, {
+		mediaEligibility: 'other print'
+	}]
+};
