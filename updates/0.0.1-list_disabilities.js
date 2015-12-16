3dd16259093c2fe15016a186f90bbf1f81b58036
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:26:33 -0500

    Create script to automatically create the list for Disabilities.

---
+++ updates/0.0.1-list_disabilities.js
@@ -0,0 +1,17 @@
+exports.create = {
+	'Disability': [{
+		disability: 'Autism'
+	}, {	
+		disability: 'Cerebral Paulsey'
+	}, {	
+		disability: 'Down Syndrome'
+	}, {	
+		disability: 'Fetal Alcohol Syndrome'
+	}, {	
+		disability: 'Hearing Impairment'
+	}, {	
+		disability: 'HIV/AIDS'
+	}, {	
+		disability: 'Visual Impairment'
+	}]
+};
