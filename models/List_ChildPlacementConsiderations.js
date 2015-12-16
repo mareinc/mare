ca4248003bb9906e065d962bf3feea429b8c1c71
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:35:09 -0500

    Add Reference list model for Child Placement Considerations.

--- models/List_ChildPlacementConsiderations.js
+++ models/List_ChildPlacementConsiderations.js
@@ -2,16 +2,16 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 // Create model. Additional options allow menu name to be used what auto-generating URLs
-var ChildPlacementConsiderations = new keystone.List('Child Placement Considerations', {
-	autokey: { path: 'key', from: 'title', unique: true },
-	map: { name: 'title' }
+var ChildPlacementConsideration = new keystone.List('Child Placement Consideration', {
+	autokey: { path: 'key', from: 'childPlacementConsideration', unique: true },
+	map: { name: 'childPlacementConsideration' }
 });
 
 // Create fields
-ChildPlacementConsiderations.add({
-	title: { type: String, label: 'Child Placement Consideration', required: true, index: true, initial: true }
+ChildPlacementConsideration.add({
+	childPlacementConsideration: { type: String, label: 'Child Placement Consideration', required: true, index: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
-ChildPlacementConsiderations.defaultColumns = 'title';
-ChildPlacementConsiderations.register();
+ChildPlacementConsideration.defaultColumns = 'childPlacementConsideration';
+ChildPlacementConsideration.register();
