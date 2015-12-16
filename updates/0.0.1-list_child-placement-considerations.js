a80dedc42127d60ea86fd76414f495e5430a806b
Jared Collier <jared.j.collier@gmail.com>
2015-12-15 22:25:36 -0500

    Create script to automatically create the list for Child Placement Considerations.

---
+++ updates/0.0.1-list_child-placement-considerations.js
@@ -0,0 +1,11 @@
+exports.create = {
+	'Child Placement Consideration': [{
+		childPlacementConsideration: 'Fire Setting'
+	}, {	
+		childPlacementConsideration: 'Hurts Animals'
+	}, {	
+		childPlacementConsideration: 'Self Harm'
+	}, {	
+		childPlacementConsideration: 'Sexual Abuse'
+	}]
+};
