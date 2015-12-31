c9aab22045de28b382f862367079f715da7332f7
Tom Koch <tommysalsa@gmail.com>
2015-12-31 16:41:57 -0500

    Registration Form: adds homestudy submission section, builds race form fields dynamically from the database.

--- routes/views/register.js
+++ routes/views/register.js
@@ -1,10 +1,22 @@
 var keystone = require('keystone');
 
+var Race = keystone.list('Race');
+
 exports = module.exports = function(req, res) {
     'use strict';
   
     var view = new keystone.View(req, res),
         locals = res.locals;
 
-    view.render('register');
+        // Use the menu ID to find all page references it contains
+    Race.model.find()
+        .exec()
+        .then(function (results) {
+            
+            locals.race = results;
+
+            view.render('register');
+        });
+
+    //view.render('register');
 };
