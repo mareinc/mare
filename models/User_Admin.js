a63db03cd00b03da0a95cf1ac5c712c2723c04ec
Jared Collier <jared.j.collier@gmail.com>
2016-01-04 22:29:55 -0500

    Update the Admin model with notes outlining how it's different from the Admin table in the old system.

--- models/User_Admin.js
+++ models/User_Admin.js
@@ -1,3 +1,25 @@
+/* Fields in old system, missing from the new one
+
+initials
+login_name
+scp_id (security profile ID)
+
+   Fields in new system, missing from the old one
+
+isVerified
+isActive
+avatar
+phone.work
+phone.mobile
+phone.home
+address.street1
+address.street2
+address.city
+address.state
+address.zipCode
+
+   End missing fields */
+
 var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
@@ -38,11 +60,11 @@ User.add('Permissions', {
 		home: { type: Types.Text, label: 'Home phone number', initial: true }
     },
 
-    address: {
-	    address1: { type: Types.Text, label: 'Street', initial: true },
-		address2: { type: Types.Text, label: '', initial: true },
+	address: {
+	    street1: { type: Types.Text, label: 'Address Line 1', initial: true },
+		street2: { type: Types.Text, label: 'Address Line 2', initial: true },
 		city: { type: Types.Text, label: 'City', initial: true },
-		state: { type: Types.Select, options: 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming', label: 'State', index: true, initial: true },
+		state: { type: Types.Relationship, label: 'State', ref: 'State', index: true, initial: true },
 		zipCode: { type: Types.Text, label: 'Zip code', index: true, initial: true }
 	}
 
