e5fadc3f7a95514fc3b94c5fc34fe5e57c3d6cfc
Tom Koch <tommysalsa@gmail.com>
2016-02-07 15:52:52 -0500

    Adds adoption party registration form, first attempt.

--- routes/views/form.js
+++ routes/views/form.js
@@ -1,15 +1,54 @@
 var keystone 	= require('keystone'),
+    async = require('async'),
+    Race = keystone.list('Race'),
+    State = keystone.list('State'),
+    Gender = keystone.list('Gender'),
 	Form 		= keystone.list('Form');
 
 exports = module.exports = function(req, res) {
     'use strict';
     
     var view 	= new keystone.View(req, res),
-    	locals 	= res.locals;
+    	locals 	= res.locals,
+    	url 	= req.originalUrl.replace("/form/", "");
+
+    async.parallel([
+        function(done) {
+            State.model.find().select('state').exec().then(function(states) {
+                locals.states = states;
+                done();
+            })},
+        function(done) {
+            Race.model.find().select('race').exec().then(function(races) {
+                locals.races = races;
+                done();
+            })},
+        function(done) {
+            Gender.model.find().select('gender').exec().then(function(genders) {
+                locals.genders = genders;
+                done();
+            })}
+    ], function() {
+        
+        	switch(url) {
+
+			    case "adoption-party-family-registration-form":
+			        view.render('forms/adoption-party.hbs');
+			        break;
+
+			    case "adoption-party-family-registration-form2":
+			        view.render('forms/adoption-party.hbs');
+			        break;
+
+			    default:
+			        view.render('form');
+			}
+    });
 
     // Fetch the page with the matching URL
     // If it exists, pass the object into the rendering
     // TODO: If it doesn't exist, forward to a 404 page
+    /*
     Form.model.find()
 		.where('url', req.originalUrl)
 		.exec()
@@ -20,5 +59,8 @@ exports = module.exports = function(req, res) {
 			// Render the view
     		view.render('form');
 
-		});    
+		});
+	*/
+
+
 };
