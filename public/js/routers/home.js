3db1061ae86734aa50d4af4427f0347927dacfcf
Jared Collier <jared.j.collier@gmail.com>
2016-03-06 10:00:59 -0500

    Add home router.

---
+++ public/js/routers/home.js
@@ -0,0 +1,17 @@
+(function () {
+	'use strict';
+
+	mare.routers.Home = Backbone.Router.extend({
+
+		routes: {
+			'.*' : 'loadHomePage',
+		},
+
+		loadHomePage: function() {
+			// Load the view for the home page
+			mare.views.home = mare.views.home || new mare.views.Home();
+		}
+
+	});
+
+})();
