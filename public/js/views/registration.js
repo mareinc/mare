e95afb331908be83fe5eea4b7eb85e85d5c05771
Jared Collier <jared.j.collier@gmail.com>
2016-03-06 11:25:55 -0500

    Remove routing features from general registration view and create functions for hiding and showing the different forms.

--- public/js/views/registration.js
+++ public/js/views/registration.js
@@ -9,15 +9,11 @@
 		},
 
 		initialize: function() {
-			// Initialize the views for the three registration forms
-			mare.views.siteVisitorRegistration = mare.views.siteVisitorRegistration || new mare.views.SiteVisitorRegistration();
-			mare.views.socialWorkerRegistration = mare.views.socialWorkerRegistration || new mare.views.SocialWorkerRegistration();
-			mare.views.familyRegistration = mare.views.familyRegistration || new mare.views.FamilyRegistration();
 			// DOM cache any commonly used elements to improve performance
-			this.$formSelector = $('.registration-type-selector');
-			this.$siteForm = $('.site-visitor-registration');
-			this.$socialWorkerForm = $('.social-worker-registration');
-			this.$prospectiveParentForm = $('.prospective-parent-registration');
+			this.$formSelector			= $( '.registration-type-selector' );
+			this.$siteForm				= $( '.site-visitor-registration' );
+			this.$socialWorkerForm		= $( '.social-worker-registration' );
+			this.$prospectiveParentForm = $( '.prospective-parent-registration' );
 
 			this.currentForm = 'siteVisitor';
 		},
@@ -26,10 +22,29 @@
 			this.currentForm = this.$formSelector.val();
 
 			switch(this.currentForm) {
-				case 'siteVisitor': this.$socialWorkerForm.hide(); this.$prospectiveParentForm.hide(); this.$siteForm.show(); break;
-				case 'socialWorker': this.$siteForm.hide(); this.$prospectiveParentForm.hide(); this.$socialWorkerForm.show(); break;
-				case 'prospectiveParent': this.$siteForm.hide(); this.$socialWorkerForm.hide(); this.$prospectiveParentForm.show(); break;
+				case 'siteVisitor'			: mare.routers.registration.navigate( 'site-user', { trigger: true } ); break;
+				case 'socialWorker'			: mare.routers.registration.navigate( 'social-worker', { trigger: true } ); break;
+				case 'prospectiveParent'	: mare.routers.registration.navigate( 'family', { trigger: true } );
+				default						: mare.routers.registration.navigate( 'site-user', { trigger: true, replace: true } );
 			}
+		},
+
+		showSiteUserForm: function showSiteUserForm() {
+			this.$socialWorkerForm.hide();
+			this.$prospectiveParentForm.hide();
+			this.$siteForm.show();
+		},
+
+		showSocialWorkerForm: function showSocialWorkerForm() {
+			this.$siteForm.hide();
+			this.$prospectiveParentForm.hide();
+			this.$socialWorkerForm.show();
+		},
+
+		showFamilyForm: function showFamilyForm() {
+			this.$siteForm.hide();
+			this.$socialWorkerForm.hide();
+			this.$prospectiveParentForm.show();
 		}
 
 	});
