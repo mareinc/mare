cc8a2c805df147baf8afbbf68625f4c1b53327e2
Jared Collier <jared.j.collier@gmail.com>
2016-03-06 10:01:15 -0500

    Add donations router.

---
+++ public/js/routers/donations.js
@@ -0,0 +1,17 @@
+(function () {
+	'use strict';
+
+	mare.routers.Donations = Backbone.Router.extend({
+
+		routes: {
+			'.*' : 'loadDonationsPage',
+		},
+
+		loadDonationsPage: function() {
+			// Load the view for the donations page
+			mare.views.donations = mare.views.donations || new mare.views.Donations();
+		}
+
+	});
+
+})();
