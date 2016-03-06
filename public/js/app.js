d0b9bde8fe3f762a7c2087e4a43d35fd5000319a
Jared Collier <jared.j.collier@gmail.com>
2016-03-06 09:57:26 -0500

    Update app.js with util functions and core structure
    app.js is designed to store the mare namespace and initialize any client side config variables as well as all utility functions.

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,56 @@
 (function() {
     'use strict';
 
-    window.app = {
-		utils: {},
-		settings: {},
-		url: {},
-		config: {
+    window.mare = {
+        models:{},
+        collections:{},
+        views:{},
+        routers:{},
+        settings: {},
+        url: {},
+        config: {
 			STRIPE_TEST_KEY: 'pk_test_wxuno1w3EmQ14Q6EPar6d59w',
 			STRIPE_KEY: 'pk_live_VJ7k6spQvGiizCPLUt4NHwzG'
+		},
+        utils: {
+
+        	storeUrlInfo: function() {
+				var host		= window.location.host;
+				var pathName	=  window.location.pathname.substr(1); // get the pathname without the leading '/'
+				var pathArray	= pathName.split( '/' );
+
+				// Store relevant url information in mare namespace
+				mare.url.protocol	= window.location.protocol;
+				mare.url.siteArea	= pathArray[0];
+				mare.url.page		= pathArray[1];
+        	},
+
+			bindTouch: function() {
+				// Bind adding of 'hover' class to tap vs hover depending on whether it is a touch enabled device
+				if (Modernizr.touch) {
+					$(".nav li").unbind('mouseenter mouseleave');
+					$(".nav li a.parent").unbind('click');
+					$(".nav li .more").unbind('click').bind('click', function() {
+						$(this).parent("li").toggleClass("hover");
+					});
+
+				} else {
+					$(".nav li").removeClass("hover");
+					$(".nav li a").unbind('click');
+					$(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+						// must be attached to li so that mouseleave is not triggered when hovering over a submenu
+						$(this).toggleClass('hover');
+					});
+				}
+			},
+
+			enablePageScrolling: function() {
+				$('html, body').removeClass('scrolling-disabled');
+			},
+
+			disablePageScrolling: function() {
+				$('html, body').addClass('scrolling-disabled');
+			}
 		}
 	};
 })();
