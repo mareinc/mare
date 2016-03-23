44d59f05d01232b9bbddd57848d38225b1354289
Jared Collier <jared.j.collier@gmail.com>
2016-03-22 21:37:25 -0400

    Update the Mailing List model with relationship fields to select users that have signed up.

--- models/MailingList.js
+++ models/MailingList.js
@@ -9,10 +9,11 @@ var MailingList = new keystone.List('Mailing List', {
 
 // Create fields
 MailingList.add({
-	mailingList: { type: Types.Text, label: 'mailing list', required: true, index: true, initial: true }
+	mailingList: { type: Types.Text, label: 'mailing list', required: true, index: true, initial: true },
+	siteUserAttendees: { type: Types.Relationship, label: 'site users', ref: 'Site User', many: true, initial: true },
+	socialWorkerAttendees: { type: Types.Relationship, label: 'social workers', ref: 'Social Worker', many: true, initial: true },
+	prospectiveParentOrFamilyAttendees: { type: Types.Relationship, label: 'prospective parents or families', ref: 'Prospective Parent or Family', many: true, initial: true }
 });
-
-MailingList.relationship({ path: 'mailing-lists', ref: 'Prospective Parent or Family', refPath: 'prospectiveParentOrFamily' });
 
 // Define default columns in the admin interface and register the model
 MailingList.defaultColumns = 'mailingList';
