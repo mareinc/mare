3856086c56d4adfc1c2937f3e59477b3cedf56ba
Jared Collier <jared.j.collier@gmail.com>
2016-03-22 21:36:06 -0400

    Add lots of new form validation to the Family registration form.

--- public/js/views/registration-family.js
+++ public/js/views/registration-family.js
@@ -13,14 +13,25 @@
 
 		initialize: function() {
 			// DOM cache any commonly used elements to improve performance
-			this.$howDidYouHearOther			= this.$('#family-how-did-you-hear-other');
 			this.$state							= this.$('#family-state');
+			this.$homestudyCompletionDate		= this.$('#homestudy-date-complete');
+			this.$socialWorkerName				= this.$('#social-worker-name');
+			this.$socialWorkerAgency			= this.$('#social-worker-agency');
+			this.$socialWorkerPhone				= this.$('#social-worker-phone');
+			this.$socialWorkerEmail				= this.$('#social-worker-email');
 			this.$homestudySection				= this.$('.family-submit-your-homestudy-section');
 			this.$homestudySubmissionSection	= this.$('.family-homestudy-details-section');
+			this.$howDidYouHearOther			= this.$('#family-how-did-you-hear-other');
 			// Initialize parsley validation on the form
 			this.form = this.$el.parsley();
 			// Bind the hidden 'other' text box for use in binding/unbinding validation
 			this.howDidYouHearOtherValidator = this.$howDidYouHearOther.parsley();
+			// Bind the hidden homestudy text boxes for use in binding/unbinding validation
+			this.homestudyCompletionDateValidator 	= this.$homestudyCompletionDate.parsley();
+			this.socialWorkerNameValidator 			= this.$socialWorkerName.parsley();
+			this.socialWorkerAgencyValidator 		= this.$socialWorkerAgency.parsley();
+			this.socialWorkerPhoneValidator 		= this.$socialWorkerPhone.parsley();
+			this.socialWorkerEmailValidator 		= this.$socialWorkerEmail.parsley();
 			// DOM cache the Parsley validation message for the hidden 'other' field for use in binding/unbinding validation
 			this.$howDidYouHearOtherErrorMessage = this.$howDidYouHearOther.next();
 
@@ -28,7 +39,53 @@
 		},
 
 		toggleOtherWayToHearTextField: function toggleOtherWayToHearTextField() {
+			// Hide/show the hidden 'other' field via the hidden class
 			this.$howDidYouHearOther.toggleClass('hidden');
+
+			if(this.$howDidYouHearOther.hasClass('hidden')) {
+				// Clear out the input box since it's hidden and not part of the form submission
+				this.$howDidYouHearOther.val('');
+				// Remove the validation binding
+				this.$howDidYouHearOther.attr('data-parsley-required', 'false');
+				// Reset validation on the field.  If it was already validated, we need to clear out the check so the form can be submitted
+				this.howDidYouHearOtherValidator.reset();
+			} else {
+				// Add validation binding
+				this.$howDidYouHearOther.attr('data-parsley-required', 'true');
+			}
+		},
+
+		toggleHomestudySection: function toggleHomestudySection() {
+			// Hide/show the hidden homestudy section via the hidden class
+			this.$homestudySubmissionSection.toggleClass('hidden');
+
+			if(this.$homestudySubmissionSection.hasClass('hidden')) {
+				// Clear out the homestudy input fields since the section is hidden and not part of the form submission
+				this.$homestudyCompletionDate.val('');
+				this.$socialWorkerName.val('');
+				this.$socialWorkerAgency.val('');
+				this.$socialWorkerPhone.val('');
+				this.$socialWorkerEmail.val('');
+				// Remove validation bindings
+				this.$homestudyCompletionDate.attr('data-parsley-required', 'false');
+				this.$socialWorkerName.attr('data-parsley-required', 'false');
+				this.$socialWorkerAgency.attr('data-parsley-required', 'false');
+				this.$socialWorkerPhone.attr('data-parsley-required', 'false');
+				this.$socialWorkerEmail.attr('data-parsley-required', 'false');
+				// Reset validation on the fields.  If they were already validated, we need to clear out the checks so the form can be submitted
+				this.homestudyCompletionDateValidator.reset();
+				this.socialWorkerNameValidator.reset();
+				this.socialWorkerAgencyValidator.reset();
+				this.socialWorkerPhoneValidator.reset();
+				this.socialWorkerEmailValidator.reset();
+			} else {
+				// Add validation binding
+				this.$homestudyCompletionDate.attr('data-parsley-required', 'true');
+				this.$socialWorkerName.attr('data-parsley-required', 'true');
+				this.$socialWorkerAgency.attr('data-parsley-required', 'true');
+				this.$socialWorkerPhone.attr('data-parsley-required', 'true');
+				this.$socialWorkerEmail.attr('data-parsley-required', 'true');
+			}
 		},
 
 		toggleHomestudySubmission: function toggleHomestudySubmission() {
@@ -46,14 +103,11 @@
 			}
 		},
 
-		toggleHomestudySection: function toggleHomestudySection() {
-			this.$homestudySubmissionSection.toggleClass('hidden');
-		},
-
 		uploadForm: function uploadForm(event) {
+			// Get the full path to the file and trim everything up to and including the last slash to give us just the file name
 			var filepath = event.target.value;
 			var filename = filepath.substr( filepath.lastIndexOf("\\") + 1 );
-
+			// Show the file name to the user as a point of reference after they've selected the file they wish to upload
 			this.$(".homestudy-file-upload-text").html(filename);
 		},
 
