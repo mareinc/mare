3cb744f7c92838f1d2961655c99ec7c7210aeb94
Jared Collier <jared.j.collier@gmail.com>
2016-04-03 01:05:04 -0400

    Update base user model for inheritence.

--- models/User.js
+++ models/User.js
@@ -24,16 +24,6 @@ User.add('Permissions', {
 
 });
 
-/* TODO: VERY IMPORTANT:  Need to fix this to provide the link to access the keystone admin panel again */
-/* 						  Changing names or reworking this file changed the check in node_modules/keystone/templates/views/signin.jade
-/*						  for user.isAdmin on line 14 */
-// Provide access to Keystone
-User.schema.virtual('canAccessKeystone').get(function() {
-	'use strict';
-
-	return true;
-});
-
 // Define default columns in the admin interface and register the model
-User.defaultColumns = 'name.full, email, isActive, isVerified';
+User.defaultColumns = 'email, permissions.isActive, permissions.isVerified';
 User.register();
