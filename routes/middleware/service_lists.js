9aa94b7653d162cb57a042dbafcd5cb44a022383
Jared Collier <jared.j.collier@gmail.com>
2016-04-04 23:55:08 -0400

    services - add services for list types
    1.  Add service to fetch all regions.
    2.  Add service to fetch all social worker positions.
    3.  Add service to fetch all races.
    4.  Add service to fetch all states.
    5.  Add service to fetch all genders.
    6.  Add service to fetch all legal statuses.
    7.  Add service to fetch all family constellations.
    8.  Add service to fetch all languages.
    9.  Add service to fetch all family constellations.
    10. Add service to fetch all disabilities.
    11. Add service to fetch other considerations.
    12. Add service to fetch all ways to hear about MARE.

---
+++ routes/middleware/service_lists.js
@@ -0,0 +1,256 @@
+var keystone				= require('keystone'),
+	_						= require('underscore'),
+	Region 					= keystone.list('Region'),
+	Position				= keystone.list('Social Worker Position'),
+	Race					= keystone.list('Race'),
+	State					= keystone.list('State'),
+	Gender					= keystone.list('Gender'),
+	LegalStatus				= keystone.list('Legal Status'),
+	Language				= keystone.list('Language'),
+	FamilyConstellation		= keystone.list('Family Constellation'),
+	Disability				= keystone.list('Disability'),
+	OtherConsideration		= keystone.list('Other Consideration'),
+	WayToHearAboutMARE		= keystone.list('Way To Hear About MARE');
+
+exports.getAllRegions = function getAllRegions(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	Region.model.find()
+				.exec()
+				.then(function (regions) {
+
+					locals.regions = regions;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllSocialWorkerPositions = function getAllSocialWorkerPositions(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	Position.model.find()
+				.exec()
+				.then(function (positions) {
+
+					locals.positions = positions;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllRaces = function getAllRaces(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	Race.model.find()
+				.exec()
+				.then(function (races) {
+
+					locals.races = races;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllStates = function getAllStates(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	State.model.find()
+				.exec()
+				.then(function (states) {
+
+					_.each(states, function(state) {
+						if(state.state === 'Massachusetts') {
+							state.defaultSelection = true;
+						}
+					});
+
+					locals.states = states;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllGenders = function getAllGenders(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	Gender.model.find()
+				.exec()
+				.then(function (genders) {
+
+					locals.genders = genders;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllLegalStatuses = function getAllLegalStatuses(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	LegalStatus.model.find()
+				.exec()
+				.then(function (legalStatuses) {
+
+					locals.legalStatuses = legalStatuses;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllLanguages = function getAllLanguages(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	Language.model.find()
+				.exec()
+				.then(function (languages) {
+
+					locals.languages = languages;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllFamilyConstellations = function getAllFamilyConstellations(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	FamilyConstellation.model.find()
+				.exec()
+				.then(function (familyConstellations) {
+
+					locals.familyConstellations = familyConstellations;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllDisabilities = function getAllDisabilities(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	Disability.model.find()
+				.exec()
+				.then(function (disabilities) {
+
+					locals.disabilities = disabilities;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getOtherConsiderations = function getOtherConsiderations(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	OtherConsideration.model.find()
+				.exec()
+				.then(function (otherConsiderations) {
+
+					locals.otherConsiderations = otherConsiderations;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getAllWaysToHearAboutMARE = function getAllWaysToHearAboutMARE(req, res, done) {
+
+	req.locals = res.locals || {};
+	var locals = res.locals;
+
+	WayToHearAboutMARE.model.find()
+				.exec()
+				.then(function (waysToHearAboutMARE) {
+
+					_.each(waysToHearAboutMARE, function(wayToHearAboutMARE) {
+						if(wayToHearAboutMARE.wayToHearAboutMARE === 'other') {
+							wayToHearAboutMARE.other = true;
+						}
+					});
+
+					locals.waysToHearAboutMARE = waysToHearAboutMARE;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
