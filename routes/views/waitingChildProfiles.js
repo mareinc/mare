73c7fe26e24bbe1e6dd403353eb74c4b78f9bcba
Jared Collier <jared.j.collier@gmail.com>
2016-04-09 18:12:30 -0400

    gallery page - restrict access to children based on user type
    1.  Add checks to look at the user type and only show children that should be available to them.

--- routes/views/waitingChildProfiles.js
+++ routes/views/waitingChildProfiles.js
@@ -1,20 +1,27 @@
 var keystone		= require('keystone'),
 	async			= require('async'),
-	User			= keystone.list('User'),
-	Child			= keystone.list('Child');
 	childService	= require('../middleware/service_child'),
 	userService		= require('../middleware/service_user');
 
 exports = module.exports = function(req, res) {
 	'use strict';
 
-	var view	= new keystone.View(req, res),
-		locals	= res.locals,
-		userId	= req.user.get('_id');
+	var view			= new keystone.View(req, res),
+		locals			= res.locals;
 
-	async.parallel([
-		function(done) { childService.getAllChildren(req, res, done); },
-		function(done) { userService.getUserById(req, res, done, userId); }
+	// Set local variables
+	locals.userType	= req.user ? req.user.get('userType') : 'anonymous';
+
+	async.series([
+		function(done) {
+
+			if(locals.userType === 'anonymous' || locals.userType === 'site visitor') {
+				childService.getUnrestrictedChildren(req, res, done);
+			} else {
+				childService.getAllChildren(req, res, done);
+			}
+		}
+
 	], function() {
 
 		view.render('waitingChildProfiles');
