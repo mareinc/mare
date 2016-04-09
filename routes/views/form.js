39342b8d7715a290a1e4082b0bf45c1d96cfcbf1
Jared Collier <jared.j.collier@gmail.com>
2016-04-09 16:45:20 -0400

    forms - incorporate services
    1.  Incorporate service calls that fetch data to clean up the forms view.
    2.  Clean up code that isn't in use anymore.

--- routes/views/form.js
+++ routes/views/form.js
@@ -1,102 +1,55 @@
-var keystone 	= require('keystone'),
-    async = require('async'),
-    Race = keystone.list('Race'),
-    State = keystone.list('State'),
-    Gender = keystone.list('Gender'),
-    WayToHearAboutMARE  = keystone.list('Way To Hear About MARE'),
-	Form 		= keystone.list('Form'),
-    _ = require('underscore');
+// TODO: break these each into their own routes, combining all these forms is going to be a mess once we start processing their expected actions
+var keystone					= require('keystone'),
+	async						= require('async'),
+	_							= require('underscore'),
+	listsService				= require('../middleware/service_lists'),
+	Form						= keystone.list('Form');
 
 exports = module.exports = function(req, res) {
-    'use strict';
-    
-    var view 	= new keystone.View(req, res),
-    	locals 	= res.locals,
-    	url 	= req.originalUrl.replace("/form/", "");
+	'use strict';
 
-    async.parallel([
-        function(done) {
-            State.model.find().select('state').exec().then(function(states) {
-                locals.states = states;
-                done();
-            })},
-        function(done) {
-            Race.model.find().select('race').exec().then(function(races) {
-                _.each(races, function(race) {
-                    if(race.race === 'other') {
-                        race.other = true;
-                    }
-                });
-                locals.races = races;
-                done();
-            })},
-        function(done) {
-            Gender.model.find().select('gender').exec().then(function(genders) {
-                locals.genders = genders;
-                done();
-            })},
-        function(done) {
-            WayToHearAboutMARE.model.find().select('wayToHearAboutMARE').exec().then(function(waysToHearAboutMARE) {
+	var view 	= new keystone.View(req, res),
+		locals 	= res.locals,
+		url 	= req.originalUrl.replace("/form/", "");
 
-                _.each(waysToHearAboutMARE, function(wayToHearAboutMARE) {
-                    if(wayToHearAboutMARE.wayToHearAboutMARE === 'other') {
-                        wayToHearAboutMARE.other = true;
-                    }
-                });
+	// objects with additional search parameters
+	var raceOptions			= { other: true },
+		waysToHearOptions	= { other: true };
 
-                locals.waysToHearAboutMARE = waysToHearAboutMARE;
-                done();
-            })}
-    ], function() {
-        
-        	switch(url) {
+	async.parallel([
+		function(done) { listsService.getAllStates(req, res, done) },
+		function(done) { listsService.getAllRaces(req, res, done, raceOptions) },
+		function(done) { listsService.getAllGenders(req, res, done) },
+		function(done) { listsService.getAllWaysToHearAboutMARE(req, res, done, waysToHearOptions) }
+	], function() {
 
-			    case "adoption-party-family-registration-form":
-			        view.render('forms/adoption-party.hbs');
-			        break;
+			switch(url) {
 
-                case "adoption-party-social-worker-registration-form":
-                //The URL in there now is misspelled... this case catches it. registr(ai)ton.
+				case "adoption-party-family-registration-form":
+					view.render('forms/adoption-party.hbs');
+					break;
 
-			    case "adoption-party-social-worker-registraiton-form":
-			        view.render('forms/adoption-party-social-worker.hbs');
-			        break;
+				case "adoption-party-social-worker-registration-form":
+					view.render('forms/adoption-party-social-worker.hbs');
+					break;
 
-                case "agency-event-submission-form":
-                    view.render('forms/agency-event-submission.hbs');
-                    break;
+				case "agency-event-submission-form":
+					view.render('forms/agency-event-submission.hbs');
+					break;
 
-                case "car-donation-form":
-                    view.render('forms/car-donation.hbs');
-                    break;
+				case "car-donation-form":
+					view.render('forms/car-donation.hbs');
+					break;
 
-                case "child-registration-form":
-                    view.render('forms/child-registration.hbs');
-                    break;
+				case "child-registration-form":
+					view.render('forms/child-registration.hbs');
+					break;
 
-                 case "information-request-form":
-                    view.render('forms/information-request-form.hbs');
-                    break;
-
-			    default:
-
-                    // Fetch the page with the matching URL
-                    // If it exists, pass the object into the rendering
-                    // TODO: If it doesn't exist, forward to a 404 page
-                    
-                    Form.model.find()
-                        .where('url', req.originalUrl)
-                        .exec()
-                        .then(function (targetForm) {
-                            
-                            locals.targetForm = targetForm[0];
-
-                            // Render the view
-                            view.render('form');
-
-                        });
-                    
-			        //view.render('form');
+				 case "information-request-form":
+					view.render('forms/information-request-form.hbs');
+					break;
+				// TODO: Create a custom 404 page
+				default: view.render('404');
 			}
-    });
+	});
 };
