699b8524d200893dedb8b2de6585334b0185ea44
Jared Collier <jared.j.collier@gmail.com>
2016-04-09 18:10:21 -0400

    child services - add function for fetching unrestricted children
    1.  Add service function to fetch children who are available to everyone.  This will allow us to hide children who should only be seen by registered families/social workers from certain users.

--- routes/middleware/service_child.js
+++ routes/middleware/service_child.js
@@ -3,7 +3,6 @@ var keystone	= require('keystone'),
 
 exports.getAllChildren = function getAllChildren(req, res, done) {
 
-	req.locals = res.locals || {};
 	var locals = res.locals;
 
 	Child.model.find()
@@ -17,7 +16,31 @@ exports.getAllChildren = function getAllChildren(req, res, done) {
 				}, function(err) {
 
 					console.log(err);
+					// execute done function if async is used to continue the flow of execution
 					done();
 
 				});
+}
+
+exports.getUnrestrictedChildren = function getUnrestrictedChildren(req, res, done) {
+
+	var locals = res.locals;
+
+	Child.model.find()
+				.where('siteVisibility', 'everyone')
+				.exec()
+				.then(function (children) {
+
+					locals.children = children;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					// execute done function if async is used to continue the flow of execution
+					done();
+
+				});
+
 }
