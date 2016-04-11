7ad9f18eca12f87b2d161441f5cc73ae6966cc6c
Jared Collier <jared.j.collier@gmail.com>
2016-04-10 21:25:09 -0400

    gallery page - new functions and data manipulation in child services
    1.  Derive data from each child model and each to the model for use during server side templating.
    2.  Ensure only active children are considered for showing in the gallery.
    3.  Add function to send children data to the gallery page once the view for that page is initialized.

--- routes/middleware/service_child.js
+++ routes/middleware/service_child.js
@@ -1,5 +1,7 @@
 var keystone	= require('keystone'),
 	_			= require('underscore'),
+	async		= require('async'),
+	middleware	= require('./middleware'),
 	Child		= keystone.list('Child');
 
 exports.getAllChildren = function getAllChildren(req, res, done) {
@@ -8,15 +10,25 @@ exports.getAllChildren = function getAllChildren(req, res, done) {
 
 	Child.model.find()
 				.populate('gender')
+				.populate('status')
 				.populate('legalStatus')
 				.exec()
 				.then(function (children) {
+					// Filter out all children who don't have a status of 'active'
+					children = _.filter(children, function(child) {
+						return child.status.childStatus === 'active'
+					});
 
-					locals.children = children;
 					_.each(children, function(child) {
 						// adjust the image to the blank male/female image if needed
 						exports.setNoChildImage(req, res, child);
+						// set extra information needed for rendering the child to the page
+						child.age						= middleware.getAge(child.birthDate);
+			    		child.ageConverted				= middleware.convertDate(child.birthDate);
+			    		child.registrationDateConverted	= middleware.convertDate(child.registrationDate);
 					});
+
+					locals.children = children;
 					// execute done function if async is used to continue the flow of execution
 					done()
 
@@ -27,7 +39,7 @@ exports.getAllChildren = function getAllChildren(req, res, done) {
 					done();
 
 				});
-}
+};
 
 exports.getUnrestrictedChildren = function getUnrestrictedChildren(req, res, done) {
 
@@ -36,16 +48,25 @@ exports.getUnrestrictedChildren = function getUnrestrictedChildren(req, res, don
 	Child.model.find()
 				.where('siteVisibility', 'everyone')
 				.populate('gender')
+				.populate('status')
 				.populate('legalStatus')
 				.exec()
 				.then(function (children) {
-
-					locals.children = children;
+					// Filter out all children who don't have a status of 'active'
+					children = _.filter(children, function(child) {
+						return child.status.childStatus === 'active'
+					});
 
 					_.each(children, function(child) {
 						// adjust the image to the blank male/female image if needed
-						exports.setChildImage(req, res, child);
+						exports.setNoChildImage(req, res, child);
+						// set extra information needed for rendering the child to the page
+						child.age						= middleware.getAge(child.birthDate);
+			    		child.ageConverted				= middleware.convertDate(child.birthDate);
+			    		child.registrationDateConverted	= middleware.convertDate(child.registrationDate);
 					});
+
+					locals.children = children;
 					// execute done function if async is used to continue the flow of execution
 					done()
 
@@ -56,8 +77,7 @@ exports.getUnrestrictedChildren = function getUnrestrictedChildren(req, res, don
 					done();
 
 				});
-
-}
+};
 
 /* Sets the images for display in the gallery to a blank male/female face in the following cases:
  *	1. No image was uploaded for the child
@@ -84,5 +104,55 @@ exports.setNoChildImage = function setNoChildImage(req, res, child) {
 
 		}
 	}
+};
 
-}
+/* Expose the child data to the front-end via an API call */
+exports.sendChildrenData = function sendChildrenData(req, res, next) {
+
+	var locals = res.locals;
+	// Set local variables
+	locals.publicChildrenData = [];
+	locals.userType	= req.user ? req.user.get('userType') : 'anonymous';
+	locals.targetChildren = locals.userType === 'anonymous' || locals.userType === 'site visitor' ? 'unrestricted' : 'all';
+
+
+	async.series([
+		function(done) {
+
+			if(locals.targetChildren === 'all') {
+				exports.getAllChildren(req, res, done);
+			} else {
+				exports.getUnrestrictedChildren(req, res, done);
+			}
+
+		}
+
+	], function() {
+		// Full child records have been fetched and stored on res.locals
+		_.each(locals.children, function(child) {
+
+			var relevantData = {
+	    		name						: child.name.first,
+	    		age							: middleware.getAge(child.birthDate),
+	    		ageConverted				: middleware.convertDate(child.birthDate),
+	    		registrationDateConverted	: middleware.convertDate(child.registrationDate),
+	    		gender						: child.gender.gender,
+	    		registrationNumber			: child.registrationNumber,
+	    		profilePart1				: child.profile.part1,
+	    		profilePart2				: child.profile.part2,
+	    		profilePart3				: child.profile.part3,
+	    		detailImage					: child.detailImage,
+	    		hasImage					: child.image.url && child.image.url.length > 0 ? true : false,
+	    		hasVideo					: child.video && child.video.length > 0 ? true : false,
+	    		video						: child.video && child.video.length > 0 ? child.video.replace('watch?v=', 'embed/') : '',
+	    		wednesdaysChild				: child.wednesdaysChild
+	    	};
+
+	    	locals.publicChildrenData.push(relevantData);
+
+		});
+
+		res.send(locals.publicChildrenData);
+
+	});
+};
