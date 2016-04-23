58aa7b9c71ea773e8b59c7e3dcb0f0c81741bcad
Jared Collier <jared.j.collier@gmail.com>
2016-04-23 18:00:02 -0400

    registration page - update child type content creation scripts
    1.  Updat the content creation script for child types to include the new fields availableOnWebsite and displayName.

--- updates/0.0.1-list_child-types.js
+++ updates/0.0.1-list_child-types.js
@@ -1,15 +1,23 @@
 exports.create = {
 	'Child Type': [{
-		childType: 'biological'
-	}, {	
-		childType: 'adopted - domestic'
-	}, {	
-		childType: 'adopted - international'
-	}, {	
-		childType: 'adopted - foster care'
-	}, {	
-		childType: 'other'
-	}, {	
-		childType: 'unknown'
+		childType			: 'biological',
+		availableOnWebsite	: true,
+		displayName			: 'Biological'
+	}, {
+		childType			: 'adopted - domestic',
+		availableOnWebsite	: false
+	}, {
+		childType			: 'adopted - international',
+		availableOnWebsite	: false
+	}, {
+		childType			: 'adopted - foster care',
+		availableOnWebsite	: true,
+		displayName			: 'Adopted'
+	}, {
+		childType			: 'other',
+		availableOnWebsite	: false
+	}, {
+		childType			: 'unknown',
+		availableOnWebsite	: false
 	}]
 };
