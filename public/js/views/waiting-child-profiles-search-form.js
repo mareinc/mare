f113e2288d01a86c701d8fabde859fd4e292dbb7
Jared Collier <jared.j.collier@gmail.com>
2016-04-24 12:05:09 -0400

    gallery page - search form sub-view
    1.  Create a sub-view for the gallery search form.

---
+++ public/js/views/waiting-child-profiles-search-form.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+
+	mare.views.GallerySearchForm = Backbone.View.extend({
+		// This view controls everything inside the element with class 'gallery-search-form'
+		el: '.gallery-search-form',
+
+		events: {},
+
+		initialize: function() {}
+
+	});
+})();
