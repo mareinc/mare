cdf8645d60129fc73fed7097cb8b424fdb3fe793
Jared Collier <jared.j.collier@gmail.com>
2016-04-26 19:52:36 -0400

    gallery page - add getPermissions function
    - add an asynchronous call to load the user permissions for the page, resolving a promise once we have them.  The permissions are needed to render the gallery client side so we don't allow restricted functionality for users that shouldn't have it.  NOTE: Verification must be added to the server as well to ensure malicious users can't do things they're not supposed to.

--- public/js/routers/waiting-child-profiles.js
+++ public/js/routers/waiting-child-profiles.js
@@ -11,28 +11,45 @@
 		},
 
 		initialize: function initialize() {
+			// Create a promise to resolve once we have permissions for the user (All actions are verified on the server so this won't introduce any risk)
+			mare.promises.permissionsLoaded = $.Deferred();
+			// Fetch the users permissions
+			this.getPermissions();
 			// Load the view for the waiting child profiles page as a whole
 			mare.views.waitingChildProfiles = mare.views.waitingChildProfiles || new mare.views.WaitingChildProfiles();
 		},
 
+		getPermissions: function getPermissions() {
+			// Fetch permissions for the current user
+			$.ajax({
+				dataType: 'json',
+				url: '/services/get-gallery-permissions',
+				type: 'POST'
+			}).done(function(permissions) {
+				// Store the permissions on the namespace to allow us to access them in all views and subviews for this page
+				mare.permissions.gallery = permissions;
+				// Resolve the promise tracking permissions loading
+				mare.promises.permissionsLoaded.resolve();
+
+			}).fail(function(err) {
+				// TODO: Show an error message if we failed to fetch the permissions
+				console.log(err);
+			});
+		},
+
 		loadGallery: function loadGallery() {
-			// Initialize the view for the gallery if it doesn't already exist
-			// TODO: Test to see if this can be removed as the sub-views are created when the containing waitingChildProfiles view is created
-			mare.views.gallery = mare.views.gallery || new mare.views.Gallery();
 			// Use the view for the waiting child profiles page as a whole to display the correct area
 			mare.views.waitingChildProfiles.showGallery();
 		},
 
 		loadSearch: function loadSearch() {
-			// Initialize the view for the search form if it doesn't already exist
-			// TODO: Test to see if this can be removed as the sub-views are created when the containing waitingChildProfiles view is created
-			mare.views.gallerySearchForm = mare.views.gallerySearchForm || new mare.views.GallerySearchForm();
 			// Use the view for the waiting child profiles page as a whole to display the correct area
 			mare.views.waitingChildProfiles.showSearchForm();
 		},
 
 		/* 	Handle any poorly formed routes or navigation to the waiting child profiles page without specifying a route by rerouting to the gallery */
 		loadDefault: function loadDefault() {
+			// Route to the gallery page without triggering Backbone history with 'replace' to prevent the back button from reloading the bad route
 			this.navigate( 'gallery', { trigger: true, replace: true } );
 		}
 
