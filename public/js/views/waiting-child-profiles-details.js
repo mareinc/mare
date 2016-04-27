d443444ce515830ae8db204e29c62b71607d1af1
Jared Collier <jared.j.collier@gmail.com>
2016-04-27 19:54:07 -0400

    gallery page - details view
    Pull events into the Backbone event handler section.
    
    Make render dependent on a custom event instead of a promise so we don't need to create promises in every function that wants to fetch the child details.
    
    Fix the navigation, and make it work off the current order of models in the Backbone collection to allow for ordering the collection and navigating to the correct next/prev child.  This was done by checking the next element in the DOM in the past which didn't work...  At all.
    
    Clean up pretty much every other function in the view.  It's almost all better now.

--- public/js/views/waiting-child-profiles-details.js
+++ public/js/views/waiting-child-profiles-details.js
@@ -7,87 +7,128 @@
 		// Give the container for our view a class we can hook into
   		className: 'child-details',
 
+  		events: {
+  			'click .modal__close': closeModal,
+			'click .profile-navigation__previous': handleNavClick,
+			'click .profile-navigation__next': handleNavClick
+  		}
+
 		initialize: function initialize() {
 			// Store a reference to this for insde callbacks where context is lost
 			var view = this;
 			// Create a hook to access the gallery template
-			var childDetailsHtml = $('#child-details-template').html();
+			var html = $('#child-details-template').html();
 			// Compile the template to be used during rendering/repainting the gallery
-			this.template = Handlebars.compile(childDetailsHtml);
+			this.template = Handlebars.compile(html);
 			// Initialize the details modal once we've fetched the basic child data, this is needed because the details will be appended to the same collection
 			mare.promises.childrenDataLoaded.done(function() {
 				view.collection = mare.collections.children;
-				// Bind event handlers
-				view.collection.on('child-details-loaded', view.render);
+				// Bind event handler for when child details are returned
+				view.on('child-details-loaded', view.render);
 			});
 		},
 
 		render: function render(childModel) {
-			// Get the index of the current child model in the children collection
-			var childIndex = mare.collections.children.indexOf(childModel);
-			// Check to see if there are children to navigate to before and after the currently displayed child
-			var hasPreviousChild = childIndex > 0;
-			var hasNextChild = childIndex !== mare.collections.children.length - 1;
-			// Set whether there are previous and next children on the model so we have access to the information during rendering
-			childModel.set('hasPreviousChild', hasPreviousChild);
-			childModel.set('hasNextChild', hasNextChild);
+			// Depending on the order of the collection, the next and previous differs per rendering
+			this.setNavigation(childModel);
 			// Pass the child model to through the template we stored during initialization
 			var html = this.template(childModel.toJSON());
+			this.$el.html(html);
 			// Render the contents area and tabs
-			$('.modal-container__contents').html(html);
+			$('.modal-container__contents').html(this.$el);
 			// Remove the loading indicator and display the details content
 			$('.modal-container__loading').fadeOut(function() {
 				$('.modal-container__contents').fadeIn();
 			});
+			// Set a data attribute with the displayed child's id for use in next/prev navigation
+			this.$el.attr('data-registration-number', childModel.get('registrationNumber'));
 			// Set up the modal tab click events
-			// TODO: combine these with the generic bindEvents function below
 			this.initializeModalTabs();
-			// Set up binding for all events in the modal
-			this.bindEvents();
+
 		},
 
-		bindEvents: function bindEvents() {
-			$('.modal__close').click(this.closeModal);
-			$('.profile-navigation__previous').click(this.displayPreviousChildDetails);
-			$('.profile-navigation__next').click(this.displayNextChildDetails);
+		setNavigation: function setNavigation(childModel) {
+			// Get the index of the current child model in the children collection
+			var childIndex = this.collection.indexOf(childModel);
+			// Check to see if there are children to navigate to before and after the currently displayed child
+			var hasPrevious = childIndex > 0;
+			var hasNext = childIndex !== this.collection.length - 1;
+			// Set the index of the previous/next child for easy access in the collection
+			var previousIndex = hasPrevious ? childIndex - 1 : undefined;
+			var nextIndex = hasNext ? childIndex + 1 : undefined;
+			// Set whether there are previous and next children, as well as their indices, so we have the information during rendering
+			childModel.set('hasPrevious', hasPrevious);
+			childModel.set('hasNext', hasNext);
+			childModel.set('previousIndex', previousIndex);
+			childModel.set('nextIndex', nextIndex);
+		},
+
+		getChildByRegistrationNumber: function getChildByRegistrationNumber(registrationNumber) {
+			// Find the child with the matching registration number and store it in childModel
+			var childModel = this.collection.find(function(child) {
+				return child.get('registrationNumber') === registrationNumber;
+			});
+			// Return the matching child model
+			return childModel;
+		},
+
+		getChildByIndex: function getChildByIndex(index) {
+			// Fetch the child at the specified index in the children collection
+			return this.collection.at(index);
+
 		},
 
 		/* When a child card is clicked, display detailed information for that child in a modal window */
-		displayChildDetails: function displayChildDetails(event) {
+		handleGalleryClick: function handleGalleryClick(event) {
 			// Store a reference to this for insde callbacks where context is lost
 			var view = this;
 
-			var selectedChild = $(event.currentTarget),
-				registrationNumber = selectedChild.data('registration-number');
+			var selectedChild		= $(event.currentTarget),
+				registrationNumber	= selectedChild.data('registration-number'),
+				childModel			= this.getChildByRegistrationNumber(registrationNumber);
 			// Open the modal immediately with a loading indicator to keep the site feeling snappy
 			this.openModal();
-			// Create a promise to determine when the child details have been fetched
-			var hasChildDetails = $.Deferred();
 			// Fetch the child details information
-			this.getChildData(registrationNumber, hasChildDetails);
-			// Render the details modal once the child data is returned
-			hasChildDetails.done(function(childModel) {
-				view.render(childModel);
-			});
+			this.getDetails(childModel);
 		},
 
-		/* Look at the current child, then traverse the DOM to determine which child to dislpay next */
-		displayNextChildDetails: function displayNextChildDetails(event) {
+		handleNavClick: function handleNavClick(event) {
+			var selectedChild = $(event.currentTarget),
+				index = selectedChild.data('child-index');
+			// This event is called from a click event so the view context is lost, we need to explicitly call all functions
+			var child = mare.views.childDetails.getChildByIndex(index);
+
+			mare.views.childDetails.getDetails(child);
+		},
+
+		/* Make a call to fetch data for the current child to show detailed information for */
+		getDetails: function getDetails(childModel) {
 			// Store a reference to this for insde callbacks where context is lost
 			var view = this;
+			// Submit a request to the service layer to fetch child data if we don't have it
+			if(!childModel.get('hasDetails')) {
+				$.ajax({
+					dataType: 'json',
+					url: '/services/get-child-details',
+					type: 'POST',
+					data: {
+						registrationNumber: childModel.get('registrationNumber')
+					}
+				}).done(function(childDetails) {
+					// Append the new fields to the child model and set a flag so fetch the same child information a second time
+					childModel.set(childDetails);
+					childModel.set('hasDetails', true);
+					// Emit an event when we have new child details to render
+					view.trigger('child-details-loaded', childModel);
 
-			var selectedChild = $(event.currentTarget),
-				registrationNumber = selectedChild.data('registration-number');
-
-			$('.modal-container__contents').fadeOut(function() {
-
-				view.clearModalContents();
-
-				$('.modal-container__loading').fadeIn(function() {
-					view.getChildData(registrationNumber);
+				}).fail(function(err) {
+					// TODO: Show an error message to the user
+					console.log(err);
 				});
-
-			});
+			} else {
+				// We already have the child details but still want to show the child so announce that we have the child details
+				view.trigger('child-details-loaded', childModel);
+			}
 		},
 
 		/* Open the modal container */
@@ -108,8 +149,8 @@
 			$('.modal__container').fadeOut();
 
 			mare.utils.enablePageScrolling();
-
-			this.clearModalContents();
+			// This event is called from a click event so the view context is lost, we need to explicitly call all functions
+			mare.views.childDetails.clearModalContents();
 		},
 
 		/* Clear out the current contents of the modal */
@@ -148,112 +189,6 @@
 				$('[data-contents=' + selectedContentType + ']').addClass('profile-tab__contents--selected');
 
 			});
-		},
-
-		/* Make a call to fetch data for the current child to show detailed information for */
-		// TODO: once we have the general data pulled into a Backbone collection, this should be driven off the next model, not the next item in the DOM.
-		// 		 this change should also fix the bug where next/previous shows the wrong child based on DOM sorting
-		getChildData: function getChildData(registrationNumber, hasChildDetails) {
-			// Store a reference to this for insde callbacks where context is lost
-			var view = this;
-			// Fetch the model for the child we requested details about
-			var childModel = this.collection.find(function(child) {
-				return child.get('registrationNumber') === registrationNumber;
-			});
-			// Submit a request to the service layer to fetch child data if we don't have it
-			if(!childModel.get('hasDetails')) {
-				$.ajax({
-					dataType: 'json',
-					url: '/services/get-child-details',
-					type: 'POST',
-					data: {
-						registrationNumber: registrationNumber
-					}
-				}).done(function(childDetails) {
-					// Append the new fields to the child model and set a flag so fetch the same child information a second time
-					childModel.set(childDetails);
-					childModel.set('hasDetails', true);
-					// Resolve the promise saying we have the child details
-					hasChildDetails.resolve(childModel);
-
-				}).fail(function(err) {
-					// TODO: Show an error message to the user
-					console.log(err);
-				});
-			} else {
-				// We already have the child details so resolve the promise
-				hasChildDetails.resolve(childModel);
-			}
-		},
-
-		/* Determine how to handle a click on the bookmark button based on the current state of the bookmark */
-		toggleBookmark: function toggleBookmark(e) {
-			e.stopPropagation();
-			// DOM cache the current target for performance
-			var $currentTarget = $(e.currentTarget);
-			// Get the child's registration number to match them in the database
-			var registrationNumber = $currentTarget.closest('.media-box').data('registration-number');
-
-			// if we are currently saving the users attempt to toggle the bookmark and the server hasn't processed the change yet, ignore the click event
-			if( $currentTarget.hasClass('bookmark--disabled') ) {
-
-				return;
-
-			// if the child is currently bookmarked, remove them
-			} else if( $currentTarget.hasClass('bookmark--active') ) {
-
-				$currentTarget.addClass('bookmark--disabled');
-				this.removeBookmark(registrationNumber, $currentTarget);
-
-			// if the child is not currently bookmarked, add them
-			} else {
-
-				$currentTarget.addClass('bookmark--disabled');
-				this.addBookmark(registrationNumber, $currentTarget);
-
-			}
-		},
-
-		addBookmark: function addBookmark(registrationNumber, $currentTarget) {
-
-			$.ajax({
-				url: '/services/add-bookmark',
-				type: 'POST',
-				data: {
-					registrationNumber: registrationNumber
-				}
-			}).done(function(response) {
-				// Once the bookmark has been saved successfully, change the icon, re-enable the bookmark, and show it as active
-				$currentTarget.children('.bookmark__icon').removeClass('fa-plus-square-o').addClass('fa-minus-square-o');
-				$currentTarget.removeClass('bookmark--disabled');
-				$currentTarget.addClass('bookmark--active');
-
-			}).fail(function(err) {
-				// TODO: Show an error message to the user
-				console.log(err);
-			});
-
-		},
-
-		removeBookmark: function removeBookmark(registrationNumber, $currentTarget) {
-
-			$.ajax({
-				url: '/services/remove-bookmark',
-				type: 'POST',
-				data: {
-					registrationNumber: registrationNumber
-				}
-			}).done(function(response) {
-				// Once the bookmark has been removed successfully, change the icon, re-enable the bookmark, and show it as inactive
-				$currentTarget.children('.bookmark__icon').removeClass('fa-minus-square-o').addClass('fa-plus-square-o');
-				$currentTarget.removeClass('bookmark--disabled');
-				$currentTarget.removeClass('bookmark--active');
-
-			}).fail(function(err) {
-				// TODO: Show an error message to the user
-				console.log(err);
-			});
-
 		}
 
 	});
