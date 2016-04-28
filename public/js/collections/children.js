5d1f0e66d51463a19af08a95ebcf2ec81783afb5
Jared Collier <jared.j.collier@gmail.com>
2016-04-27 22:02:07 -0400

    gallery page - collection sorting
    Add functions to sort the collection by registration date, name, and age.
    
    Add default sort of registration date in reverse chronological order.

--- public/js/collections/children.js
+++ public/js/collections/children.js
@@ -4,11 +4,44 @@
     mare.collections.Children = Backbone.Collection.extend({
 
     	model: mare.models.Child,
-    	// Set the comparator on the collection to order by how recently the child was registered
-		// comparator: 'registrationDateConverted'
 
-		comparator: function(child) {
-			return -child.get('registrationDateConverted');
+    	initialize: function initialize() {
+    		this.sortByDateAdded(); // Set the default comparator on the collection to order by how recently the child was registered
+    	},
+
+		reorder: function reorder(sortBy) {
+
+			switch(sortBy) {
+				case 'date-added'	: this.sortByDateAdded(); break;
+				case 'name'			: this.sortByName(); break;
+				case 'age'			: this.sortByAge(); break;
+				default				: this.sortByDateAdded(); break;
+			}
+
+		},
+
+		sortByDateAdded: function sortByDateAdded() {
+
+			this.comparator = function comparator(child) {
+				return -child.get('registrationDateConverted');
+			}
+
+			this.sort();
+
+		},
+
+		sortByName: function sortByName() {
+
+			this.comparator = 'name';
+			this.sort();
+
+		},
+
+		sortByAge: function sortByAge() {
+
+			this.comparator = 'age';
+			this.sort();
+
 		}
 
     });
