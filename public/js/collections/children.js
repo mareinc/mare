8823c755ec1e12e6df9b05ccf92d465a063bcf85
Jared Collier <jared.j.collier@gmail.com>
2016-04-27 21:37:51 -0400

    gallery page - sorting in the gallery
    Add a comparator to the children collection to sort by the date each child was registered in reverse chronological order.

--- public/js/collections/children.js
+++ public/js/collections/children.js
@@ -3,7 +3,13 @@
 
     mare.collections.Children = Backbone.Collection.extend({
 
-    	model: mare.models.Child
+    	model: mare.models.Child,
+    	// Set the comparator on the collection to order by how recently the child was registered
+		// comparator: 'registrationDateConverted'
+
+		comparator: function(child) {
+			return -child.get('registrationDateConverted');
+		}
 
     });
 })();
