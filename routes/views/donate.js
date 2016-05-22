0381b02051ad0051bcd8bf241f82671afd0cb5e4
Jared Collier <jared.j.collier@gmail.com>
2016-05-21 20:06:30 -0400

    #32 | donate page
    - update the donation page to use the new layout.

--- routes/views/donate.js
+++ routes/views/donate.js
@@ -2,11 +2,14 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
     'use strict';
-  
+
     var view = new keystone.View(req, res),
         locals = res.locals;
 
     // TODO: add code for a logged in user showing their previous donations/donation dates
 
+    // Set the layout to render with the right sidebar
+	locals['render-with-sidebar'] = false;
+    // Render the view once all the data has been retrieved
     view.render('donate');
 };
