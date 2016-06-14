8f626a7183cd58bf2d42defdb1ea7c941a437b20
Jared Collier <jared.j.collier@gmail.com>
2016-06-13 22:28:48 -0400

    #41 | add future cleanup todo item

--- public/js/routers/success-story.js
+++ public/js/routers/success-story.js
@@ -8,6 +8,7 @@
 		},
 
 		showDetails: function showDetails() {
+			// TODO: This is loading for details views as well, which we don't need as there's no JavaScript logic to deal with
 			// Load the view for the success story
 			mare.views.successStory = mare.views.successStory || new mare.views.SuccessStory();
 		}
