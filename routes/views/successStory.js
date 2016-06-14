981d0a2c7489ff201e16ea0f5b21dac75dfda1e9
Jared Collier <jared.j.collier@gmail.com>
2016-06-13 22:25:29 -0400

    #41 | split success stories and success story Express views

--- routes/views/successStory.js
+++ routes/views/successStory.js
@@ -1,7 +1,6 @@
 var keystone 		= require('keystone'),
-	_				= require('underscore'),
 	SuccessStory	= keystone.list('Success Story'),
-	Utils			= require('../middleware/utilities');
+	_ = require('underscore');
 
 exports = module.exports = function(req, res) {
     'use strict';
@@ -10,33 +9,15 @@ exports = module.exports = function(req, res) {
     	locals 	= res.locals;
 
     /* TODO: Change this to an async function */
-    SuccessStory.model.find()
+    SuccessStory.model.findOne()
+    	.where('url', req.originalUrl)
 		.exec()
-		.then(function (successStories) {
-			// Variables to hold the target success story for the page and all other stories to display in a secondary navigation
-			locals.targetStory = {};
-			locals.otherStories = [];
-
-			var truncateOptions = {
-				targetLength: 400,
-			}
-
-			// Loop through all success stories
-			_.each(successStories, function(successStory) {
-				successStory.shortContent = Utils.truncateText(successStory.content, truncateOptions);
-				// Find the target story for the current page and store the object in locals.targetStory
-				if(successStory.url === req.originalUrl) {
-					locals.targetStory = successStory;
-				// Find all other success stories and store them in locals.otherStories
-				} else {
-					locals.otherStories.push(successStory);
-				}
-			});
-
+		.then(function (successStory) {
+			// Find the target story for the current page and store the object in locals.targetStory
+			locals.story = successStory;
 			// Set the layout to render with the right sidebar
 			locals['render-with-sidebar'] = true;
 			// Render the view once all the data has been retrieved
-    		view.render('success-story');
-
+			view.render('success-story');
 		});
 };
