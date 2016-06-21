538c90ac44377754b69b645294f9c5dd9ba67fc1
Jared Collier <jared.j.collier@gmail.com>
2016-06-20 20:41:25 -0400

    #37 | create sample auto-update script
    - Create a sample auto-update script with only two entries to show Anna what the format should look like.

---
+++ updates/0.0.1-list_sources.js
@@ -0,0 +1,15 @@
+exports.create = {
+	'Source': [{
+		source: '2002 Calendar',
+		isMediaOutlet: true,
+		isActiveMediaOutlet: false,
+		mediaFrequency: 'one-time',
+		mediaType: 'print'
+	}, {
+		source: '2011 DOT Billboards',
+		isMediaOutlet: true,
+		isActiveMediaOutlet: true,
+		mediaFrequency: 'one month',
+		mediaType: 'print'
+	}]
+};
