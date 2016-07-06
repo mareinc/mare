0cd52aa254fadb96f2d15525663bcab19ad78e33
Jared Collier <jared.j.collier@gmail.com>
2016-07-05 20:05:45 -0400

    #39 | update view to enable/disable the registration button
    - Add a promise for clicks on the register/unregister buttons to disable the button, wait for the operation to complete, then change the text and reenable the button only when another click is ready to be handled.

--- public/js/views/events.js
+++ public/js/views/events.js
@@ -17,23 +17,71 @@
         },
 
         register: function register() {
+			// Store a reference to this for insde callbacks where context is lost
+			var view = this;
+			// Create a promise to resolve once the user has been successfully registered
+			mare.promises.registered = $.Deferred();
+
+			var $button = $(event.target);
+			// Disable the button to prevent multiple clicks
+			mare.utils.disableButton($button);
 
 			var options = {
-				eventId: $(event.target).data('event-id'),
-				service: 'register-for-event'
-			}
+				eventId: $button.data('event-id'),
+				service: 'register-for-event',
+				action: 'register'
+			};
 
 			this.callEventService(options);
+
+			// Update the button once the user has been successfully registered
+			mare.promises.registered.done(function() {
+				// Remove the click event handler for the register button
+				view.unbindRegisterButton();
+				// DOM cache the button for performance
+				var $button = $('.events__register-button');
+				// Change the button text and apply the correct class
+				$button.html('Unregister');
+				$button.removeClass('events__register-button').addClass('events__unregister-button');
+				// Add the click event handler for the unregister button
+				view.bindUnregisterButton();
+				// Enable the button to allow additional click events to be handled
+				mare.utils.enableButton($button);
+			});
         },
 
         unregister: function unregister() {
+			// Store a reference to this for insde callbacks where context is lost
+			var view = this;
+			// Create a promise to resolve once the user has been successfully unregistered
+			mare.promises.unregistered = $.Deferred();
+
+			var $button = $(event.target);
+			// Disable the button to prevent multiple clicks
+			mare.utils.disableButton($button);
 
 			var options = {
-				eventId: $(event.target).data('event-id'),
-				service: 'unregister-for-event'
-			}
+				eventId: $button.data('event-id'),
+				service: 'unregister-for-event',
+				action: 'unregister'
+			};
 
 			this.callEventService(options);
+
+			// Update the button once the user has been successfully unregistered
+			mare.promises.unregistered.done(function() {
+				// Remove the click event handler for the register button
+				view.unbindUnregisterButton();
+				// DOM cache the button for performance
+				var $button = $('.events__unregister-button');
+				// Change the button text and apply the correct class
+				$button.html('Register');
+				$button.removeClass('events__unregister-button').addClass('events__register-button');
+				// Add the click event handler for the unregister button
+				view.bindRegisterButton();
+				// Enable the button to allow additional click events to be handled
+				mare.utils.enableButton($button);
+			});
 
         },
 
@@ -46,16 +94,48 @@
 					eventId: options.eventId
 				}
 			}).done(function(response) {
-				console.log(response);
-				// Once the bookmark has been saved successfully, change the icon, re-enable the bookmark, and show it as active
-				// $currentTarget.children('.bookmark__icon').removeClass('fa-plus-square-o').addClass('fa-minus-square-o');
-				// $currentTarget.removeClass('bookmark--disabled');
-				// $currentTarget.addClass('bookmark--active');
+				// resolve the appropriate promise showing that registration for the user has changed
+				if(options.action === 'register') {
+
+					mare.promises.registered.resolve();
+
+				} else if(options.action === 'unregister') {
+
+					mare.promises.unregistered.resolve();
+
+				}
 
 			}).fail(function(err) {
 				// TODO: Show an error message to the user
 				console.log(err);
 			});
+		},
+
+		bindRegisterButton: function bindRegisterButton() {
+			// Store a reference to this for insde callbacks where context is lost
+			var view = this;
+
+			$('.events__register-button').on('click', this.register.bind(view));
+
+		},
+
+		bindUnregisterButton: function bindUnregisterButton() {
+			// Store a reference to this for insde callbacks where context is lost
+			var view = this;
+
+			$('.events__unregister-button').on('click', this.unregister.bind(view));
+		},
+
+		unbindRegisterButton: function unbindRegisterButton() {
+			// Unbind all events on the register button
+			$('.events__register-button').off();
+
+		},
+
+		unbindUnregisterButton: function unbindUnregisterButton() {
+			// Unbind all events on the unregister button
+			$('.events__unregister-button').off();
+
 		}
 
 	});
