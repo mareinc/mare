12f988d2d15c965caa1e42c3b88f6f6f53f41415
Jared Collier <jared.j.collier@gmail.com>
2016-07-10 19:59:37 -0400

    #33 | create functions for finding the section header
    - For most pages it's easy as we know in the Express middleware which section we're rendering, however pages are a problem.  To handle pages, until dynamic navigation is implemented, we're relying on a map between each page's url and the section header that belongs.  This is a little flimsy which sucks as changing a page name will break it's header.  Building CSC controlled menus is being prioritized up because of this.

--- routes/middleware/service_page.js
+++ routes/middleware/service_page.js
@@ -1,9 +1,12 @@
-var keystone	= require('keystone'),
-	Page		= keystone.list('Page');
+var keystone			= require('keystone'),
+	async				= require('async'),
+	Page				= keystone.list('Page'),
+	SectionHeader		= keystone.list('Section Header'),
+	eventService		= require('./service_event'),
+	successStoryService	= require('./service_success-story');
 
 exports.getPageByUrl = function getPageByUrl(req, res, done, url) {
 
-	req.locals = res.locals || {};
 	var locals = res.locals;
 
 	Page.model.find()
@@ -21,4 +24,47 @@ exports.getPageByUrl = function getPageByUrl(req, res, done, url) {
 					done();
 
 				});
+};
+
+exports.getSectionHeader = function getSectionHeader(req, res, done, header) {
+
+	var locals = res.locals;
+
+	SectionHeader.model.findOne()
+				.where('header', header)
+				.exec()
+				.then(function (header) {
+
+					locals.sectionHeader = header;
+					// execute done function if async is used to continue the flow of execution
+					done()
+
+				}, function(err) {
+
+					console.log(err);
+					done();
+
+				});
+};
+
+exports.getPageSectionHeader = function getPageSectionHeader(req, res, done) {
+
+	var locals	= res.locals,
+		section	= locals.pageSectionMapping[req.originalUrl];
+
+	exports.getSectionHeader(req, res, done, section);
+};
+
+exports.populateSidebar = function populateSidebar(req, res, done) {
+
+	var locals = res.locals;
+
+	async.parallel([
+		function(done) { successStoryService.getRandomStory(req, res, done); },
+		function(done) { eventService.getRandomEvent(req, res, done); }
+	], function() {
+
+		done();
+
+	});
 };
