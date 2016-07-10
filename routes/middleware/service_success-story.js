524bafe4b908877e702f831db5d505a1d85d76f0
Jared Collier <jared.j.collier@gmail.com>
2016-07-09 20:25:16 -0400

    #33 | success story service

---
+++ routes/middleware/service_success-story.js
@@ -0,0 +1,17 @@
+var keystone		= require('keystone'),
+	async			= require('async'),
+	SuccessStory	= keystone.list('Success Story');
+
+	exports.getRandomStory = function getRandomStory(req, res, done) {
+
+		req.locals = res.locals || {};
+		var locals = res.locals;
+
+		// TODO: Handle the error if we get one
+		SuccessStory.model.findRandom(function(err, successStory){
+			locals.randomSuccessStory = successStory;
+			// execute done function if async is used to continue the flow of execution
+			done();
+		});
+
+	};
