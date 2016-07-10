b2cb8d474d14af8ed12fff782419511c6db9f721
Jared Collier <jared.j.collier@gmail.com>
2016-07-09 22:22:32 -0400

    #33 | load the sidebar view into events and success stories

--- public/js/routers/events.js
+++ public/js/routers/events.js
@@ -5,6 +5,8 @@
 
 		initialize: function initialize() {
 			mare.views.events = mare.views.events || new mare.views.Events();
+			// Load the view for the right sidebar
+			mare.views.sidebar = mare.views.sidebar || new mare.views.Sidebar();
 		}
 
 	});
