dcdbfc4458ea4a219864d1fc8ec53b804541074e
Jared Collier <jared.j.collier@gmail.com>
2016-07-09 20:19:32 -0400

    #33 | populate sidebar for the event page
     - Use sidebar service to populate random event and success story.
    - Change the processing before the Express view render to use async.

--- routes/views/event.js
+++ routes/views/event.js
@@ -1,8 +1,10 @@
-var keystone	= require('keystone'),
-	_			= require('underscore'),
-	moment		= require('moment'),
-	Event		= keystone.list('Event'),
-	Utils		= require('../middleware/utilities');
+var keystone		= require('keystone'),
+	async			= require('async'),
+	_				= require('underscore'),
+	moment			= require('moment'),
+	Event			= keystone.list('Event'),
+	Utils			= require('../middleware/utilities'),
+	sidebarService	= require('../middleware/service_sidebar');
 
 exports = module.exports = function(req, res) {
     'use strict';
@@ -35,68 +37,73 @@ exports = module.exports = function(req, res) {
 		default							: eventType = '';
 	}
 
-     /* TODO: Change this to an async function */
-     /* TODO: Add error handling so the page doesn't hang if we can't find the event */
-    Event.model.findOne()
-    	.where('url', req.originalUrl)
-		.populate('contact') // The contact is a relationship field, so just the ID is stored.  Force the request to fetch the referenced contact model
-		.populate('childAttendees')
-		.populate('familyAttendees')
-		.populate('socialWorkerAttendees')
-		.populate('siteVisitorAttendees')
-		.populate('cscAttendees')
-		.populate('address.state')
-		.exec()
-		.then(function (event) {
-			// Determine if the user is an administrator. We want to display the event attendees if they are
-			req.user = req.user || {};
-			locals.isAdmin = req.user && req.user.userType === 'admin' ? true : false;
-			// If there are no events to display, we need to capture that information for rendering
-			locals.eventMissing = _.isEmpty(event);
-			// Find the target event for the current page and store the object in locals for access during templating
-			locals.event = event;
-			// Pull the date and time into a string for easier templating
-			event.dateTimeString = moment(event.date).format('dddd MMMM Do, YYYY') + ' from ' + event.startTime + ' - ' + event.endTime;
-			// Determine whether or not address information exists for the event, which is helpful during rendering
-			// street1 is required, so this is enough to tell us if the address has been populated
-			event.hasAddress = event.address && event.address.street1 ? true : false;
-			// Store data on whether any attendees exist for the each group
-			// We need this to know whether we should render headers for each list during templating
-			event.hasCSCAttendees			= event.cscAttendees.length > 0 ? true : false;
-			event.hasFamilyAttendees		= event.familyAttendees.length > 0 ? true : false;
-			event.hasSocialWorkerAttendees	= event.socialWorkerAttendees.length > 0 ? true : false;
-			event.hasSiteVisitorAttendees	= event.siteVisitorAttendees.length > 0 ? true : false;
-			event.hasChildAttendees			= event.childAttendees.length > 0 ? true : false;
+	/* TODO: Add error handling so the page doesn't hang if we can't find the event */
+	async.parallel([
+		function(done) { // TODO: Pull this into the Event service
+			Event.model.findOne()
+		    	.where('url', req.originalUrl)
+				.populate('contact') // The contact is a relationship field, so just the ID is stored.  Force the request to fetch the referenced contact model
+				.populate('childAttendees')
+				.populate('familyAttendees')
+				.populate('socialWorkerAttendees')
+				.populate('siteVisitorAttendees')
+				.populate('cscAttendees')
+				.populate('address.state')
+				.exec()
+				.then(function (event) {
+					// Determine if the user is an administrator. We want to display the event attendees if they are
+					req.user = req.user || {};
+					locals.isAdmin = req.user && req.user.userType === 'admin' ? true : false;
+					// If there are no events to display, we need to capture that information for rendering
+					locals.eventMissing = _.isEmpty(event);
+					// Find the target event for the current page and store the object in locals for access during templating
+					locals.event = event;
+					// Pull the date and time into a string for easier templating
+					event.dateTimeString = moment(event.date).format('dddd MMMM Do, YYYY') + ' from ' + event.startTime + ' - ' + event.endTime;
+					// Determine whether or not address information exists for the event, which is helpful during rendering
+					// street1 is required, so this is enough to tell us if the address has been populated
+					event.hasAddress = event.address && event.address.street1 ? true : false;
+					// Store data on whether any attendees exist for the each group
+					// We need this to know whether we should render headers for each list during templating
+					event.hasCSCAttendees			= event.cscAttendees.length > 0 ? true : false;
+					event.hasFamilyAttendees		= event.familyAttendees.length > 0 ? true : false;
+					event.hasSocialWorkerAttendees	= event.socialWorkerAttendees.length > 0 ? true : false;
+					event.hasSiteVisitorAttendees	= event.siteVisitorAttendees.length > 0 ? true : false;
+					event.hasChildAttendees			= event.childAttendees.length > 0 ? true : false;
 
-			_.each(event[targetGroup], function(attendee) {
-				// Without converting to strings, these were both evaluating to Object which didn't allow for a clean comparison
-				var attendeeID = attendee._id.toString();
-				var userID = req.user._id.toString();
-				// Determine whether the user is already attending the event
-				// We can't break out of the _.each, so only keep checking if true wasn't found
-				if(!event.attending) {
-					event.attending = attendeeID === userID ? true : false;
-				}
-			});
+					_.each(event[targetGroup], function(attendee) {
+						// Without converting to strings, these were both evaluating to Object which didn't allow for a clean comparison
+						var attendeeID = attendee._id.toString();
+						var userID = req.user._id.toString();
+						// Determine whether the user is already attending the event
+						// We can't break out of the _.each, so only keep checking if true wasn't found
+						if(!event.attending) {
+							event.attending = attendeeID === userID ? true : false;
+						}
+					});
 
-			_.each(event.familyAttendees, function(family) {
-				// if the targetGroup is families, we need to prettify the attendee name
-				var contact2Exists = family.contact2.name.full.length > 0 ? true : false;
-				var sameLastName = family.contact1.name.last === family.contact2.name.last ? true : false;
+					_.each(event.familyAttendees, function(family) {
+						// if the targetGroup is families, we need to prettify the attendee name
+						var contact2Exists = family.contact2.name.full.length > 0 ? true : false;
+						var sameLastName = family.contact1.name.last === family.contact2.name.last ? true : false;
 
-				if(contact2Exists && sameLastName) {
-					family.fullName = family.contact1.name.first + ' and ' + family.contact2.name.full;
-				} else if(contact2Exists && !sameLastName) {
-					family.fullName = family.contact1.name.full + ' and ' + family.contact2.name.full;
-				} else {
-					family.fullName = family.contact1.name.full;
-				}
-			});
+						if(contact2Exists && sameLastName) {
+							family.fullName = family.contact1.name.first + ' and ' + family.contact2.name.full;
+						} else if(contact2Exists && !sameLastName) {
+							family.fullName = family.contact1.name.full + ' and ' + family.contact2.name.full;
+						} else {
+							family.fullName = family.contact1.name.full;
+						}
+					});
 
-			// Set the layout to render with the right sidebar
-			locals['render-with-sidebar'] = true;
-			// Render the view once all the data has been retrieved
-			view.render('event');
-		});
-
+					done();
+				});
+		},
+		function(done) { sidebarService.populateSidebar(req, res, done); }
+	], function() {
+		// Set the layout to render with the right sidebar
+		locals['render-with-sidebar'] = true;
+		// Render the view once all the data has been retrieved
+		view.render('event');
+	});
 };
