c7b6da0f82b82864476a125ffa547e2b6b91a9fc
Jared Collier <jared.j.collier@gmail.com>
2016-07-10 13:46:11 -0400

    #33 | remove a stray DOM selector

--- public/js/views/sidebar.js
+++ public/js/views/sidebar.js
@@ -8,11 +8,10 @@
 			'click .button--donate'				: 'navigate',
 			'click .button--have-a-question'	: 'navigate'
 		},
-
+		// TODO: Consider making this a utility function since it's used on several pages
 		navigate: function navigate(event) {
-			$(event.currentTarget);
         	window.location.href = $(event.currentTarget).data('url');
         }
-		
+
 	});
 }());
