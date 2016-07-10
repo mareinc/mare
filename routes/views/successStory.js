cb7b107902fc7adcc44728f51d44ba6efa393786
Jared Collier <jared.j.collier@gmail.com>
2016-07-09 20:17:46 -0400

    #33 | populate sidebar for the success story page
     - Use sidebar service to populate random event and success story.
    - Change the processing before the Express view render to use async.

--- routes/views/successStory.js
+++ routes/views/successStory.js
@@ -1,6 +1,8 @@
 var keystone 		= require('keystone'),
+	async			= require('async'),
 	_				= require('underscore'),
-	SuccessStory	= keystone.list('Success Story');
+	SuccessStory	= keystone.list('Success Story'),
+	sidebarService	= require('../middleware/service_sidebar');
 
 exports = module.exports = function(req, res) {
     'use strict';
@@ -8,17 +10,25 @@ exports = module.exports = function(req, res) {
     var view 	= new keystone.View(req, res),
     	locals 	= res.locals;
 
-    /* TODO: Change this to an async function */
-    /* TODO: Add error handling so the page doesn't hang if we can't find the event */
-    SuccessStory.model.findOne()
-    	.where('url', req.originalUrl)
-		.exec()
-		.then(function (successStory) {
-			// Find the target story for the current page and store the object in locals for access during templating
-			locals.story = successStory;
-			// Set the layout to render with the right sidebar
-			locals['render-with-sidebar'] = true;
-			// Render the view once all the data has been retrieved
-			view.render('success-story');
-		});
+	/* TODO: Add error handling so the page doesn't hang if we can't find the event */
+	async.parallel([
+		function(done) { // TODO: Pull this function into the success story service
+			SuccessStory.model.findOne()
+		    	.where('url', req.originalUrl)
+				.exec()
+				.then(function (successStory) {
+					// Find the target story for the current page and store the object in locals for access during templating
+					locals.story = successStory;
+
+					done();
+				});
+		},
+		function(done) { sidebarService.populateSidebar(req, res, done); }
+	], function() {
+		// Set the layout to render with the right sidebar
+		locals['render-with-sidebar'] = true;
+		// Render the view once all the data has been retrieved
+		view.render('success-story');
+	});
+
 };
