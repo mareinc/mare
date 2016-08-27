389dd2e253c5cb439c3472e4920ece0e28fc0b75
Jared Collier <jared.j.collier@gmail.com>
2016-08-27 19:15:34 -0400

    updates mailing list fields.
    adds a reference to outside contact groups and updates attendees to subscribers in all cases for readability.

--- models/MailingList.js
+++ models/MailingList.js
@@ -10,10 +10,12 @@ var MailingList = new keystone.List('Mailing List', {
 // Create fields
 MailingList.add({
 	mailingList: { type: Types.Text, label: 'mailing list', required: true, initial: true },
-	siteVisitorAttendees: { type: Types.Relationship, label: 'site visitors', ref: 'Site Visitor', many: true, initial: true },
-	socialWorkerAttendees: { type: Types.Relationship, label: 'social workers', ref: 'Social Worker', many: true, initial: true },
-	familyAttendees: { type: Types.Relationship, label: 'families', ref: 'Family', many: true, initial: true },
-	outsideContactAttendees: { type: Types.Relationship, label: 'outside contacts', ref: 'Outside Contact', many: true, initial: true }
+	adminSubscribers: { type: Types.Relationship, label: 'admin', ref: 'Admin', many: true, initial: true },
+	siteVisitorSubscribers: { type: Types.Relationship, label: 'site visitors', ref: 'Site Visitor', many: true, initial: true },
+	socialWorkerSubscribers: { type: Types.Relationship, label: 'social workers', ref: 'Social Worker', many: true, initial: true },
+	familySubscribers: { type: Types.Relationship, label: 'families', ref: 'Family', many: true, initial: true },
+	outsideContactGroupSubscribers: { type: Types.Relationship, label: 'outside contact groups', ref: 'Outside Contact Group', many: true, initial: true },
+	outsideContactSubscribers: { type: Types.Relationship, label: 'outside contacts', ref: 'Outside Contact', many: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
