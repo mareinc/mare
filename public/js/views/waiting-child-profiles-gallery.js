f88a9d6e47b8e242b399445cb7bd6089ab66e4e7
Jared Collier <jared.j.collier@gmail.com>
2016-11-08 00:15:52 -0500

    #54 | update gallery Backbone view to allow for sibling group rendering

--- public/js/views/waiting-child-profiles-gallery.js
+++ public/js/views/waiting-child-profiles-gallery.js
@@ -6,27 +6,32 @@
 		el: '.gallery',
 
 		events: {
-			'click .media-box'								: 'displayChildDetails',
+			'click .child-media-box'						: 'displayChildDetails',
+			'click .sibling-group-media-box'				: 'displaySiblingGroupDetails',
 			'click .bookmark'								: 'toggleBookmark',
 			'change .waiting-child-profiles__gallery-filter': 'sortGallery'
 		},
 
 		initialize: function initialize() {
 			// Store a reference to this for insde callbacks where context is lost
-			var view = this;
+			var view							= this;
 			// Create a hook to access the gallery template
-			var galleryHtml = $('#gallery-template').html();
-			// Compile the template to be used during rendering/repainting the gallery
-			this.template = Handlebars.compile(galleryHtml);
-			// Initialize a subview for the details modal
-			mare.views.childDetails = mare.views.childDetails || new mare.views.ChildDetails();
+			var galleryChildrenHtml				= $( '#gallery-children-template' ).html();
+			var gallerySiblingGroupsHtml		= $( '#gallery-sibling-groups-template' ).html();
+			// Compile the templates to be used during rendering/repainting the gallery
+			this.childrenTemplate				= Handlebars.compile( galleryChildrenHtml );
+			this.siblingGroupsTemplate			= Handlebars.compile( gallerySiblingGroupsHtml )
+			// Initialize a subview for the details modals
+			mare.views.childDetails				= mare.views.childDetails || new mare.views.ChildDetails();
+			mare.views.siblingGroupDetails		= mare.views.siblingGroupDetails || new mare.views.SiblingGroupDetails();
 			// Initialize the gallery once we've fetched the child data needed to display the gallery (this doesn't include child details data)
-			mare.promises.childrenDataLoaded.done(function() {
-				view.collection = mare.collections.galleryChildren;
+			mare.promises.childrenDataLoaded.done( function() {
+				view.childrenCollection			= mare.collections.galleryChildren;
+				view.siblingGroupsCollection	= mare.collections.gallerySiblingGroups;
 			});
 
 			// Bind to change events
-			mare.collections.galleryChildren.on('sorted', function() {
+			mare.collections.galleryChildren.on( 'sorted', function() {
 				view.render();
 			});
 		},
@@ -36,57 +41,73 @@
 			var view = this;
 			// The gallery can't render until we have the user permissions and the child data is loaded
 			// use the promise bound to both data to delay rendering until we have them
-			$.when(mare.promises.permissionsLoaded, mare.promises.childrenDataLoaded).then(function() {
+			$.when( mare.promises.permissionsLoaded, mare.promises.childrenDataLoaded ).then( function() {
 				// Pass the collection data through the gallery template to generate the HTML to be added to the gallery
-				var html = view.template(view.collection.toJSON());
-				view.$('.profiles-container').html(html);
+				var childrenHtml		= view.childrenTemplate( view.childrenCollection.toJSON() );
+				var siblingGroupsHtml	= view.siblingGroupsTemplate( view.siblingGroupsCollection.toJSON() );
+
+				view.$( '.profiles-container' ).html( childrenHtml + siblingGroupsHtml );
 				// Once the html is rendered to the page, initialize the gallery display plugin
-				view.initializeMediaBoxes();
+				view.initializeChildrenMediaBoxes();
+				view.initializeSiblingGroupsMediaBoxes();
 			});
 		},
 
-		initializeMediaBoxes: function initializeMediaBoxes() {
-			// initialize the photo listing gallery grid
-			$('#grid').mediaBoxes({
+		initializeChildrenMediaBoxes: function initializeChildrenMediaBoxes() {
+			// initialize the photo listing children gallery grid
+			$( '#children-grid' ).mediaBoxes({
+				boxesToLoadStart: 12,
+				boxesToLoad 	: 8
+			});
+		},
+		
+		initializeSiblingGroupsMediaBoxes: function initializeSiblingGroupMediaBoxes() {
+			// initialize the photo listing sibling group gallery grid
+			$( '#sibling-groups-grid' ).mediaBoxes({
 				boxesToLoadStart: 12,
 				boxesToLoad 	: 8
 			});
 		},
 
 		/* When a child card is clicked, pass the request to the subview in charge of the details modal */
-		displayChildDetails: function displayChildDetails(event) {
-			mare.views.childDetails.handleGalleryClick(event);
+		displayChildDetails: function displayChildDetails( event ) {
+			mare.views.childDetails.handleGalleryClick( event );
+		},
+
+		/* When a sibling group card is clicked, pass the request to the subview in charge of the details modal */
+		displaySiblingGroupDetails: function displayChildDetails( event ) {
+			mare.views.siblingGroupDetails.handleGalleryClick( event );
 		},
 
 		/* Determine how to handle a click on the bookmark button based on the current state of the bookmark */
-		toggleBookmark: function toggleBookmark(e) {
-			e.stopPropagation();
+		toggleBookmark: function toggleBookmark( event ) {
+			event.stopPropagation();
 			// DOM cache the current target for performance
-			var $currentTarget = $(e.currentTarget);
+			var $currentTarget = $( event.currentTarget );
 			// Get the child's registration number to match them in the database
-			var registrationNumber = $currentTarget.closest('.media-box').data('registration-number');
+			var registrationNumber = $currentTarget.closest( '.media-box' ).data( 'registration-number' );
 
 			// if we are currently saving the users attempt to toggle the bookmark and the server hasn't processed the change yet, ignore the click event
-			if( $currentTarget.hasClass('bookmark--disabled') ) {
+			if( $currentTarget.hasClass( 'bookmark--disabled' ) ) {
 
 				return;
 
 			// if the child is currently bookmarked, remove them
-			} else if( $currentTarget.hasClass('bookmark--active') ) {
+			} else if( $currentTarget.hasClass( 'bookmark--active' ) ) {
 
-				$currentTarget.addClass('bookmark--disabled');
-				this.removeBookmark(registrationNumber, $currentTarget);
+				$currentTarget.addClass( 'bookmark--disabled' );
+				this.removeBookmark( registrationNumber, $currentTarget );
 
 			// if the child is not currently bookmarked, add them
 			} else {
 
-				$currentTarget.addClass('bookmark--disabled');
-				this.addBookmark(registrationNumber, $currentTarget);
+				$currentTarget.addClass( 'bookmark--disabled' );
+				this.addBookmark( registrationNumber, $currentTarget );
 
 			}
 		},
 
-		addBookmark: function addBookmark(registrationNumber, $currentTarget) {
+		addBookmark: function addBookmark( registrationNumber, $currentTarget ) {
 
 			$.ajax({
 				url: '/services/add-bookmark',
@@ -94,20 +115,20 @@
 				data: {
 					registrationNumber: registrationNumber
 				}
-			}).done(function(response) {
+			}).done( function( response ) {
 				// Once the bookmark has been saved successfully, change the icon, re-enable the bookmark, and show it as active
-				$currentTarget.children('.bookmark__icon').removeClass('fa-plus-square-o').addClass('fa-minus-square-o');
-				$currentTarget.removeClass('bookmark--disabled');
-				$currentTarget.addClass('bookmark--active');
+				$currentTarget.children( '.bookmark__icon' ).removeClass( 'fa-plus-square-o' ).addClass( 'fa-minus-square-o' );
+				$currentTarget.removeClass( 'bookmark--disabled' );
+				$currentTarget.addClass( 'bookmark--active' );
 
-			}).fail(function(err) {
+			}).fail( function( err ) {
 				// TODO: Show an error message to the user
-				console.log(err);
+				console.log( err );
 			});
 
 		},
 
-		removeBookmark: function removeBookmark(registrationNumber, $currentTarget) {
+		removeBookmark: function removeBookmark( registrationNumber, $currentTarget ) {
 
 			$.ajax({
 				url: '/services/remove-bookmark',
@@ -115,25 +136,24 @@
 				data: {
 					registrationNumber: registrationNumber
 				}
-			}).done(function(response) {
+			}).done( function( response ) {
 				// Once the bookmark has been removed successfully, change the icon, re-enable the bookmark, and show it as inactive
-				$currentTarget.children('.bookmark__icon').removeClass('fa-minus-square-o').addClass('fa-plus-square-o');
-				$currentTarget.removeClass('bookmark--disabled');
-				$currentTarget.removeClass('bookmark--active');
+				$currentTarget.children( '.bookmark__icon' ).removeClass( 'fa-minus-square-o' ).addClass( 'fa-plus-square-o' );
+				$currentTarget.removeClass( 'bookmark--disabled' );
+				$currentTarget.removeClass( 'bookmark--active' );
 
-			}).fail(function(err) {
+			}).fail( function( err ) {
 				// TODO: Show an error message to the user
-				console.log(err);
+				console.log( err );
 			});
 
 		},
 
-		sortGallery: function sortGallery(event) {
-			var $currentTarget = $(event.currentTarget);
+		sortGallery: function sortGallery( event ) {
+			var $currentTarget = $( event.currentTarget );
 			var sortBy = $currentTarget.val();
 
-			mare.collections.galleryChildren.reorder(sortBy);
+			mare.collections.galleryChildren.reorder( sortBy );
 		}
-
 	});
 }());
