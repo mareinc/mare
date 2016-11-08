a7b372b01d6603a7bc611d533e63b1fe598d4379
Jared Collier <jared.j.collier@gmail.com>
2016-11-08 00:08:17 -0500

    #54 | create sibling groups Backbone collection

---
+++ public/js/collections/siblingGroups.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    mare.collections.SiblingGroups = Backbone.Collection.extend({
+
+    	model: mare.models.SiblingGroup,
+
+    	initialize: function initialize() {
+    		this.sortByDateRegistered(); // Set the default comparator on the collection to order by how recently the child was registered
+    	},
+		// TODO: update based on Lisa's input
+		reorder: function reorder(sortBy) {
+
+			switch(sortBy) {
+				case 'registered'	: this.sortByDateRegistered(); break;
+				case 'name'			: this.sortByName(); break;
+				case 'age'			: this.sortByAge(); break;
+				default				: this.sortByDateRegistered(); break;
+			}
+
+			this.trigger('sorted');
+
+		},
+		// TODO: update based on Lisa's input
+		sortByDateRegistered: function sortByDateRegistered() {
+
+			this.comparator = function comparator(child) {
+				return -child.get('registrationDateConverted');
+			}
+
+			this.sort();
+
+		},
+		// TODO: update based on Lisa's input
+		sortByName: function sortByName() {
+
+			this.comparator = 'name';
+			this.sort();
+
+		},
+		// TODO: update based on Lisa's input
+		sortByAge: function sortByAge() {
+
+			this.comparator = 'age';
+			this.sort();
+
+		}
+
+    });
+}());
