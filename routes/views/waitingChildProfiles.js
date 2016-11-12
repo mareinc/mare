85787132131ce5e99f1c4e94d6728edaa54b18e5
Jared Collier <jared.j.collier@gmail.com>
2016-11-11 20:43:41 -0500

    fix spacing

--- routes/views/waitingChildProfiles.js
+++ routes/views/waitingChildProfiles.js
@@ -6,27 +6,27 @@ var keystone		= require('keystone'),
 	listsService	= require('../middleware/service_lists'),
 	pageService		= require('../middleware/service_page');
 
-exports = module.exports = function(req, res) {
+exports = module.exports = ( req, res ) => {
 	'use strict';
 
-	var view		= new keystone.View(req, res),
+	var view		= new keystone.View( req, res ),
 		locals		= res.locals;
 
 	// Set local variables
-	locals.userType	= req.user ? req.user.get('userType') : 'anonymous';
+	locals.userType	= req.user ? req.user.get( 'userType' ) : 'anonymous';
 	// Anonymous users and site users have access only to unrestricted children, registered families and social workers have access to all children
 	locals.targetChildren = locals.userType === 'anonymous' || locals.userType === 'site visitor' ? 'unrestricted' : 'all';
 
 	async.series([
-		function(done) { familyService.setGalleryPermissions(req, res, done); },
-		function(done) { pageService.populateSidebar(req, res, done); },
-		function(done) { pageService.getSectionHeader(req, res, done, 'Meet the Children'); }
+		done => { familyService.setGalleryPermissions( req, res, done ); },
+		done => { pageService.populateSidebar( req, res, done ); },
+		done => { pageService.getSectionHeader( req, res, done, 'Meet the Children' ); }
 
-	], function() {
+	], () => {
 		// Set the layout to render without the right sidebar
-		locals['render-with-sidebar'] = false;
+		locals[ 'render-with-sidebar' ] = false;
 		// Render the view once all the data has been retrieved
-		view.render('waiting-child-profiles');
+		view.render( 'waiting-child-profiles' );
 
 	});
 
