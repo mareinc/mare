dba1f467b6b6e4176405c18be48321e67992744d
Jared Collier <jared.j.collier@gmail.com>
2016-11-11 20:01:47 -0500

    #60 | update Express view loading to reflect the client-centric approach to loading data into the gallery

--- routes/views/waitingChildProfiles.js
+++ routes/views/waitingChildProfiles.js
@@ -18,38 +18,7 @@ exports = module.exports = function(req, res) {
 	locals.targetChildren = locals.userType === 'anonymous' || locals.userType === 'site visitor' ? 'unrestricted' : 'all';
 
 	async.series([
-		// Based on what the user is allowed to see, populate the children models into locals.children
-		function(done) {
-			if(locals.targetChildren === 'all') {
-				childService.getAllChildren(req, res, done);
-			} else {
-				childService.getUnrestrictedChildren(req, res, done);
-			}
-		},
 		function(done) { familyService.setGalleryPermissions(req, res, done); },
-		function(done) { locals.canBookmarkChildren ? familyService.getBookmarkedChildren(req, res, done) : done(); },
-		function(done) {
-			if(locals.bookmarkedChildren && locals.bookmarkedChildren.length > 0) {
-				// Loop through each child model and set a property to show they've already been bookmarked by the user during templating
-				_.each(locals.children, function(child) {
-					// Get the child id to compare with the array of currently bookmarked child ids
-					var childId = child.get('_id');
-					// Set the property for use during templating to show if the child has already been bookmarked
-					child.isBookmarked = locals.bookmarkedChildren.indexOf(childId) !== -1 ? true : false;
-				});
-
-				done();
-
-			} else {
-				done();
-			}
-		},
-		function(done) { listsService.getAllGenders(req, res, done) },
-		function(done) { listsService.getAllRaces(req, res, done) },
-		function(done) { listsService.getAllLanguages(req, res, done) },
-		function(done) { listsService.getAllDisabilities(req, res, done) },
-		function(done) { listsService.getOtherConsiderations(req, res, done) },
-		function(done) { listsService.getAllFamilyConstellations(req, res, done) },
 		function(done) { pageService.populateSidebar(req, res, done); },
 		function(done) { pageService.getSectionHeader(req, res, done, 'Meet the Children'); }
 
