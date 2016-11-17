7f67fb18dbd7642ff79d187f8dd294989d1e00c1
Jared Collier <jared.j.collier@gmail.com>
2016-11-16 20:47:48 -0500

    #23 | fix name called to kick off event migration

--- routes/views/dataMigration.js
+++ routes/views/dataMigration.js
@@ -1,32 +1,32 @@
-var keystone				= require('keystone'),
-	async					= require('async'),
-	agenciesImport			= require('../middleware/data-migration-middleware/agency_model'),
-	childrenImport			= require('../middleware/data-migration-middleware/child_model'),
-	eventsImport			= require('../middleware/data-migration-middleware/event_model'),
-	familiesImport			= require('../middleware/data-migration-middleware/family_model'),
-	outsideContactImport	= require('../middleware/data-migration-middleware/outside_contact_model')
-	;
+const keystone				= require( 'keystone' );
+const async					= require( 'async' );
+const agenciesImport		= require( '../middleware/data-migration-middleware/agency_model' );
+const childrenImport		= require( '../middleware/data-migration-middleware/child_model' );
+const eventsImport			= require( '../middleware/data-migration-middleware/event_model' );
+const familiesImport		= require( '../middleware/data-migration-middleware/family_model' );
+const outsideContactImport	= require( '../middleware/data-migration-middleware/outside_contact_model' );
 	
 
 exports = module.exports = function(req, res) {
     'use strict';
 
-    var view = new keystone.View(req, res),
-        locals = res.locals;
+    const view = new keystone.View( req, res );
+
+    let locals = res.locals;
 
     async.series([
-		// function(done) { outsideContactImport.importOutsideContacts(req, res, done); }
-		// function(done) { agenciesImport.importAgencies(req,res,done); }
-		// function(done) { childrenImport.importChildren(req, res, done); }
-		function(done) { eventsImport.importFamilies(req, res, done); }
-		// function(done) { familiesImport.importFamilies(req, res, done); }
+		// function(done) { outsideContactImport.importOutsideContacts( req, res, done ); },
+		// function(done) { agenciesImport.importAgencies( req, res, done ); },
+		// function(done) { childrenImport.importChildren( req, res, done ); },
+		// function(done) { familiesImport.importFamilies( req, res, done ); },
+		function(done) { eventsImport.importEvents( req, res, done ); }
 		
 	], function() {
 		// Set the layout to render without the right sidebar
-		locals['render-with-sidebar'] = false;
-		locals['render-without-header'] = true;
+		locals[ 'render-with-sidebar' ] = false;
+		locals[ 'render-without-header' ] = true;
 		// Render the view once all the data has been retrieved
-		view.render('data-migration-output');
+		view.render( 'data-migration-output' );
 
 	});
 
