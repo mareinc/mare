45000179b613d81de3dcdd5b514715c3616be2b4
Jared Collier <jared.j.collier@gmail.com>
2016-11-23 20:41:52 -0500

    #71 | change CSC Email Contact to Staff Email Contact

--- models/StaffEmailContact.js
+++ models/StaffEmailContact.js
@@ -2,14 +2,14 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 // Create model. Additional options allow menu name to be used what auto-generating URLs
-var CSCEmailContact = new keystone.List('CSC Email Contact');
+var StaffEmailContact = new keystone.List('Staff Email Contact');
 
 // Create fields
-CSCEmailContact.add({
-	cscEmailContact: { type: Types.Relationship, label: 'MARE email contact', ref: 'Admin', required: true, initial: true },
+StaffEmailContact.add({
+	staffEmailContact: { type: Types.Relationship, label: 'staff email contact', ref: 'Admin', required: true, initial: true },
 	emailTarget: { type: Types.Relationship, label: 'primary contact for', ref: 'Email Target', required: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
-CSCEmailContact.defaultColumns = 'cscEmailContact, emailTarget';
-CSCEmailContact.register();
+StaffEmailContact.defaultColumns = 'staffEmailContact, emailTarget';
+StaffEmailContact.register();
