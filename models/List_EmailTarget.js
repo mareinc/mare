789c3260af6c115accdc88f36d137f9ec2e29421
Jared Collier <jared.j.collier@gmail.com>
2016-11-23 20:44:25 -0500

    #71 | udpate CSC email target to staff email target

--- models/List_EmailTarget.js
+++ models/List_EmailTarget.js
@@ -2,15 +2,15 @@ var keystone = require('keystone'),
 	Types = keystone.Field.Types;
 
 // Create model. Additional options allow menu name to be used what auto-generating URLs
-var CSCEmailTarget = new keystone.List('Email Target', {
-	map: { name: 'cscEmailTarget' }
+var StaffEmailTarget = new keystone.List('Staff Email Target', {
+	map: { name: 'staffEmailTarget' }
 });
 
 // Create fields
-CSCEmailTarget.add({
-	cscEmailTarget: { type: Types.Text, label: 'email target', note: 'a MARE employee will be matched with this as the primary email recipient', required: true, initial: true }
+StaffEmailTarget.add({
+	staffEmailTarget: { type: Types.Text, label: 'staff email target', note: 'a MARE employee will be matched with this as the primary email recipient', required: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
-CSCEmailTarget.defaultColumns = 'cscEmailTarget';
-CSCEmailTarget.register();
+StaffEmailTarget.defaultColumns = 'staffEmailTarget';
+StaffEmailTarget.register();
