5f15da716967de0c65aaf04cfe28cff0ce49afd1
Jared Collier <jared.j.collier@gmail.com>
2016-12-04 21:12:50 -0500

    #79 | create the base view without validation for the car donation form

---
+++ public/js/views/form_car-donation.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+
+	mare.views.Form_CarDonation = Backbone.View.extend({
+		el: '.form-name',
+
+		initialize: function() {
+			// DOM cache any commonly used elements to improve performance
+
+			// Initialize parsley validation on the form
+			this.form = this.$el.parsley();
+			
+			// this.form.on( 'field:validated', this.validateForm );
+		},
+
+		validateForm: function validateForm() {
+			var ok = $( '.parsley-error' ).length === 0;
+			$( '.bs-callout-info' ).toggleClass( 'hidden', !ok );
+			$( '.bs-callout-warning' ).toggleClass( 'hidden', ok );
+		}
+
+	});
+}());
