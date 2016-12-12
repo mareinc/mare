a80bd5e5343fbad714f83de15639c4fc907899c4
Jared Collier <jared.j.collier@gmail.com>
2016-12-11 21:23:38 -0500

    #82 | create Backbone view with form validation kick-off for have a question form

--- public/js/views/form_have-a-question.js
+++ public/js/views/form_have-a-question.js
@@ -2,15 +2,13 @@
 	'use strict';
 
 	mare.views.Form_HaveAQuestion = Backbone.View.extend({
-		el: '.form-name',
+		el: '.form--have-a-question',
 
 		initialize: function() {
-			// DOM cache any commonly used elements to improve performance
-
 			// Initialize parsley validation on the form
 			this.form = this.$el.parsley();
 			
-			// this.form.on( 'field:validated', this.validateForm );
+			this.form.on( 'field:validated', this.validateForm );
 		},
 
 		validateForm: function validateForm() {
