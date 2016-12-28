b2e31883ade4243b429ac97bed5aebbc4ef58ca1
Jared Collier <jared.j.collier@gmail.com>
2016-12-28 17:16:54 -0500

    #23 | create agency id mapping middleware

---
+++ routes/middleware/data-migration-maps/agency.js
@@ -0,0 +1,23 @@
+var dataMigrationService	= require('../service_data-migration'),
+	async					= require('async');
+
+ /* agencyMap will end up looking like:
+
+    { oldId1: newId1,
+      oldId2: newId2,
+      oldId3: newId3,
+      ... }
+
+    and can be used by typing let newId = locals.agencyMap[ oldId ], where oldId is the actual id value */
+exports.mapAgencyIds = function mapAgencyIds( req, res, done ) {
+
+    let locals = res.locals;
+    locals.agencyMap = {};
+
+    async.parallel([
+        done => { dataMigrationService.getModelMap( done, { model: 'Agency', map: locals.agencyMap } ); }
+    ], () => {
+
+        done();      
+    });
+};
