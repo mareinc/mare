4408b7dd6ba3a268e98f71df5643c5897374542d
Jared Collier <jared.j.collier@gmail.com>
2016-12-29 23:06:29 -0500

    #23 | create auto generate scripts to create the website bot and migration bot
    For when you accidentally blow out all the users in the DB like I did tonight.
    
    :/

---
+++ updates/0.0.2-user_admin.js
@@ -0,0 +1,13 @@
+exports.create = {
+	Admin: [{
+		permissions: {
+			isActive: true
+		},
+		name: {
+			first: 'Website',
+			last: 'Bot'
+		},
+		password: 'admin',
+		email: 'junit83@gmail.com'
+	}]
+};
