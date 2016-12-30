76eb50c92722d1110716e9af209df01a5c002e49
Jared Collier <jared.j.collier@gmail.com>
2016-12-29 23:06:59 -0500

    #23 | clean up the way dependency models are fetched in the social worker import

--- routes/middleware/data-migration-middleware/user_socialworker_model.js
+++ routes/middleware/data-migration-middleware/user_socialworker_model.js
@@ -14,7 +14,7 @@ const csv = require( 'csvtojson' );
 // create an array to hold all social workers.  This is created here to be available to multiple functions below
 let socialWorkers = [];
 
-module.exports.importSocialWorker = (req, res, done) => {
+module.exports.importSocialWorker = ( req, res, done ) => {
 
 	csv().fromFile( csvFilePath )
 		.on( 'end_parsed', socialWorkersArray => {
@@ -43,61 +43,70 @@ module.exports.generateSocialWorkers = function* generateSocialWorkers() {
 }
 
 // a function paired with the generator to create a record and request the generator to process the next once finished
-module.exports.createSocialWorkerRecord = ( socialWorker ) => {
-	// this fetching of agency could be wrapped in an async which may make it easier to understand
-	// NOTE: DO NOT FOLLOW THIS EXAMPLE WHEN YOU NEED TO FETCH MORE THAN ONE MODEL FOR SET UP
-	Agency.model.findOne()
-		.where( 'oldId', socialWorker.agn_id )
-		.exec()
-		.then( agency => {
-			// populate instance for SocialWorker object
-			let newSocialWorker = new SocialWorker.model({
+module.exports.createSocialWorkerRecord = socialWorker => {
+	// create a placeholder for the agency we're going to fetch related to the current social worker
+	let agency = undefined;
+	
+	async.series([
+		// fetch the agency associated with the social worker before attempting to create the record
+		done => {
+			Agency.model.findOne()
+				.where( 'oldId', socialWorker.agn_id )
+				.exec()
+				.then( retrievedAgency => {
+					agency = retrievedAgency;
+					done();
+				 });
+		}
+	], () => {
+		// populate instance for SocialWorker object
+		let newSocialWorker = new SocialWorker.model({
+			// every social worker needs a password, this will generate one we can easily determine while still being unique
+			password: `${ socialWorker.first_name }_${ socialWorker.last_name }_${ socialWorker.agc_id }`,
 
-				password: `${ socialWorker.first_name }_${ socialWorker.last_name }_${ socialWorker.agc_id }`,
+			permissions: {
+				isVerified: socialWorker.email ? true : false,			// they can only have verified their email address if they have one
+				isActive: socialWorker.is_active === 'Y' ? true : false
+			},
 
-				permissions: {
-					isVerified: true,
-					isActive: socialWorker.is_active === 'Y' ? true : false
-				},
+			name: {
+				first: socialWorker.first_name,
+				last: socialWorker.last_name
+			},
+			// TODO: every social worker needs an email address, this is just a placeholder until Lisa tells us how to handle these records
+			email: socialWorker.email.toLowerCase() || `placeholder${ socialWorker.agc_id }@email.com`,
 
-				name: {
-					first: socialWorker.first_name,
-					last: socialWorker.last_name
-				},
+			phone: {
+				work: socialWorker.phone
+			},
 
-				email: socialWorker.email.toLowerCase() || `placeholder${ socialWorker.agc_id }@email.com`,
+			agency: agency._id,
+			
+			address: {
+				street1: agency.address.street1,
+				street2: agency.address.street2,
+				city: agency.address.city,
+				state: agency.address.state,
+				zipCode: agency.address.zipCode,
+				region: agency.address.region
+			},
 
-				phone: {
-					work: socialWorker.phone
-				},
-
-				agency: agency._id,
-				
-				address: {
-					street1: agency.address.street1,
-					street2: agency.address.street2,
-					city: agency.address.city,
-					state: agency.address.state,
-					zipCode: agency.address.zipCode,
-					region: agency.address.region
-				},
-
-				notes: socialWorker.notes,
-				oldId: socialWorker.agc_id
-			});
+			notes: socialWorker.notes,
+			oldId: socialWorker.agc_id
+		});
 
 
-			newSocialWorker.save(function( err ) {
-				if( err ) {
-					console.log( `[ID#${ socialWorker.agc_id }] an error occured while saving ${ socialWorker.first_name } ${ socialWorker.last_name }.` );
-				} else {
-					console.log( `[ID#${ socialWorker.agc_id }] ${ socialWorker.first_name } ${ socialWorker.last_name } successfully saved!` );
-				}
-				// fire off the next iteration of our generator now that the record has been saved
-				socialWorkerGenerator.next();
-			});
-		})
-}
+		newSocialWorker.save(function( err ) {
+			if( err ) {
+				console.log( `[ID#${ socialWorker.agc_id }] an error occured while saving ${ socialWorker.first_name } ${ socialWorker.last_name }.` );
+			} else {
+				console.log( `[ID#${ socialWorker.agc_id }] ${ socialWorker.first_name } ${ socialWorker.last_name } successfully saved!` );
+			}
+			// fire off the next iteration of our generator now that the record has been saved
+			socialWorkerGenerator.next();
+		});
+	});
+};
 
 // instantiates the generator used to create social worker records one at a time ( preventing a memory leak issue )
 const socialWorkerGenerator = exports.generateSocialWorkers();
