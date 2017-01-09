8d09b18594e6a8ca172f87cdfe6276d3376b955a
Jared Collier <jared.j.collier@gmail.com>
2017-01-09 08:59:38 -0500

    #21 | clean up data migration view and add new dependencies

--- routes/views/dataMigration.js
+++ routes/views/dataMigration.js
@@ -1,40 +1,51 @@
-const keystone				= require( 'keystone' );
-const async					= require( 'async' );
-const agenciesImport		= require( '../middleware/data-migration-middleware/agency_model' );
-const childrenImport		= require( '../middleware/data-migration-middleware/child_model' );
-const eventsImport			= require( '../middleware/data-migration-middleware/event_model' );
-const inquiriesImport		= require( '../middleware/data-migration-middleware/inqury_model' );
-const internalNotesImport	= require( '../middleware/data-migration-middleware/internal_note_model' );
-const familiesImport		= require( '../middleware/data-migration-middleware/family_model' );
-const mailingListsImport    = require( '../middleware/data-migration-middleware/mailing_list_model');
-const placementsImport		= require( '../middleware/data-migration-middleware/placement_model' );
-const outsideContactImport	= require( '../middleware/data-migration-middleware/outside_contact_model' );
-const socialWorkerImport	= require( '../middleware/data-migration-middleware/user_socialworker_model' );
+const keystone					= require( 'keystone' );
+const async						= require( 'async' );
+const agenciesImport			= require( '../middleware/data-migration-middleware/import-agencies' );
+const childrenImport			= require( '../middleware/data-migration-middleware/import-children' );
+const eventsImport				= require( '../middleware/data-migration-middleware/import-events' );
+const inquiriesExtranetImport	= require( '../middleware/data-migration-middleware/import-inquiries-extranet' );
+const inquiriesCallImport		= require( '../middleware/data-migration-middleware/import-inquiries-calls');
+const internalNotesImport		= require( '../middleware/data-migration-middleware/import-internal-notes' );
+const familiesImport			= require( '../middleware/data-migration-middleware/import-families' );
+const mailingListsImport    	= require( '../middleware/data-migration-middleware/import-mailing-lists');
+const placementsImport			= require( '../middleware/data-migration-middleware/import-placements' );
+const outsideContactImport		= require( '../middleware/data-migration-middleware/import-outside-contacts' );
+const socialWorkerImport		= require( '../middleware/data-migration-middleware/import-social-workers' );
 // id mappings between systems
-const statesMap				= require( '../middleware/data-migration-maps/state' );
-	
+const statesMap					= require( '../middleware/data-migration-maps/state' );
+const regionsMap				= require( '../middleware/data-migration-maps/region' );
+const outsideContactGroupsMap	= require( '../middleware/data-migration-maps/outside-contact-group' );
 
-exports = module.exports = function(req, res) {
+exports = module.exports = ( req, res ) => {
     'use strict';
 
     const view = new keystone.View( req, res );
 
-    let locals = res.locals;
+    const locals = res.locals;
+	// create a namespace for all migration data needed for the import
+	// this makes clearing memory much easier
+	locals.migrationData = locals.migration || {
+		maps: {},
+		data: {}
+	};
 
     async.series([
-		// done => { statesMap.getStatesMap(req, res, done) },
-		// done => { agenciesImport.importAgencies( req, res, done ); }, 			   // 1
-		// done => { outsideContactImport.importOutsideContacts( req, res, done ); }   // 2
-		// done => { socialWorkerImport.importSocialWorker( req, res, done ); }        // 3
-		// done => { childrenImport.importChildren( req, res, done ); },			   // 4
-		done => { familiesImport.importFamilies( req, res, done ); },			   // 5
-		// done => { placementsImport.importPlacements( req, res, done ); },		   // 6
-		// done => { inquiriesImport.importInquiries( req, res, done ); },		       // 7
-		// done => { eventsImport.importEvents( req, res, done ); }					   // 8
-		// done => { mailingListsImport.importMailingLists( req, res, done ); }		   // 9
-		// done => { internalNotesImport.importInternalNotes( req, res, done ); }	   // 10
+		done => { statesMap.getStatesMap(req, res, done); },
+		done => { regionsMap.getRegionsMap( req, res, done ); },
+		done => { outsideContactGroupsMap.getOutsideContactGroupsMap( req, res, done ) },
+		// done => { agenciesImport.importAgencies( req, res, done ); },
+		done => { outsideContactImport.importOutsideContacts( req, res, done ); },
+		// done => { socialWorkerImport.importSocialWorkers( req, res, done ); },
+		// done => { childrenImport.importChildren( req, res, done ); },
+		// done => { familiesImport.importFamilies( req, res, done ); },
+		// done => { placementsImport.importPlacements( req, res, done ); },
+		// done => { inquiriesExtranetImport.importInquiries( req, res, done ); },
+		// done => { inquiriesCallImport.importInquiries( req, res, done ); },
+		// done => { eventsImport.importEvents( req, res, done ); },
+		// done => { mailingListsImport.importMailingLists( req, res, done ); },
+		// done => { internalNotesImport.importInternalNotes( req, res, done ); },
 		
-	], function() {
+	], () => {
 		// Set the layout to render without the right sidebar
 		locals[ 'render-with-sidebar' ] = false;
 		locals[ 'render-without-header' ] = true;
