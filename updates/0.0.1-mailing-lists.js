e52e14cbfc0753b2f536b6862f5890a50e89220a
Jared Collier <jared.j.collier@gmail.com>
2017-01-09 10:29:09 -0500

    create automatic model generation script for Mailing Lists

---
+++ updates/0.0.1-mailing-lists.js
@@ -0,0 +1,69 @@
+exports.create = {
+	'Mailing List': [{
+		mailingList: '2007 family follow-up project'
+	}, {
+		mailingList: 'adoption mixer families 2002'
+	}, {
+		mailingList: 'adoption parties'
+	}, {
+		mailingList: 'African American families'
+	}, {
+		mailingList: 'AUKFY 2015'
+	}, {
+		mailingList: 'board of directors'
+	}, {
+		mailingList: 'Boston coalition'
+	}, {
+		mailingList: 'contracted adoption sups'
+	}, {
+		mailingList: 'contracted adoption workers'
+	}, {
+		mailingList: 'contracted exec. directors'
+	}, {
+		mailingList: 'DCF adoption supervisors'
+	}, {
+		mailingList: 'DCF adoption workers'
+	}, {
+		mailingList: 'DCF area/reg. dir. & APMs'
+	}, {
+		mailingList: 'e-mail adoption parties'
+	}, {
+		mailingList: 'e-mail newsletter'
+	}, {
+		mailingList: 'e-mail single parent matching'
+	}, {
+		mailingList: 'experienced families'
+	}, {
+		mailingList: 'family pages'
+	}, {
+		mailingList: 'former board of directors'
+	}, {
+		mailingList: 'fundraising'
+	}, {
+		mailingList: 'Heart Gallery 2005'
+	}, {
+		mailingList: 'Heart Gallery photographers'
+	}, {
+		mailingList: 'honorary board of directors'
+	}, {
+		mailingList: 'Latino families'
+	}, {
+		mailingList: 'LGBT families'
+	}, {
+		mailingList: 'newsletters'
+	}, {
+		mailingList: 'non-DCF/contracted contacts'
+	}, {
+		mailingList: 'out of country newsletters'
+	}, {
+		mailingList: 'photolisting pages'
+	}, {
+		mailingList: 'single parent matching night'
+	}, {
+		mailingList: 'statewide recruitment'
+	}, {
+		mailingList: 'video libraries'
+	}, {
+		mailingList: 'volunteers'
+	}]
+};
