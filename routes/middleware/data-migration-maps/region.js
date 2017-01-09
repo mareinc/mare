2cf5e1b36324a81aa5820873f3a1449b445c188d
Jared Collier <jared.j.collier@gmail.com>
2017-01-09 09:04:23 -0500

    #21 | fix regions mapping

--- routes/middleware/data-migration-maps/region.js
+++ routes/middleware/data-migration-maps/region.js
@@ -1,36 +1,27 @@
-var dataMigrationService	= require('../service_data-migration'),
-	async					= require('async');
+var dataMigrationService	= require( '../service_data-migration' ),
+	async					= require( 'async' );
 
-exports.getRegionsMap = function getRegionsMap(req, res, done) {
+exports.getRegionsMap = ( req, res, done ) => {
+
+	console.log( `fetching regions map` );
 
 	var locals = res.locals;
+	// create an area in locals for the regions map
+	locals.migration.maps.regions = {};
 
 	async.parallel([
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'western', returnTarget: 'regionWestern'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'central', returnTarget: 'regionCentral'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'other', returnTarget: 'regionContractedAgencies'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'northern', returnTarget: 'regionNorthern'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'southern', returnTarget: 'regionSoutheast'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'Boston', returnTarget: 'regionBoston'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'other', returnTarget: 'regionMetro'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'out of state', returnTarget: 'regionOutofState'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Region', targetField: 'region', targetValue: 'other', returnTarget: 'regionWPrivateAgency'  }); }
-		
-	], function(err, results) {
 
-		locals.regionsMap = {
-			
-			1000: locals.regionWestern,
-			1001: locals.regionCentral,
-			1002: locals.regionContractedAgencies,
-			1003: locals.regionNorthern,
-			1004: locals.regionSoutheast,
-			1005: locals.regionNorthern,
-			1006: locals.regionMetro,
-			1007: locals.regionOutofState,
-			1008: locals.regionPrivateAgency
-		};
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'western', mapTo: [ 1000 ], namespace: locals.migration.maps.regions }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'central', mapTo: [ 1001 ], namespace: locals.migration.maps.regions }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'other', mapTo: [ 1002, 1006, 1008 ], namespace: locals.migration.maps.regions }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'northern', mapTo: [ 1003 ], namespace: locals.migration.maps.regions }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'southern', mapTo: [ 1004 ], namespace: locals.migration.maps.regions }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'Boston', mapTo: [ 1005 ], namespace: locals.migration.maps.regions }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Region', field: 'region', value: 'out of state', mapTo: [ 1007 ], namespace: locals.migration.maps.regions }, done ); }
 
+	], () => {
+
+		console.log( `regions map set` );
 		done();
 	});
 }
