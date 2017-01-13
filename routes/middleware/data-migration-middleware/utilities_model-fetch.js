9deca1ed536046deb50a634f62b10de5795d2c8c
Jared Collier <jared.j.collier@gmail.com>
2017-01-13 14:47:30 -0500

    #23 | add model fetch function to get an agency by oldId

--- routes/middleware/data-migration-middleware/utilities_model-fetch.js
+++ routes/middleware/data-migration-middleware/utilities_model-fetch.js
@@ -1,6 +1,26 @@
-/* fetch functions to fetch model information for use during the migration */
+/* functions to fetch model information for use during the migration */
 
-/*  */
-module.exports.sdf = () => {
+const Agency = keystone.list( 'Agency' );
 
+module.exports.getAgencyById = ( resolve, reject, agencyId ) => {
+
+    Agency.model.findOne()
+        .where( 'oldId', agencyId )
+        .exec()
+        .then( retrievedAgency => {
+            // if no agency was found
+			if( !retrievedAgency ) {
+				// log the issue
+				console.error( `error fetching agency by oldId ${ agencyId }` );
+				// and reject the promise
+				reject();
+			}
+            // otherwise, accept the promise and pass back the retrieved agency
+            resolve( retrievedAgency );
+
+        }, err => {
+
+            console.error( `error in getModelId() ${ err }` );
+			done();
+        });
 }
