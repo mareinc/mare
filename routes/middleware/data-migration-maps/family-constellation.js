f15b2ac516cd3b159e87092ecb68713a0170be40
Jared Collier <jared.j.collier@gmail.com>
2017-01-15 12:33:13 -0500

    #23 | rewrite the family constellation map middleware

--- routes/middleware/data-migration-maps/family-constellation.js
+++ routes/middleware/data-migration-maps/family-constellation.js
@@ -1,37 +1,29 @@
-var dataMigrationService	= require('../service_data-migration'),
-	async					= require('async');
+var dataMigrationService	= require( '../service_data-migration' ),
+	async					= require( 'async' );
 
-exports.getFamilyConstellationsMap = function getFamilyConstellationsMap(req, res, done) {
+exports.getFamilyConstellationsMap = ( req, res, done ) => {
+
+	console.log( `fetching family constellations map` );
 
 	var locals = res.locals;
+	// create an area in locals for the family constellations map
+	locals.migration.maps.familyConstellations = {};
 
 	async.parallel([
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'female/female couple', returnTarget: 'femaleFemaleCouple'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'male/female couple', returnTarget: 'maleFemaleCouple'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'male/male couple', returnTarget: 'maleMaleCouple'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'single female', returnTarget: 'singleFemale'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'single gay female', returnTarget: 'singleGayFemale'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'single gay male', returnTarget: 'singleGayMale'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'single male', returnTarget: 'singleMale'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'single straight female', returnTarget: 'singleStraightFemale'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'single straight male', returnTarget: 'singleStraightMale'  }); },
-		function(done) { dataMigrationService.getModelId(req, res, done, { model: 'Family Constellation', targetField: 'familyConstellation', targetValue: 'unknown', returnTarget: 'unknown'  }); }
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'female/female couple', mapTo: [ 'FFC' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'male/female couple', mapTo: [ 'MFC' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'male/male couple', mapTo: [ 'MMC' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'single female', mapTo: [ 'SIF' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'single gay female', mapTo: [ 'SGF' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'single gay male', mapTo: [ 'SGM' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'single male', mapTo: [ 'SIM' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'single straight female', mapTo: [ 'SSF' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'single straight male', mapTo: [ 'SSM' ], namespace: locals.migration.maps.familyConstellations }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Family Constellation', field: 'familyConstellation', value: 'unknown', mapTo: [ 'UUU' ], namespace: locals.migration.maps.familyConstellations }, done ); }
 		
-	], function() {
+	], () => {
 
-		locals.familyConstellationsMap = {
-			"FFC" : locals.femaleFemaleCouple,
-			"MFC" : locals.maleFemaleCouple,
-			"MMC" : locals.maleMaleCouple,
-			"SIF" : locals.singleFemale,
-			"SGF" : locals.singleGayFemale,
-			"SGM" : locals.singleGayMale,
-			"SIM" : locals.singleMale,
-			"SSF" : locals.singleStraightFemale,
-			"SSM" : locals.singleStraightMale,
-			"UUU" : locals.unknown
-		};
-
+		console.log( `family constellations map set` );
 		done();
 	});
 }
