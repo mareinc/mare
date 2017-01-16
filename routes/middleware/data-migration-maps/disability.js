cf7679d20afc8abdd2c9ce1a52bfb44eb041695f
Jared Collier <jared.j.collier@gmail.com>
2017-01-15 21:15:21 -0500

    #23 | update the disability mapping middleware

--- routes/middleware/data-migration-maps/disability.js
+++ routes/middleware/data-migration-maps/disability.js
@@ -1,16 +1,26 @@
+var dataMigrationService	= require( '../service_data-migration' ),
+	async					= require( 'async' );
+
 exports.getDisabilitiesMap = ( req, res, done ) => {
 
-	console.log( `setting disabilities map` );
+	console.log( `fetching disabilities map` );
 
 	var locals = res.locals;
-	// create an area in locals for the disabilities map
-	locals.migration.maps.disabilities = {
-		10: 'none',
-		20: 'mild',
-		30: 'moderate',
-		40: 'severe'
-	};
+	// create an area in locals for the states map
+	locals.migration.maps.disabilities = {};
 
-	console.log( `disabilities map set` );
-	done();
-};
+	async.parallel([
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'HIV/AIDS', mapTo: [ 30 ], namespace: locals.migration.maps.disabilities }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'autism spectrum', mapTo: [ 70 ], namespace: locals.migration.maps.disabilities }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'cerebral palsy', mapTo: [ 20 ], namespace: locals.migration.maps.disabilities }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'down syndrome', mapTo: [ 10 ], namespace: locals.migration.maps.disabilities }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'fetal alcohol syndrome', mapTo: [ 60 ], namespace: locals.migration.maps.disabilities }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'hearing impairment', mapTo: [ 40 ], namespace: locals.migration.maps.disabilities }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Disability', field: 'disability', value: 'visual impairment', mapTo: [ 50 ], namespace: locals.migration.maps.disabilities }, done ); }
+	
+	], () => {
+
+		console.log( `disabilities map set` );
+		done();
+	});
+}
