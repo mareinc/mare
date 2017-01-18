b32478abb39b4cdfdaeee9eb60abcd3a38d33388
Jared Collier <jared.j.collier@gmail.com>
2017-01-17 22:21:03 -0500

    #23 | create social worker middleware for saving adoption and recruitment worker fields

---
+++ routes/middleware/service_social-worker.js
@@ -0,0 +1,20 @@
+ const keystone     = require( 'keystone' );
+ const SocialWorker = keystone.list( 'Social Worker' );
+ 
+ // TODO: this implementation is ugly, it needs to be rewritten in a non-crappy way
+ exports.getSocialWorkerById = ( id, container, field, done ) => { 
+    // if the id isn't set
+	if( !id ) {
+        // return control to the calling context
+        return done();
+    }
+    // fetch the social worker record
+    SocialWorker.model.findById( id )
+        .exec()
+        .then( socialWorker => {
+            // set the field from the outer context to the social worker
+            container[ field ] = socialWorker;
+            // return control to the parent context
+            done();
+        });
+ }
