84f995d8e4e5fb2c7ca876c95fa754801512128d
Jared Collier <jared.j.collier@gmail.com>
2017-01-27 14:14:58 -0500

    #23 | update language map with new outliers from the family migration

--- routes/middleware/data-migration-maps/language.js
+++ routes/middleware/data-migration-maps/language.js
@@ -11,12 +11,12 @@ exports.getLanguagesMap = ( req, res, done ) => {
 
 	async.parallel([
 		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'Chinese', mapTo: [ 'chinese', 'cantonese', 'mandarin chinese' ], namespace: locals.migration.maps.languages }, done ); },
-		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'English', mapTo: [ 'english', 'eng' ], namespace: locals.migration.maps.languages }, done ); },
-		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'Portuguese', mapTo: [ 'portuguese' ], namespace: locals.migration.maps.languages }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'English', mapTo: [ 'english', 'englilsh', 'engish', 'eng', 'bilingual' ], namespace: locals.migration.maps.languages }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'Portuguese', mapTo: [ 'portuguese', 'portugese' ], namespace: locals.migration.maps.languages }, done ); },
 		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'Russian', mapTo: [ 'russian' ], namespace: locals.migration.maps.languages }, done ); },
-		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'Spanish', mapTo: [ 'spanish', 'spanis', 'span', 'spa', 'sp' ], namespace: locals.migration.maps.languages }, done ); },
-		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'French', mapTo: [ 'creole' ], namespace: locals.migration.maps.languages }, done ); },
-		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'other', mapTo: [ 'other', 'laos', 'khmer', 'grebo' ], namespace: locals.migration.maps.languages }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'Spanish', mapTo: [ 'spanish', 'sapanish', 'spansih', 'spanis', 'spanih', 'span', 'spa', 'sp' ], namespace: locals.migration.maps.languages }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'French', mapTo: [ 'french', 'creole', 'french creole' ], namespace: locals.migration.maps.languages }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'other', mapTo: [ 'other', 'arabic', 'laos', 'khmer', 'grebo', 'greek', 'lebanese' ], namespace: locals.migration.maps.languages }, done ); },
 		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'non-verbal', mapTo: [ 'non-verbal', 'nonverbal', 'english (non-verbal)' ], namespace: locals.migration.maps.languages }, done ); },
 		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'sign language', mapTo: [ 'sign language' ], namespace: locals.migration.maps.languages }, done ); },
 		done => { dataMigrationService.getModelId( { model: 'Language', field: 'language', value: 'ASL', mapTo: [ 'asl' ], namespace: locals.migration.maps.languages }, done ); }
