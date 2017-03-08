8482141f69ae06fc3d3f6156b31b64bea7f82b85
Jared Collier <jared.j.collier@gmail.com>
2017-03-08 12:17:37 -0500

    #23 | create inquiry method map

---
+++ routes/middleware/data-migration-maps/inquiry-method.js
@@ -0,0 +1,22 @@
+var dataMigrationService	= require( '../service_data-migration' ),
+	async					= require( 'async' );
+
+exports.getInquiryMethodsMap = ( req, res, done ) => {
+
+	console.log( `fetching inquiry methods map` );
+
+	var locals = res.locals;
+	// create an area in locals for the inquiry methods map
+	locals.migration.maps.inquiryMethods = {};
+
+	async.parallel([
+		done => { dataMigrationService.getModelId( { model: 'Inquiry Method', field: 'inquiryMethod', value: 'email', mapTo: [ 'E' ], namespace: locals.migration.maps.inquiryMethods }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Inquiry Method', field: 'inquiryMethod', value: 'in person', mapTo: [ 'I' ], namespace: locals.migration.maps.inquiryMethods }, done ); },
+		done => { dataMigrationService.getModelId( { model: 'Inquiry Method', field: 'inquiryMethod', value: 'phone', mapTo: [ 'P' ], namespace: locals.migration.maps.inquiryMethods }, done ); }
+		
+	], () => {
+
+		console.log( `inquiry methods map set` );
+		done();
+	});
+};
