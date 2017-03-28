5f38cf5feb70f7a1c35e7e3a056f62d0bf338600
Jared Collier <jared.j.collier@gmail.com>
2017-03-28 08:03:04 -0400

    #161 | make adoption preferences required only when ‘gathering information’ is checked

--- public/js/views/registration-family.js
+++ public/js/views/registration-family.js
@@ -10,7 +10,8 @@
 			'change .homestudy-completed-checkbox'	: 'toggleHomestudySection',
 			'change #upload-button'					: 'uploadForm',
 			'change .info-packet-toggle'			: 'toggleInfoPacketDetailsSection',
-			'change #children-in-home'				: 'toggleFamilyDetailsForm'
+			'change #children-in-home'				: 'toggleFamilyDetailsForm',
+			'change #gathering-information-checkbox': 'toggleAdoptionPreferencesSection'
 		},
 
 		initialize: function() {
@@ -19,18 +20,39 @@
 			// Compile the template to be used adding/removing child in home field groups
 			this.template = Handlebars.compile(childInHomeHtml);
 			// DOM cache any commonly used elements to improve performance
-			this.$state							= this.$('#family-state');
-			this.$homestudyCompletionDate		= this.$('#homestudy-date-complete');
-			this.$socialWorkerName				= this.$('#social-worker-name');
-			this.$socialWorkerAgency			= this.$('#social-worker-agency');
-			this.$socialWorkerPhone				= this.$('#social-worker-phone');
-			this.$socialWorkerEmail				= this.$('#social-worker-email');
-			this.$homestudySection				= this.$('.family-submit-your-homestudy-section');
-			this.$homestudySubmissionSection	= this.$('.family-homestudy-details-section');
-			this.$howDidYouHearOther			= this.$('#family-how-did-you-hear-other');
-			this.$infoPacketDetails				= this.$('.info-packet-details');
-			this.$childrenInHome 				= this.$('#children-in-home');
-			this.$childrenInHomeDetails 		= this.$('.children-in-home-details');
+			this.$state									= this.$( '#family-state' );
+			this.$homestudyCompletionDate				= this.$( '#homestudy-date-complete' );
+			this.$socialWorkerName						= this.$( '#social-worker-name' );
+			this.$socialWorkerAgency					= this.$( '#social-worker-agency' );
+			this.$socialWorkerPhone						= this.$( '#social-worker-phone' );
+			this.$socialWorkerEmail						= this.$( '#social-worker-email' );
+			this.$homestudySection						= this.$( '.family-submit-your-homestudy-section' );
+			this.$homestudySubmissionSection			= this.$( '.family-homestudy-details-section' );
+			this.$howDidYouHearOther					= this.$( '#family-how-did-you-hear-other' );
+			this.$infoPacketDetails						= this.$( '.info-packet-details' );
+			this.$childrenInHome 						= this.$( '#children-in-home' );
+			this.$childrenInHomeDetails 				= this.$( '.children-in-home-details' );
+			this.$gatheringInformationCheckbox			= this.$( '#gathering-information-checkbox' );
+			this.$preferredGenderLabel					= this.$( '.preferred-gender-label' );
+			this.$preferredGender						= this.$( '.preferred-gender' );
+			this.$legalStatusLabel						= this.$( '.legal-status-label' );
+			this.$legalStatus							= this.$( '.legal-status' );
+			this.$ageRangeLabel							= this.$( '.age-range-label' );
+			this.$ageRangeFrom							= this.$( '.age-range-from' );
+			this.$ageRangeTo							= this.$( '.age-range-to' );
+			this.$numberOfChildrenPreferredLabel		= this.$( '.number-of-children-preferred-label' );
+			this.$numberOfChildrenPreferred 			= this.$( '.number-of-children-preferred' );
+			this.$contactWithBiologicalSiblingsLabel	= this.$( '.contact-with-biological-siblings-label' );
+			this.$contactWithBiologicalSiblings			= this.$( '.contact-with-biological-siblings' );
+			this.$raceLabel								= this.$( '.race-label' );
+			this.$race									= this.$( '.race' );
+			this.$maximumPhysicalNeedsLabel				= this.$( '.maximum-physical-needs-label' );
+			this.$maximumPhysicalNeeds					= this.$( '.maximum-physical-needs' );
+			this.$maximumEmotionalNeedsLabel			= this.$( '.maximum-emotional-needs-label' );
+			this.$maximumEmotionalNeeds					= this.$( '.maximum-emotional-needs' );
+			this.$maximumIntellectualNeedsLabel			= this.$( '.maximum-intellectual-needs-label' );
+			this.$maximumIntellectualNeeds				= this.$( '.maximum-intellectual-needs' );
+
 			// Initialize parsley validation on the form
 			this.form = this.$el.parsley();
 
@@ -139,6 +161,41 @@
 					$('.child' + i + '-form-heading').remove(); // TODO: Include the heading as part of the form to make cleanup easier
 				}
 			}
+		},
+		/* toggles form validation on the adoption preferences fields.  They are only required when 'gathering information' is checked */
+		toggleAdoptionPreferencesSection: function toggleAdoptionPreferencesSection() {
+			// toggle the required-field class on the label to hide/show the red * after it
+			this.$preferredGenderLabel.toggleClass( 'required-field' );
+			this.$legalStatusLabel.toggleClass( 'required-field' );
+			this.$ageRangeLabel.toggleClass( 'required-field' );
+			this.$numberOfChildrenPreferredLabel.toggleClass( 'required-field' );
+			this.$contactWithBiologicalSiblingsLabel.toggleClass( 'required-field' );
+			this.$raceLabel.toggleClass( 'required-field' );
+			this.$maximumPhysicalNeedsLabel.toggleClass( 'required-field' );
+			this.$maximumEmotionalNeedsLabel.toggleClass( 'required-field' );
+			this.$maximumIntellectualNeedsLabel.toggleClass( 'required-field' );
+			// toggle the required class on the input text to display the red text during form validation
+			this.$preferredGender.toggleClass( 'required' );
+			this.$legalStatus.toggleClass( 'required' );
+			this.$ageRangeFrom.toggleClass( 'required' );
+			this.$ageRangeTo.toggleClass( 'required' );
+			this.$numberOfChildrenPreferred.toggleClass( 'required' );
+			this.$contactWithBiologicalSiblings.toggleClass( 'required' );
+			this.$race.toggleClass( 'required' );
+			this.$maximumPhysicalNeeds.toggleClass( 'required' );
+			this.$maximumEmotionalNeeds.toggleClass( 'required' );
+			this.$maximumIntellectualNeeds.toggleClass( 'required' );
+			// toggle the required attribute needed to hide/show the red background during form validation
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$preferredGender.attr( 'required', 'true' ) 				: this.$preferredGender.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$legalStatus.attr( 'required', 'true' )					: this.$legalStatus.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$ageRangeFrom.attr( 'required', 'true' )					: this.$ageRangeFrom.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$ageRangeTo.attr( 'required', 'true' )					: this.$ageRangeTo.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$numberOfChildrenPreferred.attr( 'required', 'true' )	: this.$numberOfChildrenPreferred.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$contactWithBiologicalSiblings.attr( 'required', 'true' ): this.$contactWithBiologicalSiblings.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$race.attr( 'required', 'true' )							: this.$race.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$maximumPhysicalNeeds.attr( 'required', 'true' )			: this.$maximumPhysicalNeeds.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$maximumEmotionalNeeds.attr( 'required', 'true' )		: this.$maximumEmotionalNeeds.attr( 'required', 'false' );
+			this.$gatheringInformationCheckbox.is( ':checked' ) ? this.$maximumIntellectualNeeds.attr( 'required', 'true' )		: this.$maximumIntellectualNeeds.attr( 'required', 'false' );
 		},
 		// TODO: This needs to be cleaned up a bit, both logic for efficiency and the creation should be handled in a template instead of jQuery.
 		generateChildDetailInputs: function generateChildDetailInputs(selectedNumberOfChildren) {
