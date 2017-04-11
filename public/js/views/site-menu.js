d760b1c4c3556913fca76dac3fe819cf2c2eb701
Anna Kutch <annakutch@gmail.com>
2017-04-10 20:48:38 -0400

    #34 Add logic to show the submenu items on click - dynamically calculate left padding of submenu items - remove jump from the submenu hover indicator

--- public/js/views/site-menu.js
+++ public/js/views/site-menu.js
@@ -2,11 +2,12 @@
 	'use strict';
 
 	mare.views.SiteMenu = Backbone.View.extend({
-		el: '.top-nav',
+		el: '.global-header',		// I changed this... why did I have to change this?
 
 		events: {
 			'click .log-in-link'		: 'logIn',
-			'click .log-in-container'	: 'fixClickPropagation' // TODO: This is a hacky mess, change the top nav for better markup and JavaScript
+			'click .log-in-container'	: 'fixClickPropagation', // TODO: This is a hacky mess, change the top nav for better markup and JavaScript
+			'click .main-nav__item'		: 'toggleMenuExpand'
 		},
 
 		initialize: function() {
@@ -20,6 +21,27 @@
 
 		fixClickPropagation: function fixClickPropagation(event) {
 			event.stopPropagation();
+		},
+
+		toggleMenuExpand: function toggleMenuExpand(event) {
+			// remove previously active class
+			$('.active').removeClass('active');
+
+			// add active class to this menu element
+			var $target = $(event.currentTarget);
+			$target.addClass('active');
+
+			// calculate the left padding of the submenu
+			var distFromLeft = $target.offset().left;
+
+			// set the left padding of the submenu
+			$target.children('.main-nav__items--submenu').css('padding-left', distFromLeft);
+
+			// TODO: if there is not enough room to show the menus underneath, 
+			// align right and give more space for the items...
+
+			// TODO: figure out how to keep the active class on even when the page refreshes?
+			
 		}
 
 	});
