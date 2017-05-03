43e245368fe66c11e4653ac42b58f7855256f9fe
Jared Collier <jared.j.collier@gmail.com>
2017-05-02 21:09:43 -0400

    #238 | remove section header middleware

--- routes/middleware/service_page.js
+++ routes/middleware/service_page.js
@@ -1,7 +1,6 @@
 var keystone			= require('keystone'),
 	async				= require('async'),
 	Page				= keystone.list('Page'),
-	SectionHeader		= keystone.list('Section Header'),
 	eventService		= require('./service_event'),
 	successStoryService	= require('./service_success-story');
 
@@ -24,35 +23,6 @@ exports.getPageByUrl = function getPageByUrl(req, res, done, url) {
 					done();
 
 				});
-};
-
-exports.getSectionHeader = function getSectionHeader(req, res, done, header) {
-
-	var locals = res.locals;
-
-	SectionHeader.model.findOne()
-				.where('header', header)
-				.exec()
-				.then(function (header) {
-
-					locals.sectionHeader = header;
-					// execute done function if async is used to continue the flow of execution
-					done()
-
-				}, function(err) {
-
-					console.log(err);
-					done();
-
-				});
-};
-
-exports.getPageSectionHeader = function getPageSectionHeader(req, res, done) {
-
-	var locals	= res.locals,
-		section	= locals.pageSectionMapping[req.originalUrl];
-
-	exports.getSectionHeader(req, res, done, section);
 };
 
 exports.populateSidebar = function populateSidebar(req, res, done) {
