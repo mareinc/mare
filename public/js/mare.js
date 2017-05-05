0fc1022c3542afc7a22d733beed978c034c37edb
Jared Collier <jared.j.collier@gmail.com>
2017-05-05 18:55:42 -0400

    #238 | update global header to be position fixed all the time

--- public/js/mare.js
+++ public/js/mare.js
@@ -14,50 +14,50 @@
 
         	storeUrlInfo: function storeUrlInfo() {
 				var host		= window.location.host;
-				var pathName	=  window.location.pathname.substr(1); // get the pathname without the leading '/'
+				var pathName	=  window.location.pathname.substr( 1 ); // get the pathname without the leading '/'
 				var pathArray	= pathName.split( '/' );
 
 				// Store relevant url information in mare namespace
 				mare.url.protocol	= window.location.protocol;
-				mare.url.siteArea	= pathArray[0];
-				mare.url.page		= pathArray[1];
+				mare.url.siteArea	= pathArray[ 0 ];
+				mare.url.page		= pathArray[ 1 ];
         	},
 
 			bindTouch: function bindTouch() {
 				// Bind adding of 'hover' class to tap vs hover depending on whether it is a touch enabled device
-				if (Modernizr.touch) {
-					$(".nav li").unbind('mouseenter mouseleave');
-					$(".nav li a.parent").unbind('click');
-					$(".nav li .more").unbind('click').bind('click', function() {
-						$(this).parent("li").toggleClass("hover");
+				if( Modernizr.touch ) {
+					$( '.nav li' ).unbind('mouseenter mouseleave');
+					$( '.nav li a.parent' ).unbind( 'click' );
+					$( '.nav li .more' ).unbind( 'click' ).bind( 'click', function() {
+						$( this ).parent( 'li' ).toggleClass( 'hover' );
 					});
 
 				} else {
-					$(".nav li").removeClass("hover");
-					$(".nav li a").unbind('click');
-					$(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+					$( '.nav li' ).removeClass( 'hover' );
+					$( '.nav li a' ).unbind( 'click' );
+					$( '.nav li' ).unbind( 'mouseenter mouseleave' ).bind( 'mouseenter mouseleave', function() {
 						// must be attached to li so that mouseleave is not triggered when hovering over a submenu
-						$(this).toggleClass('hover');
+						$( this ).toggleClass( 'hover' );
 					});
 				}
 			},
 
 			enablePageScrolling: function enablePageScrolling() {
-				$('html, body').removeClass('scrolling-disabled');
+				$( 'html, body' ).removeClass( 'scrolling-disabled' );
 			},
 
 			disablePageScrolling: function disablePageScrolling() {
-				$('html, body').addClass('scrolling-disabled');
+				$( 'html, body' ).addClass( 'scrolling-disabled' );
 			},
 			// TODO: Once the button code is fixed, this should bind to the .button--disabled class
-			enableButton: function enableButton($button) {
-				$button.removeAttr('disabled');
-				$button.removeClass('card__button--disabled');
+			enableButton: function enableButton( $button ) {
+				$button.removeAttr( 'disabled' );
+				$button.removeClass( 'card__button--disabled' );
 			},
 			// TODO: Once the button code is fixed, this should bind to the .button--disabled class
-			disableButton: function disableButton($button) {
-				$button.attr('disabled', 'disabled');
-				$button.addClass('card__button--disabled');
+			disableButton: function disableButton( $button ) {
+				$button.attr( 'disabled', 'disabled' );
+				$button.addClass( 'card__button--disabled' );
 			}
 		}
 	};
