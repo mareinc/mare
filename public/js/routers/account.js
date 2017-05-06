3a0279a3578f9b8d0311f4142b9c44e54fe015fb
Jared Collier <jared.j.collier@gmail.com>
2017-05-06 17:08:49 -0400

    #238 | change preferences to account

---
+++ public/js/routers/account.js
@@ -0,0 +1,12 @@
+(function () {
+	'use strict';
+
+	mare.routers.Account = Backbone.Router.extend({
+
+		initialize: function initialize() {
+			console.log('loaded account page router');
+		}
+
+	});
+
+}());
