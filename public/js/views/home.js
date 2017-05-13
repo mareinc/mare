288bc7687c24ac7ad7999a687b704160e7c8b5ad
Jared Collier <jared.j.collier@gmail.com>
2017-05-13 17:10:43 -0400

    #21 | update homepage carousel to be full width with no margins

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -4,26 +4,17 @@
     mare.views.Home = Backbone.View.extend({
         el: 'body',
 
-        events: {
-            'click .panel-toggle'	: 'toggleFeaturePanel'
-        },
-
         initialize: function() {
         	// DOM cache any commonly used elements to improve performance
-        	this.$featuredPanelOverlay = $('.panel-overlay');
-            // initialize the owl carousel and make any needed modifications to carousel image loading
-            this.initializeCarousel();
+        	this.$featuredPanelOverlay = $( '.panel-overlay' );
+            // initialize the owl carousel and make any needed modifications to slideshow image loading
+            this.initializeSlideshow();
         },
 
-        /* Shows/hides the list of events in the rightmost featured panel under the carousel */
-        toggleFeaturePanel: function toggleFeaturePanel() {
-            this.$featuredPanelOverlay.toggleClass('panel-overlay--closed');
-        },
-
-        /* initializes the carousel */
-        initializeCarousel: function initializeCarousel() {
-            // initialize the carousel default settings
-            $("#owl-demo").owlCarousel({
+        /* initializes the slideshow */
+        initializeSlideshow: function initializeSlideshow() {
+            // initialize the slideshow default settings
+            $( '#slideshow' ).owlCarousel({
                 autoPlay : 3000,
                 stopOnHover: true,
                 singleItem : true,
@@ -32,10 +23,10 @@
                 autoHeight: true,
                 transitionStyle : 'fade'
             });
-            // prevents the carousel image descriptions from loading in before the images do
+            // prevents the slideshow image descriptions from loading in before the images do
             // TODO: This doesn't seem to be working as well as it needs to.
-            $('#owl-demo img').load(function() {
-                $(this).siblings('.featured-description').removeClass('hidden');
+            $( '#slideshow img' ).load(function() {
+                $(this).siblings( '.slideshow__description' ).removeClass( 'hidden' );
             });
         }
 
