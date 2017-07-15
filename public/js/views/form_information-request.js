e704e6c7dc062b3ae4c38108677d852f3025ddae
Jared Collier <jared.j.collier@gmail.com>
2017-07-15 09:47:20 -0400

    #92, #353 | add parsley form validation to information request form and update required fields

--- public/js/views/form_information-request.js
+++ public/js/views/form_information-request.js
@@ -2,21 +2,37 @@
 	'use strict';
 
 	mare.views.Form_InformationRequest = Backbone.View.extend({
-		el: '.form-name',
+		el: '.form--information-request',
+
+		// TODO: show registration numbers only for child registration
+		events: {
+			'change .interest': 'toggleChildRegistrationNumbersInput'
+		},
 
 		initialize: function() {
 			// DOM cache any commonly used elements to improve performance
+			this.$interest = $( '.interest' );
+			this.$childRegistrationNumbersContainer = $( '.child-registration-numbers-container' );
 
 			// Initialize parsley validation on the form
 			this.form = this.$el.parsley();
 			
-			// this.form.on( 'field:validated', this.validateForm );
+			this.form.on( 'field:validated', this.validateForm );
 		},
 
 		validateForm: function validateForm() {
 			var ok = $( '.parsley-error' ).length === 0;
 			$( '.bs-callout-info' ).toggleClass( 'hidden', !ok );
 			$( '.bs-callout-warning' ).toggleClass( 'hidden', ok );
+		},
+
+		toggleChildRegistrationNumbersInput: function toggleChildRegistrationNumbersInput() {
+			// get the selected interest radio button value
+			var selectedInterest = this.$( '.interest:checked' ).val();
+			// if the selected interest is child info, show the information packet section, otherwise hide it
+			selectedInterest === 'child info'
+				? this.$childRegistrationNumbersContainer.removeClass( 'hidden' )
+				: this.$childRegistrationNumbersContainer.addClass( 'hidden' );
 		}
 
 	});
