a3cdc44ba48e532263462b0ce01c9249918e4fdd
Jared Collier <jared.j.collier@gmail.com>
2017-07-16 15:09:21 -0400

    #350, #351 | create a new template section and Express view handling for additional page buttons on the Register a Child and Register a Family pages

--- public/js/routers/pages.js
+++ public/js/routers/pages.js
@@ -4,6 +4,7 @@
 	mare.routers.Pages = Backbone.Router.extend({
 
 		initialize: function initialize() {
+			mare.views.pages = mare.views.pages || new mare.views.Pages();
 			// Load the view for the right sidebar
 			mare.views.sidebar = mare.views.sidebar || new mare.views.Sidebar();
 		}
