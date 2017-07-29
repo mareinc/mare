c83fbc7b4b5ee8e0fa8848fc2d3ef1848bf8c02c
Jared Collier <jared.j.collier@gmail.com>
2017-07-29 00:24:29 -0400

    #251 | rewrite email service thank you emails to newly registered users to use native promises

--- routes/middleware/emails_register.js
+++ routes/middleware/emails_register.js
@@ -44,55 +44,55 @@ exports.sendRegistrationConfirmationEmailToStaff = ( user, registrationStaffCont
 
 };
 
-exports.sendThankYouEmailToUser = ( staffContactName, staffContactEmail, userEmail, userType, verificationCode, host, done ) => {
-	// do nothing if sending of the email is not currently allowed
-	if( process.env.MIGRATION === 'true' || process.env.SEND_REGISTRATION_THANK_YOU_EMAILS === 'false' ) {
-		return done();
-	}
-	// find the email template in templates/emails/
-	new keystone.Email({
-
-		templateExt 	: 'hbs',
-		templateEngine 	: require( 'handlebars' ),
-		templateName 	: 'register_thank-you-to-user'
-
-	}).send({
-
-		to: 'jared.j.collier@gmail.com',
-		from: {
-			name 	: 'MARE',
-			email 	: 'admin@adoptions.io'
-		},
-		subject       		: 'thank you for registering',
-		isSiteVisitor 		: userType === 'site visitor',
-		isSocialWorker		: userType === 'social worker',
-		isFamily      		: userType === 'family',
-		emailSubject		: `${ userType } registration question`,
-		staffContactEmail,
-		staffContactName,
-		host,
-		userType,
-		verificationCode
-
-	}, ( err, message ) => {
-		// log any errors
-		if( err ) {
-			console.log( `error sending registration thank you email: ${ err }` );
-			return done();
+exports.sendThankYouEmailToUser = ( staffContactName, staffContactEmail, userEmail, userType, verificationCode, host ) => {
+	
+	return new Promise( ( resolve, reject ) => {
+		// TODO: check the logic around process.env.migration, it doesn't seem to make sense
+		// if sending of the email is not currently allowed
+		if( process.env.MIGRATION === 'true' || process.env.SEND_REGISTRATION_THANK_YOU_EMAILS === 'false' ) {
+			// resolve the promise before any further processing takes place
+			return reject( `sending of registration thank you emails is currently turned off, no email was sent to ${ userEmail }` );
 		}
-		// the response object is stored as the 0th element of the returned message
-		const response = message ? message[ 0 ] : undefined;
-		// if the email failed to send, or an error occurred ( which is does, rarely ) causing the response message to be empty
-		if( response && [ 'rejected', 'invalid', undefined ].includes( response.status ) ) {
-			console.log( `thank you to new registered user email failed to send: ${ message }` );
-			console.log( `error: ${ err }` );
-			return done();
-		}
+		// find the email template in templates/emails/
+		new keystone.Email({
 
-		console.log( `thank you to user email sent successfully` );
-		// mark the inquiry thank you email as having been sent to prevent it being sent in the future
-		// inquiry.thankYouSentToInquirer = true;
-		done();
+			templateExt 	: 'hbs',
+			templateEngine 	: require( 'handlebars' ),
+			templateName 	: 'register_thank-you-to-user'
+
+		}).send({
+
+			to: 'jared.j.collier@gmail.com',
+			from: {
+				name 	: 'MARE',
+				email 	: 'admin@adoptions.io'
+			},
+			subject       		: 'thank you for registering',
+			isSiteVisitor 		: userType === 'site visitor',
+			isSocialWorker		: userType === 'social worker',
+			isFamily      		: userType === 'family',
+			emailSubject		: `${ userType } registration question`,
+			staffContactEmail,
+			staffContactName,
+			host,
+			userType,
+			verificationCode
+
+		}, ( err, message ) => {
+			// log any errors
+			if( err ) {
+				return reject( `error sending registration thank you email: ${ err }` );
+			}
+			// the response object is stored as the 0th element of the returned message
+			const response = message ? message[ 0 ] : undefined;
+			// if the email failed to send, or an error occurred ( which it does, rarely ) causing the response message to be empty
+			if( response && [ 'rejected', 'invalid', undefined ].includes( response.status ) ) {
+				// reject the promise with details
+				return reject( `thank you to new registered user email failed to send: ${ message }.  Error: ${ err }` );
+			}
+			// mark the inquiry thank you email as having been sent to prevent it being sent in the future
+			// inquiry.thankYouSentToInquirer = true;
+			resolve();
+		});
 	});
-
 };
