1af11a7f92654b5f94bf5f98a444c03cdffe1a7c
Jared Collier <jared.j.collier@gmail.com>
2017-07-29 00:18:01 -0400

    #292 | update registration Express view to fetch registration mailing lists before rendering

--- routes/views/register.js
+++ routes/views/register.js
@@ -1,22 +1,21 @@
 const keystone					= require( 'keystone' );
-const async						= require( 'async' );
-const registrationMiddleware	= require( '../middleware/service_register' );
-const listsService				= require( '../middleware/service_lists' );
-const pageService				= require( '../middleware/service_page' );
-
+	  async						= require( 'async' ),
+	  listsService				= require( '../middleware/service_lists' ),
+	  pageService				= require( '../middleware/service_page' ),
+	  mailingListService		= require( '../middleware/service_mailing-list' );
 
 exports = module.exports = ( req, res ) => {
 	'use strict';
 
-	const view 		= new keystone.View( req, res );
-	const locals 	= res.locals;
+	const view 		= new keystone.View( req, res ),
+		  locals 	= res.locals;
 
 	// objects with additional search parameters
-	const stateOptions		= { default: 'Massachusetts' };
-	const raceOptions		= { other: true };
-	const waysToHearOptions	= { other: true };
+	const stateOptions		= { default: 'Massachusetts' },
+		  raceOptions		= { other: true },
+		  waysToHearOptions	= { other: true };
 
-	// Fetch all the dynamic data to fill in the form dropdown and selection areas.
+	// fetch all the dynamic data to fill in the form dropdown and selection areas.
 	async.parallel([
 		done => { listsService.getAllStates( req, res, done, stateOptions ); },
 		done => { listsService.getAllRaces( req, res, done, raceOptions ); },
@@ -29,11 +28,12 @@ exports = module.exports = ( req, res ) => {
 		done => { listsService.getAllOtherConsiderations( req, res, done ); },
 		done => { listsService.getChildTypesForWebsite( req, res, done ); },
 		done => { listsService.getAllWaysToHearAboutMARE( req, res, done, waysToHearOptions ); },
+		done => { mailingListService.getRegistrationMailingLists( req, res, done ); },
 		done => { pageService.populateSidebar( req, res, done ); }
 	], () => {
-		// Set the layout to render with the right sidebar
+		// set the layout to render with the right sidebar
 		locals[ 'render-with-sidebar' ] = true;
-		// Render the view once all the data has been retrieved
+		// render the view once all the data has been retrieved
 		view.render( 'register' );
 
 	});
