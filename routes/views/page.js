04c94c7df18234c42ffce17f4eff259afbee0254
Jared Collier <jared.j.collier@gmail.com>
2017-07-30 18:59:35 -0400

    #350, #351 | add page actions sub-template for appending page sections
    this is used to show a link to register for non-social workers on the social worker child and family registration pages.

--- routes/views/page.js
+++ routes/views/page.js
@@ -9,8 +9,10 @@ exports = module.exports = function( req, res ) {
         locals 	= res.locals;
     // create a container for any additional page actions to render after the content
     locals.pageActions = {};
-    // create a placeholder for any buttons that may render after the content
+    // create a placeholder for any buttons that may render in a button group after the content
     locals.pageActions.buttons = [];
+    // create a placeholder for any sections that may render after the content
+    locals.pageActions.sections = [];
 
     // fetch the page with the matching URL
     // if it exists, pass the object into the rendering
@@ -24,16 +26,30 @@ exports = module.exports = function( req, res ) {
         locals[ 'render-with-sidebar' ] = true;
         // if the user requested the 'Register a child' page, specify that it should render a button after the content
         if( locals.targetPage.get( 'key' ) === 'register-a-child' ) {
-            // specify that it should render a button after the content
-            locals.pageActions.hasButtons = true;
-            // set the button contents
-            locals.pageActions.buttons.push( { text: 'Register a Child', target: '/forms/child-registration-form' } );
+            // if the user is logged in as a social worker
+            if( locals.user.userType === 'social worker' ) {
+                // specify that it should render a button after the content
+                locals.pageActions.hasButtons = true;
+                // set the button contents
+                locals.pageActions.buttons.push( { text: 'Register a Child', target: '/forms/child-registration-form' } );
+            // if the user is not a logged in social worker
+            } else {
+                // set the section contents
+                locals.pageActions.sections.push( `You must be logged in as a social worker to register a child.  If you're a social worker, you can <a href="/register#social-worker">register here</a>.` );
+            }
         // otherwise, if the user requested the 'Register a family' page, specify that it should render a button after the content
         } else if( locals.targetPage.get( 'key' ) === 'register-a-family' ) {
-            // specify that it should render a button after the content
-            locals.pageActions.hasButtons = true;
-            // set the button contents
-            locals.pageActions.buttons.push( { text: 'Register a Family', target: '/forms/family-registration-form' } );
+            // if the user is logged in as a social worker
+            if( locals.user.userType === 'social worker' ) {
+                // specify that it should render a button after the content
+                locals.pageActions.hasButtons = true;
+                // set the button contents
+                locals.pageActions.buttons.push( { text: 'Register a Family', target: '/forms/family-registration-form' } );
+            // if the user is not a logged in social worker
+            } else {
+                // set the section contents
+                locals.pageActions.sections.push( `You must be logged in as a social worker to register a family.  If you're a social worker, you can <a href="/register#social-worker">register here</a>.` );
+            }
         // otherwise, if the user requested any page in the 'Considering Adoption section
         } else if( locals.currentSection.title === 'Considering Adoption?' ) {
             // specify that it should render a button after the content
