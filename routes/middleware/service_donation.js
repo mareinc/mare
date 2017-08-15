04b85e63fa2683874d9c1e4ab6cb6eeb2272971c
Mohamed Gueye <gmoha92@gmail.com>
2017-08-14 22:58:39 -0400

    #25| Initial setup of the donations service. Need to setup stripe key and code cleanup.

---
+++ routes/middleware/service_donation.js
@@ -0,0 +1,104 @@
+const stripe = require('stripe')
+      moment = require('moment')
+      q      = requrie('q')
+      donation = require('../models/Donation');
+
+
+
+/*
+*   Define subscription plan types as per stripe API: https://stripe.com/docs/api#create_plan
+*/
+const plan_types = {
+    monthly: {
+        id: 'monthly',
+        interval: 'month'
+    },
+
+    annual: {
+        id: 'annual',
+        interval: 'year'
+    },
+
+    semiannual: {
+        id: 'semiannual',
+        interval: 'month',
+        interval_count: '6'  /*interval_count: The number of intervals between each subscription billing. 
+                                               For example, interval=month and interval_count=3 bills every 3 months.
+                                               Maximum of one year interval allowed (1 year, 12 months, or 52 weeks).*/
+    }
+};
+
+
+
+//Some helper functions
+function createStripeCustomer(stripeToken, email){
+    
+    //Create stripe customer
+    return stripe.customers.create({
+            email: email, 
+            source: stripeToken
+    });
+}
+
+
+function createCharge(customer, amount){
+
+    return stripe.charges.create({
+        amount: amount,
+        currency: 'usd',
+        customer: customer.id
+    });
+}
+
+function createPlan(customer,amount,plan_type){
+
+    return stripe.plans.create({
+        amount: amount,
+        name: plan_type.id,
+        id: plan_type.id,
+        currency: 'usd',
+        interval: plan_type.interval,
+        interval_count: (plan_type.interval_count)? plan_type.interval_count: 1
+    });
+}
+
+
+exports = module.exports = {
+
+    oneTimeCharge: (stripeToken, email, amount) =>{
+
+        var deferred = q.defer(); //start q promise
+
+        createStripeCustomer(stripeToken, email)
+        .then((customer) => createCharge(customer,amount))
+        .then((charge) =>{
+            //Log the customer has been charged x 
+            deferred.resolve();
+        })
+        .catch( (error) =>{
+            //Log the error 
+            deferred.reject();   
+        });
+
+        return deferred.promise; //end q promise
+    },
+
+    recurringCharge: (stripeToken, email, amount, plan_type) =>{
+
+        var deferred = q.defer(); //start q promise
+
+        createStripeCustomer(stripeToken, email)
+        .then((customer) => createPlan(customer,amount,plan_type))
+        .then((plan) =>{
+            //Log the plan has been created and is ready to be used
+            deferred.resolve();
+        })
+        .catch( (error) =>{
+            //Log the error 
+            deferred.reject();   
+        });
+
+        return deferred.promise; //end q promise
+    }
+
+}
