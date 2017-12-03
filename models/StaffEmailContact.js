f4aab8170d77a270e57916e2f80a526928bf84cc
Jared Collier <jared.j.collier@gmail.com>
2017-12-03 18:16:09 -0500

    clean up ordering in staff email contact and CSC region contact models

--- models/StaffEmailContact.js
+++ models/StaffEmailContact.js
@@ -6,10 +6,10 @@ var StaffEmailContact = new keystone.List('Staff Email Contact');
 
 // Create fields
 StaffEmailContact.add({
-	staffEmailContact: { type: Types.Relationship, label: 'staff email contact', ref: 'Admin', filters: { isActive: true }, required: true, initial: true },
-	emailTarget: { type: Types.Relationship, label: 'primary contact for', ref: 'Staff Email Target', required: true, initial: true }
+	emailTarget: { type: Types.Relationship, label: 'primary contact for', ref: 'Staff Email Target', required: true, initial: true },
+	staffEmailContact: { type: Types.Relationship, label: 'staff email contact', ref: 'Admin', filters: { isActive: true }, required: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
-StaffEmailContact.defaultColumns = 'staffEmailContact, emailTarget';
+StaffEmailContact.defaultColumns = 'emailTarget, staffEmailContact';
 StaffEmailContact.register();
