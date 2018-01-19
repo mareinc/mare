662102fad473bebb9d4ccb9adacac3c5c0ee28b5
Jared Collier <jared.j.collier@gmail.com>
2018-01-19 18:51:27 -0500

    update resetPasswordToken field to be uneditable

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ User.add( 'Login Information', {
 
 	email: { type: Types.Email, label: 'email address', unique: true, required: true, initial: true },
 	password: { type: Types.Password, label: 'password', required: true, initial: true },
-	resetPasswordToken: { type: String, hidden: true }
+	resetPasswordToken: { type: String, hidden: true, noedit: true }
 
 }, {
 
