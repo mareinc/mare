2ef627102d5930d1dcb192fe6e313e28482a5051
Jared Collier <jared.j.collier@gmail.com>
2018-01-23 14:10:14 -0500

    add new cities/towns to the auto creation scripts

---
+++ updates/0.0.3-list_city-or-town.js
@@ -0,0 +1,15 @@
+exports.create = {
+	'City or Town': [{
+		cityOrTown: 'Assonet',
+		areaOffice: 'New Bedford Area Office',
+		textRegion: 'southern'
+	}, {
+		cityOrTown: 'Baldwinville',
+		areaOffice: 'North Central Area Office',
+		textRegion: 'central'
+	}, {
+		cityOrTown: `Martha's Vineyard`,
+		areaOffice: 'Cape & Islands Area Office',
+		textRegion: 'southern'
+	}]
+};
