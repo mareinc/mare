6512747b22a5ca7979224424bde881cf0086e365
Noah Weinert <n-devr@users.noreply.github.com>
2018-01-24 16:30:03 -0500

    adds child gallery to the my account page for social workers
    loads all children, not just the children they work with

--- public/js/views/account-children.js
+++ public/js/views/account-children.js
@@ -16,18 +16,30 @@
 			var html = this.template();
 			// render the template to the page
 			this.$el.html( html );
+			// render the child gallery
+			this.renderChildGallery();
 		},
 		
 		hide: function hide() {
 			// hide the section
 			this.$el.hide();
-			// remove the contents of the view
-			this.$el.empty();
-			// NOTE: if any events are bound to DOM elements, they should be explicitly removed here as well
 		},
 
 		show: function show() {
 			this.$el.show();
+		},
+
+		renderChildGallery: function renderChildGallery() {
+			// create a new WaitingChildProfiles view
+			mare.views.waitingChildProfiles = mare.views.waitingChildProfiles || new mare.views.WaitingChildProfiles();
+			// hide the search form
+			mare.views.waitingChildProfiles.$searchForm.hide();
+			// hide the child profiles header section
+			mare.views.waitingChildProfiles.$childProfilesHeaderCard.hide();
+			// show the gallery
+			mare.views.waitingChildProfiles.$gallery.show();
+			// render the gallery
+			mare.views.gallery.render();
 		}
 	});
 }());
