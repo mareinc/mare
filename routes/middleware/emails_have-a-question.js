08f72ae230e8509a9c59291046b162f20539e0a8
Jared Collier <jared.j.collier@gmail.com>
2018-05-18 10:18:34 -1000

    updates email error messages to log more than [ object, object ]

--- routes/middleware/emails_have-a-question.js
+++ routes/middleware/emails_have-a-question.js
@@ -1,48 +1,48 @@
-const keystone				= require( 'keystone' ),
-	  utilitiesMiddleware   = require( './utilities' );
+const keystone = require('keystone'),
+	utilitiesMiddleware = require('./utilities');
 
 exports.sendNewQuestionNotificationEmailToMARE = ( question, staffEmailContact ) => {
 
 	return new Promise( ( resolve, reject ) => {
 
 		const staffEmail = staffEmailContact.email;
-		
+
 		// if sending of the email is not currently allowed
-		if( process.env.SEND_QUESTION_RECEIVED_EMAILS_TO_MARE !== 'true' ) {
+		if ( process.env.SEND_QUESTION_RECEIVED_EMAILS_TO_MARE !== 'true' ) {
 			// reject the promise with information about why
 			return reject( `sending of the email is disabled` );
 		}
 
-		if( !staffEmail ) {
+		if (!staffEmail) {
 			return reject( `no staff contact was provided` );
 		}
 
 		// the email template can be found in templates/emails/
 		new keystone.Email({
-			templateExt		: 'hbs',
-			templateEngine 	: require( 'handlebars' ),
-			templateName 	: 'have-a-question-notification-to-mare'
+			templateExt: 'hbs',
+			templateEngine: require( 'handlebars' ),
+			templateName: 'have-a-question-notification-to-mare'
 		}).send({
-			to				: staffEmail,
+			to: staffEmail,
 			from: {
-				name 	: 'MARE',
-				email 	: 'admin@adoptions.io'
+				name: 'MARE',
+				email: 'admin@adoptions.io'
 			},
-			subject		: `new question`,
+			subject: `new question`,
 			question
 
-		}, ( err, message ) => {
+		}, (err, message) => {
 			// if there was an error sending the email
-			if( err ) {
+			if (err) {
 				// reject the promise with details
 				return reject( `error sending new question notification email to MARE - ${ err }` );
 			}
 			// the response object is stored as the 0th element of the returned message
 			const response = message ? message[ 0 ] : undefined;
 			// if the email failed to send, or an error occurred ( which it does, rarely ) causing the response message to be empty
-			if( response && [ 'rejected', 'invalid', undefined ].includes( response.status ) ) {
+			if (response && [ 'rejected', 'invalid', undefined ].includes( response.status ) ) {
 				// reject the promise with details
-				return reject( `error sending new question notification email to MARE - ${ err }` );
+				return reject( `error sending new question notification email to MARE - ${ response.status } - ${ response.email } - ${ response.reject_reason } - ${ err }` );
 			}
 
 			resolve();
