39395953033cefecdd6d0e707ef8c1d0fcb1f273
Jared Collier <jared.j.collier@gmail.com>
2018-10-15 09:57:03 -0400

    fix list display showing incorrect dates (#179)
    This was causing dates to be stored at the specified date with a time of 00:00:00.000Z in UTC, which was interpreted as -4:00:00 when displayed in date columns.
    
    Note: in order to correct the issue for previously saved dates, 5 hours need to be added to each date field in the database.

--- models/Tracking_SocialWorkerHistory.js
+++ models/Tracking_SocialWorkerHistory.js
@@ -12,7 +12,7 @@ var SocialWorkerHistory = new keystone.List('Social Worker History', {
 SocialWorkerHistory.add({
 
     socialWorker: { type: Types.Relationship, label: 'social worker', ref: 'Social Worker', required: true, noedit: true, initial: true },
-    date: { type: Types.Date, label: 'date', inputFormat: 'MM/DD/YYYY', format: 'MM/DD/YYYY', utc: true, default: Date.now, required: true, noedit: true },
+    date: { type: Types.Date, label: 'date', inputFormat: 'MM/DD/YYYY', format: 'MM/DD/YYYY', default: Date.now, required: true, noedit: true },
     summary: { type: Types.Text, label: 'summary of changes', noedit: true, initial: false },
     changes: { type: Types.Html, label: 'changes', wysiwyg: true, noedit: true, initial: false },
     modifiedBy: { type: Types.Relationship, label: 'modified by', ref: 'Admin', required: true, noedit: true, initial: true }
