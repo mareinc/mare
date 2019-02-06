e014d6b27cbe9313150939a7c6f515d5c6024976
Jared Collier <jared.j.collier@gmail.com>
2019-02-06 09:58:16 -0500

    Add better logging to mailing list services (#24)

--- routes/middleware/service_mailing-list.js
+++ routes/middleware/service_mailing-list.js
@@ -6,9 +6,9 @@ exports.getRegistrationMailingLists = () => {
 
 		keystone.list( 'Mailing List' ).model
 			.find( { $or: [
-						{ 'showOnSiteVisitorRegistrationPage': true },
-						{ 'showOnSocialWorkerRegistrationPage': true },
-						{ 'showOnFamilyRegistrationPage': true } ] } )
+				{ 'showOnSiteVisitorRegistrationPage': true },
+				{ 'showOnSocialWorkerRegistrationPage': true },
+				{ 'showOnFamilyRegistrationPage': true } ] } )
 			.exec()
 			.then( mailingLists => {
 				// if no mailing lists could not be found
@@ -16,7 +16,7 @@ exports.getRegistrationMailingLists = () => {
 					// log a message for debugging purposes
 					console.log( `no mailing lists could be found` );
 					// resolve the promise
-					return resolve();
+					return resolve( [] );
 				}
 				// if mailing lists were successfully returned, resolve with the array
 				resolve( mailingLists );
@@ -36,18 +36,10 @@ exports.getMailingListsByUserType = userType => {
 		let query;
 		
 		switch( userType ) {
-			case 'social worker':
-				query = { 'showOnSocialWorkerRegistrationPage': true };
-				break;
-			case 'site visitor':
-				query = { 'showOnSiteVisitorRegistrationPage': true };
-				break;
-			case 'family':
-				query = { 'showOnFamilyRegistrationPage': true };
-				break;
-			default:
-				resolve( [ ] );
-				return;
+			case 'social worker'	: query = { 'showOnSocialWorkerRegistrationPage': true }; break;
+			case 'site visitor'		: query = { 'showOnSiteVisitorRegistrationPage': true }; break;
+			case 'family'			: query = { 'showOnFamilyRegistrationPage': true }; break;
+			default					: return resolve( [] );
 		}
 
 		keystone.list( 'Mailing List' ).model
@@ -57,16 +49,16 @@ exports.getMailingListsByUserType = userType => {
 				// if no mailing lists could not be found
 				if( mailingLists.length === 0 ) {
 					// log a message for debugging purposes
-					console.log( `no mailing lists could be found` );
+					console.log( `no mailing lists could be found for user type ${ userType }` );
 					// resolve the promise
-					return resolve();
+					return resolve( [] );
 				}
 				// if mailing lists were successfully returned, resolve with the array
 				resolve( mailingLists );
 			// if an error was encountered fetching from the database
 			}, err => {
 				// log the error for debugging purposes
-				console.error( `error fetching the mailing lists for the registration page - ${ err }` );
+				console.error( `error fetching the mailing lists by user type ${ userType } - ${ err }` );
 				// reject the promise
 				reject();
 			});
