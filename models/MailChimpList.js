f8ea04b34a3089d81cf588b0c7ef95664595f57d
Noah Weinert <noahweinert@gmail.com>
2019-04-25 08:25:25 -0400

    Update MailChimpList model configuration

--- models/MailChimpList.js
+++ models/MailChimpList.js
@@ -8,11 +8,11 @@ const MailChimpList = new keystone.List('MailChimpList', {
 });
 
 // Create fields
-MailChimpList.add( 'MailChimp Mailing List', {
-    name:      { type: Types.Text, required: true, initial: true },
+MailChimpList.add( 'Mailing List', {
+    name:      { type: Types.Text, label: 'mailing list name', required: true, initial: true },
     displayText: { type: Types.Text, label: 'label on website', initial: true }
     }, 'MailChimp Configuration', {
-    mailChimpId:    { type: Types.Text, label: 'MailChimp List ID', required: true, initial: true }
+    mailChimpId:    { type: Types.Text, label: 'mailchimp list id', required: true, initial: true }
 });
 
 // Define default columns in the admin interface and register the model
