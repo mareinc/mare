145f4973822b419dc041ca5a048b835937593554
Jared Collier <jared.j.collier@gmail.com>
2019-05-16 23:23:48 -0400

    Update image fields to be of the AWS persuasion (#128)

--- models/SlideshowItem.js
+++ models/SlideshowItem.js
@@ -33,15 +33,16 @@ var SlideshowItem = new keystone.List('Slideshow Item', {
 SlideshowItem.add({
 
 	tempImage: { type: Types.File, storage: imageStorage, label: 'temp image' },
-	image: {
-		type: Types.CloudinaryImage,
-		folder: `${ process.env.CLOUDINARY_DIRECTORY }/slideshow/`,
-		select: true,
-		selectPrefix: `${ process.env.CLOUDINARY_DIRECTORY }/slideshow/`,
-		autoCleanup: true,
-		whenExists: 'overwrite',
-		filenameAsPublicID: true
-	},
+	image: { type: Types.File, storage: imageStorage, label: 'image' },
+	// image: {
+	// 	type: Types.CloudinaryImage,
+	// 	folder: `${ process.env.CLOUDINARY_DIRECTORY }/slideshow/`,
+	// 	select: true,
+	// 	selectPrefix: `${ process.env.CLOUDINARY_DIRECTORY }/slideshow/`,
+	// 	autoCleanup: true,
+	// 	whenExists: 'overwrite',
+	// 	filenameAsPublicID: true
+	// },
 	parent: { type: Types.Relationship, label: 'slideshow', ref: 'Slideshow', initial: true },
 	order: { type: Types.Number, label: 'order', initial: true }
 
