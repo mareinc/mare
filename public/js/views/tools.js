ddeff0796e021b07bfb9610f449b531186f2446e
Arek Jankowski <ajankowski@neptuneweb.com>
2019-07-02 15:29:52 -0400

    Add highlighting of active navigation item based on the current page

--- public/js/views/tools.js
+++ public/js/views/tools.js
@@ -14,22 +14,32 @@
 		
 		showDashboard: function( fromDate, toDate ) {
 			mare.views.dashboard.render( fromDate, toDate );
+			// update the body class to indicate what screen this is
+			this.$el.attr('class', 'tools__dashboard');
 		},
 		
 		showFamilyMatching: function( familyID, params ) {
 			mare.views.familyMatching.render( familyID, params );
+			// update the body class to indicate what screen this is
+			this.$el.attr('class', 'tools__family-matching');
 		},
 		
 		showChildMatching: function( childID, params ) {
 			mare.views.childMatching.render( childID, params );
+			// update the body class to indicate what screen this is
+			this.$el.attr('class', 'tools__child-matching');
 		},
 		
 		showFamilyMatchingRequest: function() {
 			mare.views.familyMatchingRequest.render();
+			// update the body class to indicate what screen this is
+			this.$el.attr('class', 'tools__family-matching');
 		},
 		
 		showChildMatchingRequest: function() {
 			mare.views.childMatchingRequest.render();
+			// update the body class to indicate what screen this is
+			this.$el.attr('class', 'tools__child-matching');
 		}
 		
 	});
