2bd594533e439beb82dc67fe30a21ee7d677afa7
Noah Weinert <noahweinert@gmail.com>
2019-12-18 14:40:17 -0500

    Create Careers Tab Under About Us (#300)

--- src/utils/setup.middleware.js
+++ src/utils/setup.middleware.js
@@ -62,7 +62,8 @@ exports.initLocals = function( req, res, next ) {
 			{ title: 'Meet the Staff', href: '/page/meet-the-staff' },
 			{ title: 'Board of Directors', href: '/page/board-of-directors' },
 			{ title: 'MARE in the News', href: '/mare-in-the-news' },
-			{ title: 'Annual Report', href: '/page/annual-report' }
+			{ title: 'Annual Report', href: '/page/annual-report' },
+			{ title: 'Careers', href: '/page/careers' }
 		]}];
 
 	// based on the url from the requested page, fetch the navigation object for the site section
