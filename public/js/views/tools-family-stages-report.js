872821ba7b7834870e6ae7fdc1701af28154eee7
Noah Weinert <noahweinert@gmail.com>
2021-01-19 14:30:01 -0500

    Implement configurable date search component (#636)

--- public/js/views/tools-family-stages-report.js
+++ public/js/views/tools-family-stages-report.js
@@ -17,6 +17,8 @@
         
         initializeSearchForm: function( regDateFrom, regDateTo, params ) {
 
+			var view = this;
+
 			// initialize the registration date range picker
 			this.$el.find( '[name="registration-date-range"]' ).daterangepicker({
 				startDate: moment( regDateFrom ),
@@ -31,7 +33,72 @@
 					'Year to Date': [ moment().startOf( 'year' ), moment() ],
 					'All Time': [ moment( '2000-01-01' ), moment() ]
 				}
-            });
+			});
+
+			// initialize all date search fields
+			this.$el.find( '.reporting-date-search-field' ).each( function() {
+				view.initializeDateSearchField( $( this ), moment( regDateFrom ), moment( regDateTo ) );
+			});
+		},
+
+		initializeDateSearchField: function( $el, defaultDateRangeStart, defaultDateRangeEnd ) {
+
+			// initialize the date pickers
+			$el.find( '.date-range-picker' )
+			
+			// initilize the search type select
+			var $dateSearchTypeField = $el.find( '.date-search-type-select' );
+			
+			// add change handler
+			$dateSearchTypeField.change( function() {
+				
+				var searchType = $( this ).val();
+				var $datePicker = $el.find( '.date-range-picker' );
+				
+				// update date picker visiblity and configuration
+				switch ( searchType ) {
+					case 'ignore':
+						// hide the date picker
+						$datePicker.hide();
+						break;
+					case 'before':
+					case 'after':
+						// show the picker, configure as a single date
+						$datePicker.daterangepicker({
+							singleDatePicker: true,
+							showDropdowns: true,
+							minYear: 2000,
+							maxYear: parseInt( moment().format( 'YYYY' ), 10 )
+						});
+						$datePicker.show();
+						break;
+					case 'between':
+						// show the picker, configure as a date range
+						$datePicker.daterangepicker({
+							startDate: defaultDateRangeStart,
+							endDate: defaultDateRangeEnd,
+							alwaysShowCalendars: true,
+							showDropdowns: true,
+							linkedCalendars: false,
+							minYear: 2000,
+							maxYear: parseInt( moment().format( 'YYYY' ), 10 ),
+							ranges: {
+								'Last 30 Days': [ moment().subtract( 29, 'days' ), moment() ],
+								'Year to Date': [ moment().startOf( 'year' ), moment() ],
+								'All Time': [ moment( '2000-01-01' ), moment() ]
+							}
+						});
+						$datePicker.show();
+						break;
+					default:
+						
+				}
+				// update the date picker name
+				$datePicker.attr( 'name', $datePicker.data( 'fieldName' ) + '-' + searchType );
+			});
+
+			// trigger change event to ensure search field is initialized in the correct state
+			$dateSearchTypeField.change();
 		},
 
 		/* render the view onto the page */
