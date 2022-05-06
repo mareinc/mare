6204f659b8de367cc93a95cfbbab804e80c3fc8e
Noah Weinert <noahweinert@gmail.com>
2022-05-06 11:37:58 -0400

    Initializes family activity date range search field (#733)

--- public/js/views/tools-family-activity-report.js
+++ public/js/views/tools-family-activity-report.js
@@ -16,7 +16,26 @@
 			this.template = Handlebars.compile( toolsFamilyActivityReportTemplate );
         },
 
-        initializeSearchForm: function( fromDate, toDate, params ) {},
+        initializeSearchForm: function( fromDate, toDate, params ) {
+
+            // initialize the date range picker
+			this.$el.find( '[name="family-activity-date-range"]' ).daterangepicker({
+				startDate: moment( fromDate ),
+    			endDate: moment( toDate ),
+				alwaysShowCalendars: true,
+				showDropdowns: true,
+				linkedCalendars: false,
+                minDate: moment( '2010-01-01' ),
+                maxDate: moment().subtract( 1, 'day' ),
+				minYear: 2010,
+				maxYear: parseInt( moment().format( 'YYYY' ), 10 ),
+				ranges: {
+					'Last 30 Days': [ moment().subtract( 31, 'days' ), moment() ],
+					'Year to Date': [ moment().startOf( 'year' ), moment() ],
+					'All Time': [ moment( '2021-06-14' ), moment() ]
+				}
+			});
+        },
 
         handleSearchClick: function() {},
 
@@ -28,6 +47,7 @@
 			
 			var view = this;
             view.$el.html( view.template() );
+            view.initializeSearchForm( view.$el.find( '#defaultFromDate' ).val(), view.$el.find( '#defaultToDate' ).val() );
         }
 	});
 }());
