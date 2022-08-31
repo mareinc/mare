117bb9c62aae419b88aebc72484275e1f2585807
Noah Weinert <noahweinert@gmail.com>
2022-08-31 14:11:56 -0400

    Implements service to create new HubSpot contacts (#807)

--- src/components/hubspot services/hubspot.controllers.js
+++ src/components/hubspot services/hubspot.controllers.js
@@ -8,10 +8,51 @@ exports.findContactByEmail = async function findContactByEmail( email ) {
     const filter = { propertyName: 'email', operator: 'EQ', value: email };
     const filterGroup = { filters: [ filter ] };
 
-    // search hubspot contacts by email
-    const hubspotApiResponse = await hubspotClient.crm.contacts.searchApi.doSearch({
-        filterGroups: [ filterGroup ]
-    });
+    let hubspotApiResponse = false;
+
+    try {
+
+        // search hubspot contacts by email
+        hubspotApiResponse = await hubspotClient.crm.contacts.searchApi.doSearch({
+            filterGroups: [ filterGroup ]
+        });
+
+    } catch ( error ) {
+
+        // log any errors
+        error.message === 'HTTP request failed'
+            ? console.error( JSON.stringify( error.response, null, 2 ) )
+            : console.error( error );
+    }
+
+    return hubspotApiResponse;
+}
+
+exports.createNewContact = async function createNewContact( email, firstName, lastName ) {
+
+    // configure contact properties
+    const properties = {
+        'email': email,
+        'firstname': firstName,
+        'lastname': lastName
+    };
+
+    let hubspotApiResponse = false;
+
+    try {
+
+        // create the contact in hubspot
+        hubspotApiResponse = await hubspotClient.crm.contacts.basicApi.create({
+            properties
+        });
+
+    } catch ( error ) {
+
+        // log any errors
+        error.message === 'HTTP request failed'
+            ? console.error( JSON.stringify( error.response, null, 2 ) )
+            : console.error( error );
+    }
 
     return hubspotApiResponse;
 }
