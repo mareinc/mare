4c6c1c3d36431c42ae71b1e249167fe964c0bcfd
Noah Weinert <noahweinert@gmail.com>
2022-08-31 15:09:01 -0400

    Implements a helper function in HubSpot services (#807)
    Only creates a HubSpot contact if an existing contact isn’t found.

--- src/components/hubspot services/hubspot.controllers.js
+++ src/components/hubspot services/hubspot.controllers.js
@@ -28,6 +28,7 @@ exports.findContactByEmail = async function findContactByEmail( email ) {
     return hubspotApiResponse;
 }
 
+// create a hubspot cms contact
 exports.createNewContact = async function createNewContact( email, firstName, lastName ) {
 
     // configure contact properties
@@ -55,4 +56,48 @@ exports.createNewContact = async function createNewContact( email, firstName, la
     }
 
     return hubspotApiResponse;
+}
+
+// helper function to find an existing contact or create a new one if it doesn't exist
+exports.findOrCreateNewContact = async function findOrCreateNewContact( email, firstName, lastName ) {
+
+    console.log( `Creating a new HubSpot contact for user with email: ${email}` );
+    
+    try {
+
+        // search for an existing contact
+        const findContactByEmailResponse = await exports.findContactByEmail( email );
+
+        // if the contact already exists, do not create a duplicate contact
+        if ( findContactByEmailResponse && findContactByEmailResponse.total > 0 ) {
+
+            console.log( `HubSpot contact already exists for user with email: ${email}` );
+            return;
+        }
+
+        // create a new contact
+        const createNewContactResponse = await exports.createNewContact( email, firstName, lastName );
+        
+        // if the contact was created successfully...
+        if ( !createNewContactResponse.message ) {
+            
+            console.log( `Created a new HubSpot contact for user with email: ${email}` );
+            console.log( `HubSpot contact ID: ${createNewContactResponse.id}` );
+        
+        // if there was an error during contact creation...
+        } else {
+
+            console.log( 'Failed to create a new HubSpot contact.' );
+            console.error( JSON.stringify( createNewContactResponse ) );
+        }
+        
+        return;
+
+    } catch ( error ) {
+
+        // log any errors
+        error.message === 'HTTP request failed'
+            ? console.error( JSON.stringify( error.response, null, 2 ) )
+            : console.error( error );
+    }
 }
