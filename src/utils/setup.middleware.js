ee19ac539158d6c2169eb7655bfc68530ef55843
Noah Weinert <noahweinert@gmail.com>
2022-10-12 13:16:59 -0400

    Update main nav menu items to match HubSpot menu (#814)

--- src/utils/setup.middleware.js
+++ src/utils/setup.middleware.js
@@ -19,53 +19,44 @@ exports.initLocals = function( req, res, next ) {
 	// store the CloudFront url for image urls
 	locals.cloudfrontUrl = process.env.CLOUDFRONT_URL;
 
+	const HUBSPOT_SITE_BASE_URL = 'https://21835120.hs-sites.com';
+
 	// create the main menu navigation.
 	locals.mainNav = [
 		// TODO: add custom header background image for each menu item
-		{ title: 'Considering Adoption?', subMenu: [
-			{ title: 'Types of Adoption', href: '/page/types-of-adoption' },
-			{ title: 'Can I adopt a Child from Foster Care?', href: '/page/can-i-adopt-a-child-from-foster-care' },
-			{ title: 'Steps in the Process', href: '/steps-in-the-process' },
-			{ title: 'How Can MARE Help?', href: '/page/how-can-mare-help' },
-			{ title: 'Adoption Stories', href: '/adoption-stories' }
-		]},
 		{ title: 'Meet the Children', subMenu: [
-			{ title: 'Who are the Children?', href: '/page/who-are-the-children' },
-			{ title: 'Waiting Child Profiles', href: '/waiting-child-profiles' },
-			{ title: 'Other Ways to Meet Waiting Children', href: '/page/other-ways-to-meet-waiting-children' }
+			{ title: 'Featured Youth', href: `${HUBSPOT_SITE_BASE_URL}/featured-youth` },
+			{ title: 'Child Profiles', href: '/waiting-child-profiles' },
+			{ title: 'Who are the Children?', href: `${HUBSPOT_SITE_BASE_URL}/who-are-the-children` }
 		]},
-		{ title: 'Family Support Services', subMenu: [
-			{ title: 'How Does MARE Support Families', href: '/page/how-does-mare-support-families' },
-			{ title: 'Friend of the Family Mentor Program', href: '/page/friend-of-the-family-mentor-program' },
-			{ title: 'Other Family Support Services', href: '/page/other-family-support-services' }
+		{ title: 'Considering Adoption?', subMenu: [
+			{ title: 'How To Adopt From Foster Care', href: `${HUBSPOT_SITE_BASE_URL}/how-to-adopt-from-foster-care` },
+			{ title: 'Stories', href: `${HUBSPOT_SITE_BASE_URL}/stories` },
+			{ title: 'FAQ', href: `${HUBSPOT_SITE_BASE_URL}/frequently-asked-questions` },
+			{ title: 'Get Support', href: `${HUBSPOT_SITE_BASE_URL}/get-support` }
 		]},
-		{ title: 'For Social Workers', subMenu: [
-			{ title: 'How MARE Can Help You', href: '/page/how-mare-can-help-you' },
-			{ title: 'Register/Update a Child', href: '/page/register-update-a-child' },
-			{ title: 'Attend Events', href: '/page/attend-events' },
-			{ title: `Register a Family's Homestudy`, href: '/page/register-a-familys-homestudy' }
+		{ title: 'Resources', subMenu: [
+			{ title: 'E-Books & Downloads', href: `${HUBSPOT_SITE_BASE_URL}/resources` },
+			{ title: 'Learning Center', href: `${HUBSPOT_SITE_BASE_URL}/articles` }
 		]},
 		{ title: 'Events', subMenu: [
-			{ title: 'MARE Hosted Events', href: '/events/mare-hosted-events/'},
-			{ title: 'Partner Hosted Events', href: '/events/partner-hosted-events'},
-			{ title: 'MAPP Training', href: '/events/mapp-trainings/' }
-		]},
-		{ title: 'Ways to Help', subMenu: [
-			{ title: 'Why give?', href: '/page/why-give' },
-			{ title: 'How you can help', href: '/page/how-you-can-help' },
-			{ title: 'How businesses and organizations can help', href: '/page/how-businesses-and-organizations-can-help' },
-			{ title: 'Experienced families', href: '/page/experienced-families' },
-			{ title: 'Weekend Family Connections', href: '/page/weekend-family-connections' },
-			{ title: 'The Neighborhood', href: '/page/the-neighborhood' },
-			{ title: 'Shop', href: 'https://store.mareinc.org' }
+			{ title: 'MARE Family Events', href: '/events/mare-hosted-events/'},
+			{ title: 'Partner Hosted Events', href: HUBSPOT_SITE_BASE_URL },
+			{ title: 'MAPP Training', href: HUBSPOT_SITE_BASE_URL },
+			{ title: 'Community Events', href: HUBSPOT_SITE_BASE_URL }
 		]},
 		{ title: 'About Us', lastMenu: true, subMenu: [
-			{ title: 'Who We Are', href: '/page/who-we-are' },
-			{ title: 'What We Do', href: '/page/what-we-do' },
-			{ title: 'Meet the Staff', href: '/page/meet-the-staff' },
-			{ title: 'Board of Directors', href: '/page/board-of-directors' },
-			{ title: 'Annual Report', href: '/page/annual-report' },
-			{ title: 'Careers', href: '/page/careers' }
+			{ title: 'Why We Exist', href: `${HUBSPOT_SITE_BASE_URL}/who-we-are` },
+			{ title: 'Our Values and Commitments', href: `${HUBSPOT_SITE_BASE_URL}what-we-do` },
+			{ title: 'Our Organization', href: `${HUBSPOT_SITE_BASE_URL}/our-organization` },
+			{ title: 'News and Updates', href: `${HUBSPOT_SITE_BASE_URL}/news-and-updates` },
+		]},
+		{ title: 'Get Involved', subMenu: [
+			{ title: 'Donate', href: 'https://secure.givelively.org/donate/massachusetts-adoption-resource-exchange-inc' },
+			{ title: 'Weekend Family Connections', href: `${HUBSPOT_SITE_BASE_URL}/weekend-family-connections` },
+			{ title: 'Join the Neighborhood', href: `${HUBSPOT_SITE_BASE_URL}/join-the-neighborhood` },
+			{ title: 'Raise Support', href: `${HUBSPOT_SITE_BASE_URL}/raise-support` },
+			{ title: 'Shop', href: 'https://store.mareinc.org/' }
 		]}];
 
 	// based on the url from the requested page, fetch the navigation object for the site section
